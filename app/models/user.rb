48d6f884987fc5187c07f5c63605da7f37d1c36a
Duarte Henriques <duarte.henriques@gmail.com>
2018-03-20 11:18:38 +0000

    Validate username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
          :token_authenticatable
 
   has_many :authentication_tokens, class_name: "AuthToken", dependent: :destroy
-  
-  validates :username, presence: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
