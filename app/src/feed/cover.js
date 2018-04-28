ef0cedf8ce15a34bc1377f57e2a40f3e3aeffb1d
Duarte Henriques <duarte.henriques@gmail.com>
2018-04-28 16:21:52 +0100

    Change cover style

--- app/src/feed/cover.js
+++ app/src/feed/cover.js
@@ -3,20 +3,23 @@ import styled from 'styled-components'
 
 const StyledDiv = styled.div`
   position: relative;
-  margin-bottom: 2rem;
   box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.13);
   border-radius: 20px;
-  height: 220px;
+  margin-bottom: 2rem;
 `
 
 const CoverPic = styled.div`
-  height: 170px;
+  position: relative;
+  height: 240px;
   background-image: url(${({ backgroundImageUrl }) => backgroundImageUrl});
   background-size: cover;
   box-shadow: inset 0 -66px 40px -40px #fff;
 `
 
 const ProfilePic = styled.img`
+  position: absolute;
+  bottom: 15px;
+  left: 15px;
   height: 80px;
   width: 80px;
   border-radius: 50%;
@@ -24,46 +27,28 @@ const ProfilePic = styled.img`
   margin-right: 0.7rem;
 `
 
-const AbsoluteDiv = styled.div`
-  align-items: flex-end;
-  position: absolute;
-  bottom: 0;
-  padding: 120px 14px 14px 14px;
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-`
-
-const TitleAndDescription = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: calc(100% - 90px);
-`
-
 const Title = styled.div`
+  position: absolute;
+  left: 110px;
+  bottom: 0;
   font-weight: 700;
   color: black;
   font-size: 20px;
-  padding-bottom: 0.5rem;
-  padding-top: 0.5rem;
 `
 
 const Description = styled.div`
+  padding: 6px 15px 15px 110px;
   font-size: 14px;
   font-weight: 500;
 `
 
 const Cover = ({ collection }) => (
   <StyledDiv>
-    <CoverPic backgroundImageUrl={collection.coverPicUrl} />
-    <AbsoluteDiv>
-      <ProfilePic src={collection.profilePicUrl} />
-      <TitleAndDescription>
-        <Title>{collection.title}</Title>
-        <Description>{collection.description}</Description>
-      </TitleAndDescription>
-    </AbsoluteDiv>
+    <CoverPic backgroundImageUrl={collection.coverPicUrl}>
+      <Title>{collection.title}</Title>
+    </CoverPic>
+    <ProfilePic src={collection.profilePicUrl} />
+    <Description>{collection.description}</Description>
   </StyledDiv>
 )
 
