65c90cd01dbc81f5654e36f406650f33e5ce2c23
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-05-10 11:42:09 +0100

    Added master_product_ref field to Product

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426111642) do
+ActiveRecord::Schema.define(version: 20180510104040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20180426111642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "media_items"
+    t.string "master_product_ref", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
