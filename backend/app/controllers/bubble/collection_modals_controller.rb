d4438b77069699b6ae0af5904e7a26862b3764da
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-05-11 11:02:22 +0100

    added collection_id to permitted params in collection_modal and fenced_collection

--- backend/app/controllers/bubble/collection_modals_controller.rb
+++ backend/app/controllers/bubble/collection_modals_controller.rb
@@ -11,6 +11,6 @@ class Bubble::CollectionsModalsController < BubbleController
   private
 
   def collection_modal_params
-    params.require(:collection_modal).permit(:logo_pic_url, :cover_pic_url, :title, :text, :cta_text)
+    params.require(:collection_modal).permit(:collection_id, :logo_pic_url, :cover_pic_url, :title, :text, :cta_text)
   end
 end
