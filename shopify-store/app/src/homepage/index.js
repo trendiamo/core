cf890e944fd55e35d2129a043900f3050d99d5e8
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-05-26 14:25:53 +0100

    third frontend deploy

--- shopify-store/app/src/homepage/index.js
+++ shopify-store/app/src/homepage/index.js
@@ -1,5 +1,5 @@
 const template = `
-  <iframe seamless src="https://trendiamo.com/" style="display:block;border:0;width:100vw;height:100vh;" />
+  <iframe seamless src="https://nondimension.com/" style="display:block;border:0;width:100vw;height:100vh;" />
 `
 export default () => {
   document.querySelector('body').innerHTML = template
