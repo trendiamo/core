e7ba2b7241aab02a647b1416160d6fa90753148a
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-07-23 15:42:35 +0100

    Add master_product_id concept

--- solidus-platform/db/schema.rb
+++ solidus-platform/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614122926) do
+ActiveRecord::Schema.define(version: 20180723144716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -425,6 +425,7 @@ ActiveRecord::Schema.define(version: 20180614122926) do
     t.datetime "updated_at", precision: 6
     t.boolean "promotionable", default: true
     t.string "meta_title"
+    t.integer "master_product_id"
     t.index ["available_on"], name: "index_spree_products_on_available_on"
     t.index ["deleted_at"], name: "index_spree_products_on_deleted_at"
     t.index ["name"], name: "index_spree_products_on_name"
