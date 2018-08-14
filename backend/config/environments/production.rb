a2ee56cdb81d20dae114ecca82e6e888b1ce5ab9
Duarte Henriques <duarte.henriques@gmail.com>
2018-08-14 13:48:27 +0100

    Try mailgun via smtp

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -59,20 +59,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: ENV["MAILER_HOST"] }
-  # config.action_mailer.smtp_settings = {
-  #   port: ENV["MAILGUN_SMTP_PORT"],
-  #   address: ENV["MAILGUN_SMTP_SERVER"],
-  #   user_name: ENV["MAILGUN_SMTP_LOGIN"],
-  #   password: ENV["MAILGUN_SMTP_PASSWORD"],
-  #   domain: ENV["MAILGUN_DOMAIN"],
-  #   authentication: :plain,
-  # }
-  # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV["MAILGUN_API_KEY"],
+  config.action_mailer.smtp_settings = {
+    port: ENV["MAILGUN_SMTP_PORT"],
+    address: ENV["MAILGUN_SMTP_SERVER"],
+    user_name: ENV["MAILGUN_SMTP_LOGIN"],
+    password: ENV["MAILGUN_SMTP_PASSWORD"],
     domain: ENV["MAILGUN_DOMAIN"],
+    authentication: :plain,
   }
+  config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
