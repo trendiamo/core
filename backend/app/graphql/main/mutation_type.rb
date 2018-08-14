49ca2d05cdd37879925c70119d3201b6f159d5ec
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-08-14 20:40:23 +0100

    Minor change to updateBrand mutation

--- backend/app/graphql/main/mutation_type.rb
+++ backend/app/graphql/main/mutation_type.rb
@@ -120,7 +120,7 @@ Main::MutationType = GraphQL::ObjectType.define do # rubocop:disable Metrics/Blo
       @brand = Brand.find(args[:brand].id)
       authorize(@brand)
       @brand.update(brand_args)
-      if @brand.is_complete && ShopifyAPI::SmartCollection.find(@brand.shopify_collection_id).nil?
+      if @brand.is_complete && @brand.shopify_collection_id.nil?
         @create_collection = Shopify::CreateCollection.new(@brand)
         @create_collection.perform
       end
