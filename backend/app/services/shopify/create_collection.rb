64adaa0fe7a0e1c1b686039f4634be166aa1175a
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-08-16 13:30:51 +0100

    Add metafield data to collection

--- backend/app/services/shopify/create_collection.rb
+++ backend/app/services/shopify/create_collection.rb
@@ -15,6 +15,10 @@ module Shopify
     def add_shopify_collection
       @shopify_collection = ShopifyAPI::SmartCollection.new(shopify_collection_attrs)
       @shopify_collection.save
+      shopify_collection_metafields.each do |metafield|
+        @shopify_collection.add_metafield(metafield)
+      end
+      @shopify_collection.save
     end
 
     def save_shopify_collection_id
@@ -30,9 +34,64 @@ module Shopify
         sort_order: "best-selling",
         body_html: brand.short_description + "\n\n<p>###</p>\n" + brand.long_description,
         rules: [{ column: "vendor", relation: "equals", condition: slug }],
-        published: false,
+        published_scope: "global",
         image: { src: brand.logo_url },
       }
+    end
+
+    def shopify_collection_metafields
+      video_url = brand.header_content_video
+      video_id = video_url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/)[7]
+      [
+        ShopifyAPI::Metafield.new(
+          {
+            namespace: "custom_fields",
+            value: video_id,
+            key: "about_video_url",
+            value_type: "string",
+          }
+        ),
+        ShopifyAPI::Metafield.new(
+          {
+            namespace: "custom_fields",
+            value: brand.header_content_photo,
+            key: "about_image_url",
+            value_type: "string",
+          }
+        ),
+        ShopifyAPI::Metafield.new(
+          {
+            namespace: "custom_fields",
+            value: brand.legal_name,
+            key: "legal_business_name",
+            value_type: "string",
+          }
+        ),
+        ShopifyAPI::Metafield.new(
+          {
+            namespace: "custom_fields",
+            value: brand.legal_address_city + ", " + brand.legal_address_country,
+            key: "business_location",
+            value_type: "string",
+          }
+        ),
+        ShopifyAPI::Metafield.new(
+          {
+            namespace: "custom_fields",
+            value: brand.legal_address_city + ", " + brand.legal_address_country,
+            key: "business_location",
+            value_type: "string",
+          }
+        ),
+        ShopifyAPI::Metafield.new(
+          {
+            namespace: "custom_fields",
+            value: brand.eu_shipping_time,
+            key: "shipping_timeframe",
+            value_type: "string",
+          }
+        ),
+      ]
     end
 
     def slug
