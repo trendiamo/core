80d3784709ceca35b122848f0411df2b343af107
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-08-20 13:02:06 +0100

    Fix email deliver in product-manage

--- backend/app/controllers/csv_controller.rb
+++ backend/app/controllers/csv_controller.rb
@@ -3,5 +3,6 @@ class CsvController < ApplicationController
     @user = User.find_by(email: request.headers["HTTP_X_USER_EMAIL"])
     @file = params[:file]
     email = ProductUploadMailer.send_csv(@user, @file)
+    email.deliver_now
   end
 end
