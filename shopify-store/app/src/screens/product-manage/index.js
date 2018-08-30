5c054cc6f73da141a8f78d5b8945ec522c3fdc7e
Daniel Bugge <danielbugge96@gmail.com>
2018-08-30 14:10:54 +0100

    merged

--- shopify-store/app/src/screens/product-manage/index.js
+++ shopify-store/app/src/screens/product-manage/index.js
@@ -7,8 +7,9 @@ import Loader from 'shared/loader'
 import ProductsList from './products-list'
 import React from 'react'
 import { branch, compose, renderNothing, withHandlers, withProps, withState } from 'recompose'
+import { checkAuthError, treatAuthError } from 'auth/utils'
 
-const ProductManage = ({ brand, isLoading, setInfoValue }) => (
+const ProductManage = ({ brand, isLoading, onFileUpload, file, fileSubmit, alert }) => (
   <React.Fragment>
     <Helmet>
       <title>{'My Products'}</title>
@@ -20,40 +21,53 @@ const ProductManage = ({ brand, isLoading, setInfoValue }) => (
       <div className="container container--tiny" style={{ textAlign: 'center' }}>
         {' '}
         <p>
-          {'Upload a CSV file with your products to get started!'}
+          {'Upload a file containing your products to get started!'}
           <br />
-          {'You can download the template file here.'}
+          {'CSV or Excel files are prefered.'}
         </p>
-        <div className="o-layout">
-          <Loader isLoading={isLoading} />
-          <div className="o-layout__item u-1/1 u-1/2@tab">
-            <a
-              className="c-btn"
-              download="delivery-logo-postmates.png"
-              href={'https://s3.eu-central-1.amazonaws.com/trnd-assets/products_template.csv'}
-              style={{ backgroundColor: '#6B8BD2', width: '100%' }}
-              target="_blank"
-            >
-              {'DOWNLOAD TEMPLATE CSV'}
-            </a>
-          </div>
-        </div>
         <div className="account__form-buttons">
           <div className="o-layout">
             <div className="o-layout__item u-1/2 u-1/4@tab">
+              <Loader isLoading={isLoading} />
               <a className="c-btn c-btn--light account__form-submit" href="/" style={{ width: '100%' }}>
                 {'FINISH'}
               </a>
             </div>
             <div className="o-layout__item u-1/2 u-1/4@tab">
-              <input hidden id="fileUpload" onChange={setInfoValue} type="file" />
+              <input hidden id="setFile" onChange={onFileUpload} type="file" />
               <label
-                className="c-btn c-btn--primary account__form-submit"
-                htmlFor="fileUpload"
-                style={{ width: '100%' }}
+                className="c-btn c-btn--primary"
+                htmlFor="setFile"
+                style={{ backgroundColor: '#6B8BD2', width: '100%' }}
               >
                 {'UPLOAD'}
               </label>
+            </div>
+          </div>
+          <div className="account__form-buttons">
+            <div className="o-layout">
+              <div className="o-layout__item u-1/1 u-1/2@tab">
+                <input
+                  hidden
+                  id="fileSubmit"
+                  onClick={fileSubmit}
+                  style={{ marginTop: '5px', width: '100%' }}
+                  type="submit"
+                />
+                <label
+                  className="c-btn c-btn--primary account__form-submit"
+                  htmlFor="fileSubmit"
+                  style={{ marginTop: '5px', width: '100%' }}
+                >
+                  {'Submit to Tredniamo'}
+                </label>
+                <p hidden={!file} style={{ textAlign: 'left' }}>
+                  {`${file.name}`}
+                </p>
+                <p hidden={!alert} style={{ color: `${alert.color}`, textAlign: 'left' }}>
+                  {`${alert.text}`}
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -76,45 +90,52 @@ const me = gql`
 export default compose(
   graphql(me, { options: { fetchPolicy: 'network-only' } }),
   branch(({ data }) => data && (data.loading || data.error), renderNothing),
+  branch(checkAuthError, treatAuthError),
   withProps(({ data }) => ({
     brand: data.me.brand,
   })),
   withState('csv', 'setCsv', ''),
+  withState('file', 'setFile', false),
+  withState('alert', 'setAlert', false),
   withState('isLoading', 'setIsLoading', false),
   withHandlers({
-    setInfoValue: ({ setCsv, setIsLoading }) => event => {
+    fileSubmit: ({ file, setIsLoading, setFile, setAlert }) => event => {
+      event.preventDefault()
+      if (file == false) {
+        setAlert({
+          color: '#D12328',
+          text: 'You need to upload a file first!',
+        })
+      } else {
+        let formData = new FormData()
+        formData.append('file_name', 'uploadedCsv')
+        formData.append('file', file)
+        let options = {
+          body: formData,
+          headers: {
+            'X-USER-EMAIL': localStorage.getItem('authEmail'),
+            'X-USER-TOKEN': localStorage.getItem('authToken'),
+          },
+          method: 'POST',
+        }
+        setIsLoading(true)
+        fetch(`https://${process.env.API_ENDPOINT}/api/v1/csv`, options).then(response => {
+          if (response.ok == true) {
+            setIsLoading(false)
+            setFile(false)
+            setAlert({
+              color: '#3F964D',
+              text: 'Successfully sent to trendiamo, please check your email for more details!',
+            })
+          }
+        })
+      }
+    },
+    onFileUpload: ({ setFile, setAlert }) => event => {
+      setAlert(false)
       event.preventDefault()
       const file = event.target.files[0]
-      setCsv(file)
-      if (!confirm(`Upload ${file.name} to Trendiamo?`)) {
-        return
-      }
-      if (file.size / 1000000 >= 2) {
-        alert('Please upload a csv file!')
-        return
-      }
-      if (file.type !== 'text/csv') {
-        alert('Please upload a csv file!')
-        return
-      }
-      let formData = new FormData()
-      formData.append('file_name', 'uploadedCsv')
-      formData.append('file', file)
-      let options = {
-        body: formData,
-        headers: {
-          'X-USER-EMAIL': localStorage.getItem('authEmail'),
-          'X-USER-TOKEN': localStorage.getItem('authToken'),
-        },
-        method: 'POST',
-      }
-      setIsLoading(true)
-      fetch(`https://${process.env.API_ENDPOINT}/api/v1/csv`, options).then(response => {
-        if (response.ok == true) {
-          setIsLoading(false)
-          alert('Successfully uploaded the file!')
-        }
-      })
+      setFile(file)
     },
   })
 )(ProductManage)
