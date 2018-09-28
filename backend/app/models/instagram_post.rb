d99d5ccbef3362f2f342bef7d720cf14fb45ce92
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-09-28 14:58:52 +0100

    Add validations to Exposition/Influencer/InstagramPost/Video

--- backend/app/models/instagram_post.rb
+++ backend/app/models/instagram_post.rb
@@ -1,3 +1,5 @@
 class InstagramPost < ApplicationRecord
   has_and_belongs_to_many :expositions
+
+  validates :url, presence: true, uniqueness: true
 end
--- backend/app/models/video.rb
+++ backend/app/models/video.rb
@@ -1,3 +1,5 @@
 class Video < ApplicationRecord
   has_and_belongs_to_many :expositions
+  
+  validates :video_url, presence: true
 end
