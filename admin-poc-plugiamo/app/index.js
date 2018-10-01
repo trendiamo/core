ddf692b24c5e0b13ec630ad85d5dbf6a8c41fca8
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-01 14:42:19 +0100

    Adapt backend's graphql schema to GraphCMS's

--- admin-poc-plugiamo/app/index.js
+++ admin-poc-plugiamo/app/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { compose, lifecycle, withState } from "recompose";
-import { Admin, Resource, GET_LIST } from "react-admin";
-import { ExpositionsList } from "./expositions";
-import buildOpenCrudProvider, { buildQuery } from "ra-data-opencrud";
-import Hello from "./hello";
-import Home from "./home";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import React from 'react'
+import { compose, lifecycle, withState } from 'recompose'
+import { Admin, GET_LIST, Resource } from 'react-admin'
+import { ExpositionsList } from './expositions'
+import buildOpenCrudProvider, { buildQuery } from 'ra-data-opencrud'
+import Hello from './hello'
+import Home from './home'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 // import * as auth0 from 'auth0-js'
 
 // var authClient = new auth0.WebAuth({
@@ -20,34 +20,33 @@ const RouterComponent = () => (
       <Route component={Hello} path="/hello" />
     </div>
   </Router>
-);
+)
 
 const App = ({ dataProvider }) => (
   <div>
-    {!dataProvider && <div>{"Loading"}</div>}
+    {!dataProvider && <div>{'Loading'}</div>}
     {dataProvider && (
       <Admin dataProvider={dataProvider.dataProvider}>
-        <Resource name="Exposition" list={ExpositionsList} />
+        <Resource list={ExpositionsList} name="Exposition" />
       </Admin>
     )}
   </div>
-);
+)
 
 export default compose(
-  withState("dataProvider", "setDataProvider", null),
+  withState('dataProvider', 'setDataProvider', null),
   lifecycle({
     componentDidMount() {
-      const { setDataProvider } = this.props;
+      const { setDataProvider } = this.props
       buildOpenCrudProvider({
         clientOptions: {
-          uri:
-            "https://api-euwest.graphcms.com/v1/cjldl8gtb00qs01ciijssz1zr/master"
-        }
+          uri: 'https://b550d10d.ngrok.io/graphql',
+        },
       })
         .then(dataProvider => {
-          setDataProvider({ dataProvider });
+          setDataProvider({ dataProvider })
         })
-        .catch(reason => console.log(reason));
-    }
+        .catch(reason => console.log(reason))
+    },
   })
-)(App);
+)(App)
---
+++ backend/app/graphql/types/aggregate_type.rb
@@ -0,0 +1,6 @@
+Types::AggregateType = GraphQL::ObjectType.define do
+  name 'aggregateExposition'
+  field :count, !types.Int do
+    resolve ->(obj, _args, _ctx) { obj.nodes.count }
+  end
+end
