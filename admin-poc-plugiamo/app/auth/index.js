d1f3874dafd7281533e0b9dba49afbc908249127
Daniel Bugge <danielbugge96@gmail.com>
2018-10-04 15:40:25 +0100

    Added Authorization type and styled login form

--- admin-poc-plugiamo/app/auth/index.js
+++ admin-poc-plugiamo/app/auth/index.js
@@ -1,9 +1,16 @@
 import { apiSignOut } from './utils'
-import { AUTH_LOGOUT } from 'react-admin'
+import { AUTH_CHECK, AUTH_GET_PERMISSIONS, AUTH_LOGOUT } from 'react-admin'
 
 const authProvider = type => {
   if (type === AUTH_LOGOUT) {
     apiSignOut()
+  }
+  if (type === AUTH_CHECK) {
+    return localStorage.getItem('authToken') && localStorage.getItem('authEmail') ? Promise.resolve() : Promise.reject()
+  }
+  if (type === AUTH_GET_PERMISSIONS) {
+    const role = localStorage.getItem('authRole')
+    return role ? Promise.resolve(role) : Promise.reject
   }
 }
 
--- admin-poc-plugiamo/app/login/index.js
+++ admin-poc-plugiamo/app/login/index.js
@@ -1,26 +1,100 @@
 import { apiSignIn } from '../auth/utils'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import Checkbox from '@material-ui/core/Checkbox'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import FormControl from '@material-ui/core/FormControl'
-import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import LockIcon from '@material-ui/icons/LockOutlined'
+import Paper from '@material-ui/core/Paper'
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import withStyles from '@material-ui/core/styles/withStyles'
 import { compose, withHandlers, withState } from 'recompose'
 
-const Login = ({ loginForm, loginSubmit, setLoginValue }) => (
-  <form acceptCharset="UTF-8" onSubmit={loginSubmit}>
-    <Input
-      autoFocus
-      name="email"
-      onChange={setLoginValue}
-      required
-      type="text"
-      value={loginForm.email}
-    />
-    <Input name="password" onChange={setLoginValue} required type="password" value={loginForm.password} />
-    <Input type="submit" value="Login" />
-  </form>
+const styles = theme => ({
+  avatar: {
+    backgroundColor: theme.palette.secondary.main,
+    margin: theme.spacing.unit,
+  },
+  form: {
+    marginTop: theme.spacing.unit,
+    width: '100%',
+  },
+  layout: {
+    display: 'block',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      width: 400,
+    },
+  },
+  paper: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing.unit * 8,
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+})
+
+const CustomLoginJSX = ({ loginForm, loginSubmit, setLoginValue, classes }) => (
+  <React.Fragment>
+    <CssBaseline />
+    <main className={classes.layout}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockIcon />
+        </Avatar>
+          <Typography variant="headline">{'Sign in'}</Typography>
+          <form className={classes.form} onSubmit={loginSubmit}>
+          <FormControl fullWidth margin="normal" required>
+            <InputLabel htmlFor="email">{'Email Address'}</InputLabel>
+            <Input
+              autoComplete="email"
+              autoFocus
+              id="email"
+              name="email"
+              onChange={setLoginValue}
+              value={loginForm.email}/>
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">{'Password'}</InputLabel>
+            <Input
+              name="password"
+              type="password"
+              onChange={setLoginValue}
+              required
+              id="password"
+              autoComplete="current-password"
+              value={loginForm.password}
+            />
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="raised"
+            color="primary"
+            className={classes.submit}
+          >{'Sign in'}</Button>
+        </form>
+      </Paper>
+    </main>
+  </React.Fragment>
 )
 
-export default compose(
+const CustomLogin = compose(
   withState('loginForm', 'setLoginForm', { email: '', password: '' }),
   withState('errors', 'setErrors', null),
   withHandlers({
@@ -32,4 +106,6 @@ export default compose(
     setLoginValue: ({ loginForm, setLoginForm }) => event =>
       setLoginForm({ ...loginForm, [event.target.name]: event.target.value }),
   })
-)(Login)
+)(CustomLoginJSX)
+
+export default withStyles(styles)(CustomLogin)
