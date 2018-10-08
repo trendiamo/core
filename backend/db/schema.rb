66fc5cc44f1137b5cd6cd917f4efaf3ec1548758
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-08 10:52:54 +0100

    Add exposition_hostname to User model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928110231) do
+ActiveRecord::Schema.define(version: 20181008095021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,7 +159,6 @@ ActiveRecord::Schema.define(version: 20180928110231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "media_items"
-    t.string "external_product_url"
     t.integer "anonymous_likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
@@ -196,6 +195,7 @@ ActiveRecord::Schema.define(version: 20180928110231) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "subscribed_to_newsletter", default: false, null: false
+    t.string "exposition_hostname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
