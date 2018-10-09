523c55052ec37619ea457ba38238545564c4436d
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-09 15:59:58 +0100

    Fix Update Exposition bug

--- backend/app/graphql/fields/update_exposition_field.rb
+++ backend/app/graphql/fields/update_exposition_field.rb
@@ -4,7 +4,13 @@ Fields::UpdateExpositionField = GraphQL::Field.define do
   argument :data, !Types::ExpositionInputType
   resolve Resolver.new ->(obj, args, ctx) {
     use(Plugins::Pundit, obj: obj, args: args, ctx: ctx)
-    authorize(:nil)
+    variables = { id: ctx[:variables][:where][:id], domain: args[:domain] }
+    @graphcms_exposition = ExecuteQuery.execute(ExpositionQuery, variables, :exposition)
+
+    Exposition = Struct.new(*@graphcms_exposition.to_h.symbolize_keys.keys)
+    exposition_record = Exposition.new(*@graphcms_exposition.to_h.values)
+    authorize(exposition_record)
+
     id = ctx[:variables][:where][:id]
     variables = { id: id, domain: current_user.exposition_hostname, description: args[:data][:description],
                   ctaUrl: args[:data][:ctaUrl], ctaText: args[:data][:ctaText], status: args[:data][:status], }
--- backend/app/policies/nil_policy.rb
+++ backend/app/policies/nil_policy.rb
@@ -23,10 +23,6 @@ class NilPolicy < ApplicationPolicy
     user
   end
 
-  def update_exposition?
-    user
-  end
-
   def delete_exposition?
     user
   end
