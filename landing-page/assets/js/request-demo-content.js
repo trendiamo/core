febadb89950a1a0876fb4eab7896389141c786d9
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-11 16:46:04 +0100

    Add Landing page

---
+++ landing-page/assets/js/request-demo-content.js
@@ -0,0 +1,9 @@
+const requestDemoContent = `
+  <div>
+    <h3>Get your demo 🚀</h3>
+    <p>Know why our tool is going to help you to engage with your customers and increase your sales.</p>
+    <div class="request-demo-form"></div>
+  </div>
+`
+
+export default requestDemoContent
