933a28b86498973c50a1c204908eafd75cd4f945
Daniel Bugge <danielbugge96@gmail.com>
2018-10-15 18:07:07 +0100

    Remove admin-poc-plugiamo

--- admin-poc-plugiamo/app/layout/index.js
+++
@@ -1,7 +0,0 @@
-import CustomAppBar from './appbar'
-import { Layout } from 'react-admin'
-import React from 'react'
-
-const CustomLayout = props => <Layout {...props} appBar={CustomAppBar} title="customLayout" />
-
-export default CustomLayout
--- admin-poc-plugiamo/index.js
+++
@@ -1,9 +0,0 @@
-import App from './app'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const RootContainer = document.getElementById('root')
-
-const RootComponent = () => <App />
-
-ReactDOM.render(<RootComponent />, RootContainer)
