41bb640ca8482a2fcbde80f3453bc56c274c01d3
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-16 15:11:55 +0100

    Add website_ref field to User model and validations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008095021) do
+ActiveRecord::Schema.define(version: 20181016140028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(version: 20181008095021) do
     t.string "unconfirmed_email"
     t.boolean "subscribed_to_newsletter", default: false, null: false
     t.string "exposition_hostname"
+    t.string "website_ref"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
