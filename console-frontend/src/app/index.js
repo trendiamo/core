fd70ae62f62905f88e037a970ebfc5ca20fc9824
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-17 15:24:55 +0100

    Fix jss issue in prod

--- console-frontend/src/app/index.js
+++ console-frontend/src/app/index.js
@@ -1,42 +1,32 @@
 import authProvider from './auth'
 import buildOpenCrudProvider from 'ra-data-opencrud'
-import createHistory from 'history/createBrowserHistory'
-import customLayout from './layout'
+import CustomLayout from './layout'
 import customLoginPage from './screens/login'
 import customLogoutButton from './screens/logout'
 import customRoutes from './ext/custom-routes.js'
-import { HttpLink } from 'apollo-link-http'
 import React from 'react'
-import { setContext } from 'apollo-link-context'
 import { Admin, Resource } from 'react-admin'
-import { compose, lifecycle, withState } from 'recompose'
+import { branch, compose, lifecycle, renderNothing, withState } from 'recompose'
 import { ExpositionsCreate, ExpositionsEdit, ExpositionShow, ExpositionsList } from './resources/expositions'
 
-const customHistory = createHistory()
-
-const App = ({ dataProvider }) => (
-  <div>
-    {!dataProvider && <div>{'Loading'}</div>}
-    {dataProvider && (
-      <Admin
-        appLayout={customLayout}
-        authProvider={authProvider}
-        customRoutes={customRoutes}
-        dataProvider={dataProvider.dataProvider}
-        history={customHistory}
-        loginPage={customLoginPage}
-        logoutButton={customLogoutButton}
-      >
-        <Resource
-          create={ExpositionsCreate}
-          edit={ExpositionsEdit}
-          list={ExpositionsList}
-          name="Exposition"
-          show={ExpositionShow}
-        />
-      </Admin>
-    )}
-  </div>
+const App = ({ dataProvider, history }) => (
+  <Admin
+    appLayout={CustomLayout}
+    authProvider={authProvider}
+    customRoutes={customRoutes}
+    dataProvider={dataProvider.dataProvider}
+    history={history}
+    loginPage={customLoginPage}
+    logoutButton={customLogoutButton}
+  >
+    <Resource
+      create={ExpositionsCreate}
+      edit={ExpositionsEdit}
+      list={ExpositionsList}
+      name="Exposition"
+      show={ExpositionShow}
+    />
+  </Admin>
 )
 
 export default compose(
@@ -44,24 +34,11 @@ export default compose(
   withState('isLoggedIn', 'setIsloggedIn', null),
   lifecycle({
     componentDidMount() {
-      const { setDataProvider } = this.props
-      const uriPlugAdmin = `https://${process.env.REACT_APP_API_ENDPOINT}/graphql`
-      const authLink = setContext((_, { headers }) => ({
-        headers: {
-          ...headers,
-          'X-USER-EMAIL': localStorage.getItem('authEmail'),
-          'X-USER-TOKEN': localStorage.getItem('authToken'),
-        },
-      }))
-      buildOpenCrudProvider({
-        clientOptions: {
-          link: authLink.concat(new HttpLink({ uri: uriPlugAdmin })),
-        },
-      })
-        .then(dataProvider => {
-          setDataProvider({ dataProvider })
-        })
-        .catch(reason => console.log(reason))
+      const { authLink, setDataProvider } = this.props
+      buildOpenCrudProvider({ clientOptions: { link: authLink } })
+        .then(dataProvider => setDataProvider({ dataProvider }))
+        .catch(console.error)
     },
-  })
+  }),
+  branch(({ dataProvider }) => !dataProvider, renderNothing)
 )(App)
