b8e8e8ca8010bdb64f2097e634a1b8f2f59348e3
Daniel Bugge <danielbugge96@gmail.com>
2018-10-17 14:13:01 +0100

    Add custom menu component

--- console-frontend/src/app/layout/index.js
+++ console-frontend/src/app/layout/index.js
@@ -1,12 +1,13 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
 import CustomAppBar from './appbar'
+import CustomMenu from './menu'
 import { Layout } from 'react-admin'
 import React from 'react'
 
 const CustomLayout = props => (
   <React.Fragment>
     <CssBaseline />
-    <Layout {...props} appBar={CustomAppBar} title="customLayout" />
+    <Layout {...props} appBar={CustomAppBar} menu={CustomMenu} title="customLayout" />
   </React.Fragment>
 )
 
