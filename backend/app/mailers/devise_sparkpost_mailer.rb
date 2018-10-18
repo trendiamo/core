cc392991f75f7e6cf2cf5bb7fa7e61d8e0ea2f51
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-18 17:36:44 +0100

    Improve handling of emails

--- backend/app/mailers/devise_sparkpost_mailer.rb
+++ backend/app/mailers/devise_sparkpost_mailer.rb
@@ -2,27 +2,27 @@ class DeviseSparkpostMailer < Devise::Mailer
   default from: "Trendiamo Team <no-reply@trendiamo.com>"
 
   def reset_password_instructions(record, token, opts={})
-    if ENV["SPARKPOST_API_KEY"]
-      # https://developers.sparkpost.com/api/template-language/#header-links
-      sparkpost_data = {
-        substitution_data: {
-          user_identifier: record.email,
-          dynamic_html: {
-            change_password_link: %(<a href="#{users_password_edit_url(record, reset_password_token: token)}">Change my password</a>)
-          },
+    # https://developers.sparkpost.com/api/template-language/#header-links
+    sparkpost_data = {
+      substitution_data: {
+        user_identifier: record.email,
+        dynamic_html: {
+          change_password_link: %(<a href="#{users_password_edit_url(record, reset_password_token: token)}">Change my password</a>)
         },
-        template_id: "reset-password-instructions",
-      }
-      sp_mail(to: record.email, sparkpost_data: sparkpost_data)
-    else
-      super
-    end
+      },
+      template_id: "reset-password-instructions",
+    }
+    mail(to: record.email, sparkpost_data: sparkpost_data)
   end
 
   private
 
-  # https://github.com/the-refinery/sparkpost_rails#using-sparkpost-templates
-  def sp_mail(*args)
-    mail(*args) { |f| f.text { render plain: "" } }
+  def mail(*args)
+    if ENV["SPARKPOST_API_KEY"]
+      # https://github.com/the-refinery/sparkpost_rails#using-sparkpost-templates
+      mail(*args) { |f| f.text { render plain: "" } }
+    else
+      Rails.logger.info("Deliver email: #{args.join}")
+    end
   end
 end
