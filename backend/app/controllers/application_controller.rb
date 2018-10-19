729670d9b8e0c9384380e585ceef57e74e896c2e
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-19 15:17:45 +0100

    Set current tenant in application controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include Pundit
+  set_current_tenant_through_filter
+  before_action :current_tenant
+
+  def current_tenant
+    set_current_tenant(current_user.account) if current_user
+  end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable
 
+  acts_as_tenant
+
   has_many :authentication_tokens, class_name: "AuthToken", dependent: :destroy
   belongs_to :account
 
   validates :website_ref, uniqueness: true
-  acts_as_tenant
 end
