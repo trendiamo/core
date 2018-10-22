cd2f9c93d6bf38eb10fc250238c17f1924b074fe
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-22 17:23:08 +0100

    Add Websites controller and routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
         post "/users/sign_in", to: "users/sessions#create"
         delete "/users/sign_out", to: "users/sessions#destroy"
       end
+      resources :websites, only: %i[show update]
     end
     get "s3/sign", to: "s3#sign"
     # post :stripe, to: 'stripe#webhook'
