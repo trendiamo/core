debddf8075a9d4ab489f28c2b0343669fe17f374
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-23 10:40:22 +0100

    Render status 403 unauthorized response from backend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::API
   set_current_tenant_through_filter
   before_action :current_tenant
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
   def current_tenant
     set_current_tenant(current_user.account) if current_user
+  end
+
+  def user_not_authorized
+    render json: { error: "You are not authorized to perform this action." }, status: :forbidden
   end
 end
