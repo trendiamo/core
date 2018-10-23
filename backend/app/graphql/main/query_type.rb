64005ce6404729161a00e900ba762e222175969e
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-23 15:39:02 +0100

    Use new graphql approach

--- backend/app/graphql/main/query_type.rb
+++ backend/app/graphql/main/query_type.rb
@@ -2,37 +2,5 @@ Main::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
   field :me, Fields::MeField
-
-  field :expositions, Fields::ExpositionsField
-  field :exposition, Fields::ExpositionField
-  connection :expositionsConnection, Connections::ExpositionsConnection do
-    resolve ->(obj, args, ctx) {
-      use(Plugins::Pundit, obj: obj, args: args, ctx: ctx)
-      authorize(:nil)
-      variables = { domains: [current_user.exposition_hostname] }
-      ExecuteQuery.execute(ExpositionsQuery, variables, :expositions)
-    }
-  end
-
-  field :websites, Fields::WebsitesField
   field :website, Fields::WebsiteField
-  connection :websitesConnection, Connections::WebsitesConnection do
-    resolve ->(_obj, _args, _ctx) {
-      use(Plugins::Pundit, obj: obj, args: args, ctx: ctx)
-      authorize(:nil)
-      variables = { ids: [current_user.website_ref] }
-      ExecuteQuery.execute(WebsitesQuery, variables, :websites)
-    }
-  end
-
-  field :hostnames, Fields::HostnamesField
-  field :hostname, Fields::HostnameField
-  connection :hostnamesConnection, Connections::HostnamesConnection do
-    resolve ->(_obj, _args, _ctx) {
-      use(Plugins::Pundit, obj: obj, args: args, ctx: ctx)
-      authorize(:nil)
-      variables = { id: current_user.website_ref }
-      ExecuteQuery.execute(HostnamesQuery, variables, :hostnames)
-    }
-  end
 end
--- backend/app/graphql/types/exposition_status_type.rb
+++
@@ -1,7 +0,0 @@
-Types::ExpositionStatusType = GraphQL::EnumType.define do
-  name "ExpositionStatus"
-
-  value("DRAFT")
-  value("PUBLISHED")
-  value("ARCHIVED")
-end
