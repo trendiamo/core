80465b3871806960c63edeac0131c08d6f54c693
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-23 17:25:59 +0100

    Make hostname unique validation across accounts

--- backend/test/models/website_test.rb
+++ backend/test/models/website_test.rb
@@ -2,12 +2,25 @@ require "test_helper"
 
 class WebsiteTest < ActiveSupport::TestCase
   test "hostnames cannot be repeated" do
+    ActsAsTenant.default_tenant = Account.create!
     create(:website, hostnames: %w[a.com b.com])
     create(:website, hostnames: %w[c.com])
 
-    @website = build(:website, hostnames: "b.com")
+    @website = build(:website, hostnames: %w[b.com])
 
     assert !@website.valid?
-    assert @website.errors[:hostnames].include?("can't be blank")
+    assert @website.errors[:hostnames].include?("already exists")
+  end
+
+  test "hostnames cannot be repeated even across accounts" do
+    ActsAsTenant.with_tenant(Account.create!) do
+      create(:website, hostnames: %w[other.com])
+    end
+    ActsAsTenant.default_tenant = Account.create!
+
+    @website = build(:website, hostnames: %w[other.com])
+
+    assert !@website.valid?
+    assert @website.errors[:hostnames].include?("already exists")
   end
 end
--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -1,28 +1,14 @@
 DatabaseCleaner.strategy = :transaction
 
 class ActiveSupport::TestCase
-  include FactoryBot::Syntax::Methods # TODO: move this into the helper
+  include FactoryBot::Syntax::Methods
 
   setup do
     DatabaseCleaner.start
-    ActsAsTenant.default_tenant = Account.create!
   end
 
   teardown do
+    ActsAsTenant.default_tenant = nil
     DatabaseCleaner.clean
   end
 end
-
-# ActiveSupport::TestCase.add_setup_hook { DatabaseCleaner.start }
-# ActiveSupport::TestCase.add_teardown_hook { DatabaseCleaner.clean }
-
-# class Minitest::Spec
-#   before :each do
-#     DatabaseCleaner.start
-#     ActsAsTenant.default_tenant = Account.create!
-#   end
-#
-#   after :each do
-#     DatabaseCleaner.clean
-#   end
-# end
