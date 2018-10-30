bb5fbe983888e4f9ceefe55a2ba55718e9b1c5a1
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-30 11:58:32 +0000

    Use jsx-a11y rules, since react uses them already

--- console-frontend/src/shared/logotype.js
+++ console-frontend/src/shared/logotype.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Logotype = ({ ...props }) => (
   <LogotypeContainer>
-    <img src="/img/trendiamo-logo.svg" {...props} />
+    <img alt="" src="/img/trendiamo-logo.svg" {...props} />
   </LogotypeContainer>
 )
 
