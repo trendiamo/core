06118a204f77d327fffb8ae81ea08aa63ff7079b
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-10-30 15:32:58 +0000

    Add pristine concept to account page

--- console-frontend/src/app/screens/account/edit-user.js
+++ console-frontend/src/app/screens/account/edit-user.js
@@ -3,17 +3,20 @@ import Button from '@material-ui/core/Button'
 import FormControl from '@material-ui/core/FormControl'
 import Input from '@material-ui/core/Input'
 import InputLabel from '@material-ui/core/InputLabel'
+import { isEqual } from 'lodash'
 import Label from 'shared/label'
 import Link from 'shared/link'
 import Notification from 'shared/notification'
 import PictureUploader from 'shared/picture-uploader'
+import { Prompt } from 'react-router'
 import React from 'react'
 import routes from 'app/routes'
 import { apiMe, apiMeUpdate } from 'utils'
-import { compose, lifecycle, withHandlers, withState } from 'recompose'
+import { compose, lifecycle, withHandlers, withProps, withState } from 'recompose'
 
-const EditUser = ({ info, onSubmit, userForm, setFieldValue, setProfilePicUrl }) => (
+const EditUser = ({ info, isPristine, onSubmit, userForm, setFieldValue, setProfilePicUrl }) => (
   <form onSubmit={onSubmit}>
+    <Prompt message="You have unsaved changes, are you sure you want to leave?" when={!isPristine} />
     <Label>{'Picture'}</Label>
     <PictureUploader onChange={setProfilePicUrl} type="users-profile-pics" value={userForm.profilePicUrl} />
     <Notification data={info} />
@@ -45,18 +48,23 @@ const EditUser = ({ info, onSubmit, userForm, setFieldValue, setProfilePicUrl })
 )
 
 export default compose(
-  withState('userForm', 'setUserForm', {
+  withState('initialUserForm', 'setInitialUserForm', {
     email: '',
     firstName: '',
     lastName: '',
     profilePicUrl: '',
   }),
+  withState('userForm', 'setUserForm', ({ initialUserForm }) => initialUserForm),
   withState('info', 'setInfo', null),
+  withProps(({ userForm, initialUserForm }) => ({
+    isPristine: isEqual(userForm, initialUserForm),
+  })),
   withHandlers({
-    onSubmit: ({ userForm, setInfo }) => async event => {
+    onSubmit: ({ userForm, setInfo, setInitialUserForm }) => async event => {
       event.preventDefault()
       const user = await apiMeUpdate({ user: userForm }, setInfo)
       auth.setUser(user)
+      setInitialUserForm(userForm)
       return user
     },
     setFieldValue: ({ userForm, setUserForm }) => event => {
@@ -68,15 +76,17 @@ export default compose(
   }),
   lifecycle({
     async componentDidMount() {
-      const { setUserForm, setInfo } = this.props
+      const { setUserForm, setInfo, setInitialUserForm } = this.props
       const user = await apiMe(setInfo)
-      auth.setUser(user)
-      setUserForm({
+      const userObject = {
         email: user.email || '',
         firstName: user.firstName || '',
         lastName: user.lastName || '',
         profilePicUrl: user.profilePicUrl || '',
-      })
+      }
+      auth.setUser(user)
+      setInitialUserForm(userObject)
+      setUserForm(userObject)
     },
   })
 )(EditUser)
