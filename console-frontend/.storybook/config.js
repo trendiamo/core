1dad0327857bd8133cd0b4b77db99df4969fb423
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-30 11:02:23 +0000

    Add story for picture uploader

--- console-frontend/.storybook/config.js
+++ console-frontend/.storybook/config.js
@@ -1,7 +1,18 @@
+import { addDecorator } from '@storybook/react'
 import { configure } from '@storybook/react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import React from 'react'
+import theme from 'app/theme'
 
-function loadStories() {
-  require('../src/stories')
-}
+const loadStories = () => require('../src/stories')
+
+const StyleDecorator = storyFn => (
+  <React.Fragment>
+    <CssBaseline />
+    <MuiThemeProvider theme={theme}>{storyFn()}</MuiThemeProvider>
+  </React.Fragment>
+)
+addDecorator(StyleDecorator)
 
 configure(loadStories, module)
