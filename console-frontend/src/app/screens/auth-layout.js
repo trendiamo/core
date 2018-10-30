2c01f533731d477e6a3f2283f36193178c9cfa52
Duarte Henriques <duarte.henriques@gmail.com>
2018-10-30 10:00:15 +0000

    minor adjustments

--- console-frontend/src/app/screens/auth-layout.js
+++ console-frontend/src/app/screens/auth-layout.js
@@ -1,7 +1,7 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
-import theme from '../theme'
+import theme from 'app/theme'
 import Typography from '@material-ui/core/Typography'
 import { AuthMain, AuthStyledPaper } from './shared'
 
