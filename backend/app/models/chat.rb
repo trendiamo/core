e00c1a6e076d8e52be8d6fa2491e3ef29778066f
Daniel Bugge <danielbugge96@gmail.com>
2018-10-30 17:55:42 +0000

    Add validations

--- backend/app/models/chat.rb
+++ backend/app/models/chat.rb
@@ -1,3 +1,6 @@
 class Chat < ApplicationRecord
   has_many :first_chat_step, foreign_key: "chat_id", class_name: "ChatStep"
+
+  validates :path, prescence: true
+  validates :title, prescence: true
 end
--- backend/app/models/chat_option.rb
+++ backend/app/models/chat_option.rb
@@ -1,3 +1,12 @@
 class ChatOption < ApplicationRecord
   belongs_to :chat_step
+
+  validates :text, presence: true
+  validate :chat_step_cannot_equal_next
+
+  private
+
+  def chat_step_cannot_equal_next
+    errors.add(:chat_steps, "chat step cannot be the same as destination chat step") if chat_step_id == destinaton_chat_step_id
+  end
 end
