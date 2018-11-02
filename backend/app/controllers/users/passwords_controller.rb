fc2830fecac01c01d22eb8370943b4c217cf3fc2
Duarte Henriques <duarte.henriques@gmail.com>
2018-11-02 10:06:11 +0000

    Fix reset password url in backend redirect

--- backend/app/controllers/users/passwords_controller.rb
+++ backend/app/controllers/users/passwords_controller.rb
@@ -23,7 +23,7 @@ module Users
       set_minimum_password_length
       resource.reset_password_token = params[:reset_password_token]
       query = "?reset_password_token=#{params[:reset_password_token]}"
-      password_form_url = "#{ENV['FRONTEND_BASE_URL']}/password_reset#{query}"
+      password_form_url = "#{ENV['FRONTEND_BASE_URL']}/password-reset#{query}"
       redirect_to password_form_url
     end
 
