62b361371bfe36f9f4faa286644aed2c659d3dae
Duarte Henriques <duarte.henriques@gmail.com>
2018-11-06 19:12:12 +0000

    Remove unused argument

--- backend/app/graphql/fields/website_field.rb
+++ backend/app/graphql/fields/website_field.rb
@@ -1,7 +1,6 @@
 Fields::WebsiteField = GraphQL::Field.define do
   name "website"
   description "Show website"
-  argument :hostname, types.String
   type Types::WebsiteType
   resolve ->(_obj, _args, ctx) {
     ctx[:website]
