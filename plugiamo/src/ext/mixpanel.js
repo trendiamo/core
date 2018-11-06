8329943979a9e8e21b6bdbef2f324b6178bf564a
Duarte Henriques <duarte.henriques@gmail.com>
2018-11-06 17:40:20 +0000

    Add mock mixpanel id

--- plugiamo/src/ext/mixpanel.js
+++ plugiamo/src/ext/mixpanel.js
@@ -1,9 +1,22 @@
 import mixpanel from 'mixpanel-browser'
 import { production } from 'config'
 
+const mockUniqueId = () => {
+  if (!window.__mockUniqueId) {
+    window.__mockUniqueId = Array.from(Array(4))
+      .map(() =>
+        Math.random()
+          .toString(16)
+          .substring(2, 15)
+      )
+      .join('-')
+  }
+  return window.__mockUniqueId
+}
+
 const trndMixpanel = {
   get_distinct_id() {
-    return mixpanel.get_distinct_id()
+    return this.initialized ? mixpanel.get_distinct_id() : mockUniqueId()
   },
   init(mpToken, mpConfig, name) {
     if (mpToken && production) {
