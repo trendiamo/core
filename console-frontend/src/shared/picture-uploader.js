96f4800d72df46b1b93940cd0134866502edf7c8
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-11-12 15:48:58 +0000

    Fix ImageCropper aspect ratio on small screens with black background issue

--- console-frontend/src/shared/picture-uploader.js
+++ console-frontend/src/shared/picture-uploader.js
@@ -180,6 +180,7 @@ const BarebonesPictureUploader = ({
         <HiddenImg alt="" ref={setImagePreviewRef} src={image.preview} />
         <StyledReactCrop
           crop={crop}
+          imageStyle={{ maxHeight: 'initial' }}
           keepSelection
           minHeight={20}
           minWidth={20}
