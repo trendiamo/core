cdf064bc5ecc8b71ec28c20abe6075197b758b03
MaxChu23 <mymaksym@gmail.com>
2018-11-19 12:46:50 +0000

    Add name to Accounts table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181102114548) do
+ActiveRecord::Schema.define(version: 20181119123720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20181102114548) do
   create_table "accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "auth_tokens", force: :cascade do |t|
