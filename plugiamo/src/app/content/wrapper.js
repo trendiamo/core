559131483338295bf51496ece887bd70805ded38
Duarte Henriques <duarte.henriques@gmail.com>
2018-11-19 14:51:56 +0000

    Use flex rather than height

--- plugiamo/src/app/content/wrapper.js
+++ plugiamo/src/app/content/wrapper.js
@@ -5,9 +5,10 @@ const Wrapper = styled.div`
   top: 0;
   left: 0;
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  height: 100%;
+  overflow-y: auto;
   color: #333;
 `
 
