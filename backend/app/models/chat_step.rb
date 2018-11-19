dcd16975273c1a17deeda6d5afb6c7ea5da4141d
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2018-11-19 10:30:14 +0000

    Rename Chat to ScriptedChat; rename foreign key; remove path column

--- backend/app/models/chat_step.rb
+++ backend/app/models/chat_step.rb
@@ -1,6 +1,5 @@
 class ChatStep < ApplicationRecord
-  belongs_to :chat
-  belongs_to :chat_option
+  belongs_to :scripted_chat
   has_many :chat_messages
   has_many :chat_options
   has_many :refering_chat_options, foreign_key: "destinaton_chat_step_id", class_name: "ChatOption"
