aafb59fb66044b6582a74f4f7973a17332627ae7
Duarte Henriques <duarte.henriques@gmail.com>
2018-11-26 13:43:48 +0000

    Remove validation on non-existing field

--- backend/app/models/scripted_chat.rb
+++ backend/app/models/scripted_chat.rb
@@ -1,6 +1,5 @@
 class ScriptedChat < ApplicationRecord
   has_one :first_chat_step, foreign_key: "scripted_chat_id", class_name: "ChatStep"
 
-  validates :path, presence: true
   validates :title, presence: true
 end
