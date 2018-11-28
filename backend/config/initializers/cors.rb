7eec9e856269613fd25374b147fce5c8ee98f3b1
Duarte Henriques <duarte.henriques@gmail.com>
2018-11-28 14:57:52 +0000

    Support cookie-based auth across different domains

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
     origins(*ENV["CORS_ORIGIN"].split(", "))
-    resource "*", headers: :any, methods: %i[get post put patch delete options head]
+    resource "*", headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end if ENV["CORS_ORIGIN"]
 end
