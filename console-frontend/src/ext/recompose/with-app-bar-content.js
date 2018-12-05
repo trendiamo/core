416516ef9381d390425796cd22629f5005cdf777
Duarte Henriques <duarte.henriques@gmail.com>
2018-12-05 15:40:14 +0000

    Set the page title on each page

--- console-frontend/src/ext/recompose/with-app-bar-content.js
+++ console-frontend/src/ext/recompose/with-app-bar-content.js
@@ -4,9 +4,16 @@ import { compose, createSink } from 'recompose'
 import { isEqual } from 'lodash'
 import { withStoreConsumer } from './with-store'
 
+const setPageTitle = appBarContent => {
+  if (!appBarContent.breadcrumbs || !appBarContent.breadcrumbs.length) return
+  const title = appBarContent.breadcrumbs[appBarContent.breadcrumbs.length - 1].text
+  document.title = `${title} - Trendiamo Console`
+}
+
 const Sink = createSink(({ appBarContent, store, setStore, ...props }) => {
   if (typeof appBarContent === 'function') appBarContent = appBarContent(props)
   if (isEqual(appBarContent, store.appBarContent)) return
+  setPageTitle(appBarContent)
   setStore({ ...store, appBarContent })
 })
 
