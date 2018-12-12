f1e2acaa19b89020409b4b640354cbe6107bad68
MaxChu23 <mymaksym@gmail.com>
2018-12-12 15:10:39 +0000

    Fixed AppBar

--- console-frontend/src/app/layout/layout-styles.js
+++ console-frontend/src/app/layout/layout-styles.js
@@ -40,6 +40,7 @@ export const styles = theme => ({
     backgroundColor: theme.customPalette.appBar.main,
     boxShadow: '0 0 4px 0 rgba(0, 0, 0, 0.12)',
     color: '333',
+    position: 'fixed',
     transition: createTransition(theme, ['width', 'margin', 'backgroundColor']),
     width: `calc(100% - ${drawerWidthClosed}px)`,
     zIndex: theme.zIndex.drawer + 1,
