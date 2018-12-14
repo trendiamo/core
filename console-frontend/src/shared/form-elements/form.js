17a43c8a17a8c07812cca110b0190a513f8bd86d
Duarte Henriques <duarte.henriques@gmail.com>
2018-12-14 16:19:30 +0000

    Recover prompt message

--- console-frontend/src/shared/form-elements/form.js
+++ console-frontend/src/shared/form-elements/form.js
@@ -2,10 +2,10 @@ import Notification from 'shared/notification'
 import Prompt from './prompt'
 import React from 'react'
 
-const Form = ({ children, promptMessage, isFormPristine, errors, formRef, ...props }) => (
+const Form = ({ children, isFormPristine, errors, formRef, ...props }) => (
   <React.Fragment>
     <form action="" ref={formRef} {...props}>
-      <Prompt message={promptMessage} when={!isFormPristine} />
+      <Prompt message="You have unsaved changes, are you sure you want to leave?" when={!isFormPristine} />
       <Notification data={errors} />
       {children}
     </form>
