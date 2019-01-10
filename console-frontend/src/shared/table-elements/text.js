797c4a529c2784054c9b6b6c35900bb2af764c5c
Daniel Bugge <danielbugge96@gmail.com>
2019-01-10 15:36:18 +0000

    Revert text cplor in modules

--- console-frontend/src/shared/table-elements/text.js
+++ console-frontend/src/shared/table-elements/text.js
@@ -5,6 +5,7 @@ import { Typography } from '@material-ui/core'
 
 const Text = styled(({ ...props }) => <Typography variant="body1" {...omit(props, ['disabled'])} />)`
   opacity: ${({ disabled }) => disabled && 0.8};
+  color: ${({ disabled }) => (disabled ? 'rgba(0,0,0,.6)' : 'rgba(0, 0, 0, 0.87)')};
 `
 
 export default Text
