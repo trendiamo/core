ea71c728fe0a4ebc41829515b553f7aa80ce6f3f
Duarte Henriques <duarte.henriques@gmail.com>
2019-01-14 16:48:15 +0000

    Remove arrows

--- plugin-base/src/showcase/components/spotlight-content.js
+++ plugin-base/src/showcase/components/spotlight-content.js
@@ -1,10 +1,9 @@
-import Arrow from 'shared/arrow'
 import ProductItem from './product-item'
 import React from 'react'
 import styled from 'styled-components'
-import { animate, TopSlideAnimation } from 'shared/animate'
 import { compose, withProps } from 'recompose'
 import { List } from 'shared/list'
+import { TopSlideAnimation } from 'shared/animate'
 
 const Container = styled.div`
   height: 100%;
@@ -14,20 +13,8 @@ const Container = styled.div`
 const H2 = styled.h2`
   margin: 0;
   font-size: 18px;
+  margin-bottom: 12px;
 `
-
-const AnimatedBlackArrow = animate(
-  styled(Arrow).attrs({
-    color: '#000',
-    lineColor: '#444',
-    width: '150px',
-  })`
-    opacity: ${({ isEntering, isLeaving }) => (isEntering || isLeaving ? 0 : 1)};
-    transform: ${({ isEntering, isLeaving }) =>
-      isEntering ? 'translateX(-200px)' : isLeaving ? 'translateX(600px)' : 'none'};
-    transition: opacity 1s ease, transform 0.6s ease;
-  `
-)
 
 const SpotlightContent = compose(
   withProps(({ id, spotlights }) => ({
@@ -36,7 +23,6 @@ const SpotlightContent = compose(
 )(({ isLeaving, spotlight, callbacks }) => (
   <Container>
     <H2>{spotlight.translation.selectedBy}</H2>
-    <AnimatedBlackArrow delay={250 * 2} isLeaving={isLeaving} />
     <TopSlideAnimation isLeaving={isLeaving} name="content">
       <List>
         {spotlight.productPicks.map(
