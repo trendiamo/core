27de6f11f53d244d82e330607cd129be905929b5
Duarte Henriques <duarte.henriques@gmail.com>
2019-01-14 16:48:15 +0000

    Remove arrows

--- plugiamo/stories/index.stories.js
+++ plugiamo/stories/index.stories.js
@@ -1,22 +1,11 @@
-import Arrow from 'shared/arrow'
-import styled from 'styled-components'
 import Welcome from './welcome'
-import { action } from '@storybook/addon-actions'
+// import { action } from '@storybook/addon-actions'
 import { Basics, CoverFixedPositionLogic, PulsatingEffect } from './layout'
-import { Card, CardContent, CardImg, CardsContainer, CardsWrapper } from 'shared/card'
 import { h } from 'preact'
 import { IconAnimatedEllipsis, IconChevronLeft, IconChevronRight, IconClose } from 'icons'
-import { List, ListChevron, ListContent, ListImg, ListItem } from 'shared/list'
 import { Navigation, Outro, ScriptedChat, Showcase, Spotahome } from './modules'
 import { storiesOf } from '@storybook/react'
 import './styles.css'
-
-const PluginContainer = styled.div`
-  height: 500px;
-  width: 360px;
-  padding: 1rem;
-  background-color: #ebeef2;
-`
 
 storiesOf('Welcome', module).add('README', Welcome)
 
@@ -31,49 +20,6 @@ storiesOf('Modules', module)
   .add('Scripted Chat', ScriptedChat)
   .add('Outro', Outro)
   .add('Spotahome', Spotahome)
-
-storiesOf('Basic Components', module)
-  .add('Arrow', () => <Arrow />)
-  .add('Cards', () => (
-    <PluginContainer>
-      <CardsWrapper>
-        <CardsContainer>
-          <Card style={{ width: '100px' }}>
-            <CardImg src="https://placeimg.com/200/200/any?1" />
-            <CardContent>{'Buy this'}</CardContent>
-          </Card>
-          <Card style={{ width: '100px' }}>
-            <CardImg src="https://placeimg.com/200/200/any?2" />
-            <CardContent>{'Buy that'}</CardContent>
-          </Card>
-          <Card style={{ width: '100px' }}>
-            <CardImg src="https://placeimg.com/200/200/any?3" />
-            <CardContent>{'Buy the bear'}</CardContent>
-          </Card>
-          <Card style={{ width: '100px' }}>
-            <CardImg src="https://placeimg.com/200/200/any?4" />
-            <CardContent>{'Buy the other bear'}</CardContent>
-          </Card>
-        </CardsContainer>
-      </CardsWrapper>
-    </PluginContainer>
-  ))
-  .add('List', () => (
-    <PluginContainer>
-      <List>
-        <ListItem onClick={action('clicked-list-item')}>
-          <ListImg src="https://placeimg.com/200/200/any?1" />
-          <ListContent>{'Buy this'}</ListContent>
-          <ListChevron />
-        </ListItem>
-        <ListItem onClick={action('clicked-list-item')}>
-          <ListImg src="https://placeimg.com/200/200/any?2" />
-          <ListContent>{'Buy that'}</ListContent>
-          <ListChevron />
-        </ListItem>
-      </List>
-    </PluginContainer>
-  ))
 
 storiesOf('Icons', module)
   .add('close', () => <IconClose style={{ height: 24, width: 24 }} />)
--- plugin-base/src/showcase/components/showcase-cover.js
+++ plugin-base/src/showcase/components/showcase-cover.js
@@ -1,26 +1,6 @@
-import Arrow from 'shared/arrow'
 import React from 'react'
 import styled from 'styled-components'
-import { animate, TopSlideAnimation } from 'shared/animate'
-
-const AnimatedWhiteArrow = animate(
-  styled(Arrow).attrs({
-    color: '#fff',
-    lineColor: '#aaa',
-    width: '100px',
-  })`
-    opacity: ${({ isEntering, isLeaving }) => (isEntering || isLeaving ? 0 : 1)};
-    transform: ${({ isEntering, isLeaving }) =>
-      isEntering ? 'translateX(-200px)' : isLeaving ? 'translateX(600px)' : 'none'};
-    transition: opacity 1s ease, transform 0.6s ease;
-  `
-)
-
-const ArrowContainer = styled.div`
-  position: absolute;
-  bottom: 2px;
-  left: 135px;
-`
+import { TopSlideAnimation } from 'shared/animate'
 
 const Title = styled.div`
   font-size: 22px;
@@ -32,15 +12,10 @@ const SubTitle = styled.div`
 `
 
 const ShowcaseCover = ({ isLeaving, subtitle, title }) => (
-  <div>
-    <TopSlideAnimation delay={250 * 0} isLeaving={isLeaving}>
-      <Title>{title}</Title>
-      <SubTitle>{subtitle}</SubTitle>
-    </TopSlideAnimation>
-    <ArrowContainer>
-      <AnimatedWhiteArrow delay={250 * 2} isLeaving={isLeaving} />
-    </ArrowContainer>
-  </div>
+  <TopSlideAnimation delay={250 * 0} isLeaving={isLeaving}>
+    <Title>{title}</Title>
+    <SubTitle>{subtitle}</SubTitle>
+  </TopSlideAnimation>
 )
 
 export default ShowcaseCover
