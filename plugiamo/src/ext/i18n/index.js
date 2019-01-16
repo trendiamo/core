6f3bd4c20f36f8d2c1881debba0b31a11713bf71
Duarte Henriques <duarte.henriques@gmail.com>
2019-01-16 15:48:41 +0000

    Add some more translations

--- plugiamo/src/ext/i18n/index.js
+++ plugiamo/src/ext/i18n/index.js
@@ -3,6 +3,8 @@ const i18n = {
     const account = localStorage.getItem('trnd-plugin-account')
     if (['Corinthians', 'Impressorajato'].includes(account)) {
       return 'Me mostre outras sugestões'
+    } else if (account === 'RiHappy') {
+      return 'Preciso saber mais'
     } else {
       return 'I still need help'
     }
@@ -11,6 +13,8 @@ const i18n = {
     const account = localStorage.getItem('trnd-plugin-account')
     if (['Corinthians', 'Impressorajato'].includes(account)) {
       return 'Legal'
+    } else if (account === 'RiHappy') {
+      return 'Ok, otimo'
     } else {
       return 'Ok, cool'
     }
@@ -19,7 +23,7 @@ const i18n = {
     const account = localStorage.getItem('trnd-plugin-account')
     if (account === 'Corinthians') {
       return 'Conheça produtos que eu uso:'
-    } else if (['Shopinfo', 'Impressorajato'].includes(account)) {
+    } else if (['Shopinfo', 'Impressorajato', 'RiHappy'].includes(account)) {
       return `Produtos selecionados por ${firstName}:`
     } else {
       return `Products selected by ${firstName}`
