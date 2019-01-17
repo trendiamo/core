a9422da33829cbaef630b77642bb37bf7432a0af
Duarte Henriques <duarte.henriques@gmail.com>
2019-01-17 12:51:16 +0000

    Product Pick titles: don't span more than one line

--- plugin-base/src/showcase/components/product-item.js
+++ plugin-base/src/showcase/components/product-item.js
@@ -1,15 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ListChevron, ListContent, ListImg, ListItem } from 'shared/list'
+
+const Content = styled.div`
+  color: #4a4a4a;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`
+
+const Name = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.4;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`
+
+const Description = styled.div`
+  flex: 1;
+  font-size: 12px;
+`
+
+const DisplayPrice = styled.div`
+  font-size: 13px;
+  font-weight: bold;
+`
 
 const ProductItem = ({ spotlight, product, onClick }) => (
   <ListItem onClick={onClick({ product, spotlight })}>
     <ListImg src={product.picture.url} />
     <ListContent>
-      <div style={{ color: '#4a4a4a', display: 'flex', flexDirection: 'column', flex: 1 }}>
-        <div style={{ fontSize: '16px', fontWeight: '500', lineHeight: '1.4' }}>{product.name}</div>
-        <div style={{ flex: 1, fontSize: '12px' }}>{product.description}</div>
-        <div style={{ fontSize: '13px', fontWeight: 'bold' }}>{product.displayPrice}</div>
-      </div>
+      <Content>
+        <Name>{product.name}</Name>
+        <Description>{product.description}</Description>
+        <DisplayPrice>{product.displayPrice}</DisplayPrice>
+      </Content>
     </ListContent>
     <ListChevron />
   </ListItem>
