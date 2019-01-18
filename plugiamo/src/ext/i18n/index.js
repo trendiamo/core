3976fdb7ab595c71db55271d6189d8b018c2b3df
Duarte Henriques <duarte.henriques@gmail.com>
2019-01-18 17:04:11 +0000

    Add i18n for Eotica

--- plugiamo/src/ext/i18n/index.js
+++ plugiamo/src/ext/i18n/index.js
@@ -5,6 +5,8 @@ const i18n = {
       return 'Me mostre outras sugestões'
     } else if (account === 'Impressorajato') {
       return 'Quero falar com um consultor'
+    } else if (account === 'Eotica') {
+      return 'Quero saber mais'
     } else if (account === 'RiHappy') {
       return 'Preciso saber mais'
     } else {
@@ -15,6 +17,8 @@ const i18n = {
     const account = localStorage.getItem('trnd-plugin-account')
     if (['Corinthians', 'Impressorajato', 'Shopinfo', 'Shopinfo2'].includes(account)) {
       return 'Legal'
+    } else if (account === 'Eotica') {
+      return 'Legal, gostei!'
     } else if (account === 'RiHappy') {
       return 'Ok, otimo'
     } else {
@@ -25,6 +29,8 @@ const i18n = {
     const account = localStorage.getItem('trnd-plugin-account')
     if (account === 'Corinthians') {
       return 'Conheça produtos que eu uso:'
+    } else if (account === 'Eotica') {
+      return `Produtos escolhidos por ${firstName}:`
     } else if (['Shopinfo', 'Shopinfo2', 'Impressorajato', 'RiHappy'].includes(account)) {
       return `Produtos selecionados por ${firstName}:`
     } else {
