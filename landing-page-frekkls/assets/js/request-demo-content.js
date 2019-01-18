7de56394824e36487593496fe30c514490cde8ac
Daniel Welzel <daniel@familiewelzel.de>
2019-01-18 10:55:33 +0000

    Added demo section to frekkls landingpage

--- landing-page-frekkls/assets/js/request-demo-content.js
+++ landing-page-frekkls/assets/js/request-demo-content.js
@@ -1,7 +1,7 @@
 const requestDemoContent = `
   <div>
-    <h3>Get your demo 🚀</h3>
-    <p>Know why our tool is going to help you to engage with your customers and increase your sales.</p>
+    <h3>Get your personal demo 🚀</h3>
+    <p>Get in touch with us to get a personal presentation on how our tool will help you to engage with your customers and increase your sales.</p>
     <div class="request-demo-form"></div>
   </div>
 `
