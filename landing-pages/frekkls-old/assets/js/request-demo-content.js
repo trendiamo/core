caa235f6f80fc8d15032b9c130faa576180ca2f1
Duarte Henriques <duarte.henriques@gmail.com>
2019-01-31 10:55:48 +0000

    Remove old frekkls website source

--- landing-pages/frekkls-old/assets/js/request-demo-content.js
+++
@@ -1,9 +0,0 @@
-const requestDemoContent = `
-  <div>
-    <h3>Get your personal demo 🚀</h3>
-    <p>Get in touch with us to get a personal presentation on how our tool will help you to engage with your customers and increase your sales.</p>
-    <div class="request-demo-form"></div>
-  </div>
-`
-
-export default requestDemoContent
