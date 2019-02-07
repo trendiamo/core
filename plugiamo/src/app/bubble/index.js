7d19dad85e6071ad8595e4a56325d3a8897023d8
Duarte Henriques <duarte.henriques@gmail.com>
2019-02-07 10:32:01 +0000

    Remove typing effect on chat bubble
    And make it so we can re-show chat bubbles on changes to pluginState

--- plugiamo/src/app/bubble/index.js
+++ plugiamo/src/app/bubble/index.js
@@ -1,37 +1,22 @@
 import ChatBubbleFrame from './frame'
 import { branch, compose, lifecycle, renderNothing, withHandlers, withProps, withState } from 'recompose'
-import { ChatBubbleBase, Container, TextBar, TextWidthMeasure } from './components'
+import { ChatBubbleBase, Container, TextWidthMeasure } from './components'
 import { h } from 'preact'
-import { typeText } from 'plugin-base'
 
 // All time values are in seconds
 const defaultBubble = {
   timeStart: 0.5,
   timeEnd: 20,
-  typingSpeed: 0.04,
   timeStartDuration: 0.4,
   timeEndDuration: 0.4,
-  hideBarAfter: 3,
-  startTypingAfter: 0.135,
 }
 
-const ChatBubble = ({
-  position,
-  bubble,
-  message,
-  animation,
-  bar,
-  setTextWidthRef,
-  textWidth,
-  onToggleContent,
-  ...props
-}) => (
+const ChatBubble = ({ position, bubble, animation, setTextWidthRef, textWidth, onToggleContent, ...props }) => (
   <ChatBubbleFrame animation={animation} bubble={bubble} position={position} textWidth={textWidth} {...props}>
     <Container onClick={onToggleContent}>
       <ChatBubbleBase animation={animation} bubble={bubble}>
-        {message}
+        {bubble.message}
       </ChatBubbleBase>
-      {bar && <TextBar />}
       <TextWidthMeasure ref={setTextWidthRef}>{bubble.message}</TextWidthMeasure>
     </Container>
   </ChatBubbleFrame>
@@ -39,28 +24,11 @@ const ChatBubble = ({
 
 export default compose(
   withState('animation', 'setAnimation', null),
-  withState('message', 'setMessage', ''),
-  withState('bar', 'setBar', false),
   withState('textWidth', 'setTextWidth', 0),
   withState('elevation', 'setElevation', false),
   withProps(({ bubble }) => ({
     bubble: { ...defaultBubble, ...bubble },
   })),
-  withHandlers({
-    writeText: ({ bubble, setMessage, setBar }) => ({ that }) => {
-      typeText({
-        addMessage: text => {
-          setMessage(that.props.message + text)
-        },
-        speed: bubble.typingSpeed,
-        callback: () =>
-          setTimeout(() => {
-            setBar(false)
-          }, bubble.hideBarAfter * 1000),
-        textReference: bubble.message,
-      })
-    },
-  }),
   withHandlers(() => {
     let textWidthRef
     return {
@@ -72,44 +40,37 @@ export default compose(
       },
     }
   }),
-  lifecycle({
-    componentDidMount() {
-      const { setAnimation, bubble, writeText, changeTextWidth, setBar, setElevation } = this.props
+  withHandlers({
+    animateThis: ({ animation, bubble, changeTextWidth, setAnimation, setElevation, setTextWidth }) => () => {
       if (!bubble.message) return false
-      const timeStart = bubble.timeStart * 1000
-      const timeEnd = bubble.timeEnd * 1000
-      const startTypingAfter = bubble.startTypingAfter * 1000
-      const timeStartDuration = bubble.timeStartDuration * 1000
-      const timeEndDuration = bubble.timeEndDuration * 1000
-      const timeOfElevation = bubble.timeOfElevation && bubble.timeOfElevation * 1000
       setTimeout(() => {
         setAnimation('roll')
         changeTextWidth()
-      }, timeStart)
+      }, bubble.timeStart * 1000)
       setTimeout(() => {
-        setBar(true)
-      }, timeStart + timeStartDuration)
-      setTimeout(() => {
-        writeText({ that: this })
-      }, timeStart + timeStartDuration + startTypingAfter)
-      setTimeout(() => {
-        const { animation } = this.props
         if (animation === 'roll') setAnimation('unroll')
-      }, timeStart + timeStartDuration + timeEnd)
-      setTimeout(() => {
-        setAnimation(null)
-      }, timeStart + timeStartDuration + timeEnd + timeEndDuration)
-      if (timeOfElevation) {
+      }, (bubble.timeStart + bubble.timeStartDuration + bubble.timeEnd) * 1000)
+      if (bubble.timeOfElevation) {
         setTimeout(() => {
           setElevation(true)
-        }, timeStart + timeStartDuration + timeOfElevation)
+        }, (bubble.timeStart + bubble.timeStartDuration + bubble.timeOfElevation) * 1000)
       }
-    },
-    componentDidUpdate() {
-      const { showingContent, setAnimation, animation } = this.props
-      if (showingContent && animation) {
+      setTimeout(() => {
         setAnimation(null)
-      }
+        setTextWidth(0)
+        setElevation(false)
+      }, (bubble.timeStart + bubble.timeStartDuration + bubble.timeEnd + bubble.timeEndDuration) * 1000)
+    },
+  }),
+  lifecycle({
+    componentDidMount() {
+      const { animateThis } = this.props
+      animateThis()
+    },
+    componentDidUpdate(prevProps) {
+      const { animateThis, showingContent, setAnimation, animation } = this.props
+      if (prevProps.bubble.message !== this.props.bubble.message) animateThis()
+      if (showingContent && animation) setAnimation(null)
     },
   }),
   branch(({ showingContent, bubble }) => !bubble.message || showingContent, renderNothing)
