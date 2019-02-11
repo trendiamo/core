4b6ffee25c27238a0088e4e6f5bdb103a6746eea
MaxChu23 <mymaksym@gmail.com>
2019-02-11 11:46:47 +0000

    Fix background bug in Android Chrome

--- plugiamo/src/special/nowadays/image-carousel-message.js
+++ plugiamo/src/special/nowadays/image-carousel-message.js
@@ -145,7 +145,7 @@ const ImgCarouselMessage = compose(
             position: 'absolute',
             top: 0,
             right: 0,
-            bottom: 0,
+            bottom: 50,
             left: 0,
           }}
         >
