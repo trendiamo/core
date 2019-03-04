da33f9cc9a806c7384cf2f54bca143a962759ac1
Daniel Bugge <danielbugge96@gmail.com>
2019-03-04 11:13:13 +0000

    Make blog card display flex

--- landing-pages/frekkls/src/pages/blog.js
+++ landing-pages/frekkls/src/pages/blog.js
@@ -6,23 +6,31 @@ import blogPostUrl from '../utils/index.js'
 import Container from '../components/container'
 import Layout from '../components/layout'
 import Section from '../components/section'
-import { FeatureCards } from '../components/cards'
+import styled from 'styled-components'
+
+const BlogPageContainer = styled(Container)`
+  display: flex;
+  flex-direction: row;
+
+  @media (max-width: 700px) {
+    align-items: center;
+    flex-direction: column;
+  }
+`
 
 const BlogPage = ({ className, pageContext, data }) => (
   <Layout className={className} layout={data.layout} locale={pageContext.locale}>
     <Section>
-      <Container>
-        <FeatureCards>
-          {data.blogs.edges.map((blog, index) => (
-            <BlogPostCard
-              imagesData={data.imagesData.edges[index].node}
-              key={blog.node.id}
-              post={blog.node}
-              to={blogPostUrl(blog.node, pageContext.locale)}
-            />
-          ))}
-        </FeatureCards>
-      </Container>
+      <BlogPageContainer>
+        {data.blogs.edges.map((blog, index) => (
+          <BlogPostCard
+            imagesData={data.imagesData.edges[index].node}
+            key={blog.node.id}
+            post={blog.node}
+            to={blogPostUrl(blog.node, pageContext.locale)}
+          />
+        ))}
+      </BlogPageContainer>
     </Section>
   </Layout>
 )
