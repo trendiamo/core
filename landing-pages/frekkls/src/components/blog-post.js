2d155b782602b9385f8ecabe2fa99a964142792a
Daniel Bugge <danielbugge96@gmail.com>
2019-03-06 14:48:59 +0000

    Impplement new blog post page design

--- landing-pages/frekkls/src/components/blog-post.js
+++ landing-pages/frekkls/src/components/blog-post.js
@@ -1,16 +1,18 @@
 import Layout from '../components/layout'
 import React from 'react'
 import styled from 'styled-components'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
+import locales from '../../locales'
 import Seo from './seo'
+import { FacebookLink, InstagramLink, TwitterLink, YoutubeLink } from './social-media-icons'
 
 const MetaImg = styled.img`
+  margin-top: 10px;
   float: left;
   width: 60px;
   height: 60px;
   border-radius: 100%;
-  border: 2px solid #ff7966;
   padding: 2px;
   margin-right: 10px;
 `
@@ -18,24 +20,29 @@ const MetaImg = styled.img`
 const MetaAuthor = styled.div`
   font-size: 0.8em;
   font-weight: 700;
-  padding-top: 14px;
+  padding-top: 16px;
 `
 
 const MetaDate = styled.div`
   font-size: 0.8em;
   color: #a5a5a5;
-  padding-top: 4px;
 `
 
-const TitleImg = styled.img`
-  width: 100%;
-  margin-top: 18px;
-  border-radius: 10px;
+const HeaderImg = styled.div`
+  position: relative;
+  padding: 200px 0px;
+  background-color: #000;
+  background-image: linear-gradient(95deg, rgba(14, 13, 13, 0.7), rgba(255, 139, 100, 0.6)), url(${({ src }) => src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 `
 
 const TitleImgMeta = styled.div`
   font-size: 12px;
   text-transform: uppercase;
+  text-align: left;
+  margin-left: 10px;
   padding-top: 10px;
 `
 
@@ -43,8 +50,6 @@ const BlogPostBase = styled.article`
   width: 70%;
   margin: 0 auto;
   text-align: left;
-  border-top: 2px solid #ebebeb;
-  padding-top: 40px;
   padding-bottom: 3rem;
   line-height: 1.58;
   letter-spacing: -0.3px;
@@ -56,8 +61,8 @@ const BlogPostBase = styled.article`
   }
 
   h2 {
+    font-size: 30px;
     margin-top: 14px;
-    color: #565656;
   }
 
   a {
@@ -83,12 +88,75 @@ const BlogText = styled.div`
   }
 `
 
-const BlogSpacer = styled.div`
-  width: 50%;
-  height: 2px;
-  margin: 0 auto;
-  margin-top: 30px;
-  background: #ebebeb;
+const Slug = styled.div`
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  margin-bottom: 40px;
+
+  p {
+    margin-bottom: 0px;
+    font-size: 20px;
+  }
+  @media (min-width: 700px) {
+    flex-direction: row;
+  }
+`
+
+const DetailsContainer = styled.div`
+  display: flex;
+  margin-bottom: 40px;
+  border-bottom: 1px solid black;
+
+  @media (min-width: 700px) {
+    border: none;
+  }
+`
+
+const MetaDetailsContainer = styled.div`
+  width: 100%;
+  padding-bottom: 6px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  @media (min-width: 700px) {
+    border-bottom: 1px solid black;
+    flex-direction: row;
+    padding-bottom: 16px;
+  }
+`
+
+const SocialMediaLinks = styled.div`
+  padding-top: 4px;
+  margin-left: -10px;
+  @media (min-width: 700px) {
+    padding-top: 30px;
+    align-self: flex-end;
+    flex-direction: row;
+  }
+`
+
+const ArticleDetails = ({ pageContext }) => (
+  <DetailsContainer>
+    <MetaImg src={pageContext.post.node.authorImage.fixed.src} />
+    <MetaDetailsContainer>
+      <div>
+        <MetaAuthor>{pageContext.post.node.authorName}</MetaAuthor>
+        <MetaDate>{pageContext.post.node.publishingDate}</MetaDate>
+      </div>
+      <SocialMediaLinks>
+        <FacebookLink />
+        <InstagramLink />
+        <TwitterLink />
+        <YoutubeLink />
+      </SocialMediaLinks>
+    </MetaDetailsContainer>
+  </DetailsContainer>
+)
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
 `
 
 const BlogPost = ({ pageContext, data }) => (
@@ -101,21 +169,22 @@ const BlogPost = ({ pageContext, data }) => (
       meta={[]}
       title={pageContext.post.node.title}
     />
+    <HeaderImg src={pageContext.post.node.titleImage.fixed.src} />
+    <TitleImgMeta
+      dangerouslySetInnerHTML={{
+        __html: pageContext.post.node.titleImageCredit.childContentfulRichText.html,
+      }}
+    />
     <BlogPostBase>
+      <Slug>
+        <StyledLink className="header-link" to={`${locales[pageContext.locale].path}/blog`}>
+          <p>{'Blog /'}</p>
+        </StyledLink>
+        <p style={{ fontWeight: '500', marginLeft: '0.2em' }}>{pageContext.post.node.title}</p>
+      </Slug>
       <h1>{pageContext.post.node.title}</h1>
       <h2>{pageContext.post.node.secondaryTitle}</h2>
-      <div>
-        <MetaImg src={pageContext.post.node.authorImage.fixed.src} />
-        <MetaAuthor>{pageContext.post.node.authorName}</MetaAuthor>
-        <MetaDate>{pageContext.post.node.publishingDate}</MetaDate>
-      </div>
-      <TitleImg alt={pageContext.post.node.description.description} src={pageContext.post.node.titleImage.fixed.src} />
-      <TitleImgMeta
-        dangerouslySetInnerHTML={{
-          __html: pageContext.post.node.titleImageCredit.childContentfulRichText.html,
-        }}
-      />
-      <BlogSpacer />
+      <ArticleDetails pageContext={pageContext} />
       <BlogText
         className="blog-text"
         dangerouslySetInnerHTML={{
