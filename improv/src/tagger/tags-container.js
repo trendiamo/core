1077cf93e747270e0d88d046bc18997086121c64
MaxChu23 <mymaksym@gmail.com>
2019-03-12 16:33:49 +0000

    Make UI more comfortable

--- improv/src/tagger/tags-container.js
+++ improv/src/tagger/tags-container.js
@@ -1,4 +1,3 @@
-import NavigationButtons from './navigation-buttons'
 import React from 'react'
 import styled from 'styled-components'
 import Tag from './tag'
@@ -9,13 +8,12 @@ const Container = styled.div`
   padding-left: 0.5rem;
 `
 
-const TagsContainer = ({ tags, onClickTag, currentProduct, tagGroupIndex, tagsLength, nextScreen, prevScreen }) => (
+const TagsContainer = ({ tags, onClickTag, currentProduct, tagGroupIndex, tagsLength }) => (
   <Container>
     {`${tagGroupIndex + 1} / ${tagsLength}`}
     {tags.map((tag, index) => (
       <Tag currentProduct={currentProduct} key={tag} keyCode={String(index + 1)} onClick={onClickTag} tag={tag} />
     ))}
-    <NavigationButtons nextScreen={nextScreen} prevScreen={prevScreen} />
   </Container>
 )
 
