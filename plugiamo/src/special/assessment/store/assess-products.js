2171cc31bae4a6ca6c3d216d48106e54ae63b91f
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-03-19 11:05:38 +0000

    Linearize tags in assessment filter logic

--- plugiamo/src/special/assessment/store/assess-products.js
+++ plugiamo/src/special/assessment/store/assess-products.js
@@ -1,8 +1,10 @@
 const assessProducts = (products, tagsArrays) => {
   const productsResult = []
-  tagsArrays
-    .map(tags => products.filter(product => product.tags && !tags.map(tag => !!product.tags[tag]).includes(false)))
+  const tags = tagsArrays.map(tagsArray => tagsArray.join('/'))
+  tags
+    .map(tag => products.filter(product => product.tag && tag === product.tag))
     .forEach(productArray => productsResult.push(...productArray))
+
   return productsResult
 }
 
