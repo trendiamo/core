ca65d21a6eba37845ac0885f55ad77336cff469a
MaxChu23 <mymaksym@gmail.com>
2019-03-19 13:02:11 +0000

    Change tagger structure

--- improv/src/tagger/index.js
+++ improv/src/tagger/index.js
@@ -1,4 +1,5 @@
 import Buttons from './components/buttons'
+import data from './data'
 import ProductsAndTags from './components/products-and-tags'
 import React from 'react'
 import Spinner from 'shared/loading-spinner'
@@ -37,27 +38,18 @@ const Flex = styled.div`
   align-items: center;
 `
 
-const tagsMatrix = [
-  ['Basics', 'Business', 'Casual', 'Classic'],
-  ['Shirts', 'Pants', 'Jackets', 'Accessories'],
-  ['Slim Fit', 'Regular Fit', 'Classic Fit', 'Modern Fit'],
-  ['Red', 'Blue', 'Yellow', 'Geen'],
-  [{ key: 'highlight', name: 'Highlight' }],
-]
-
-const Tagger = ({ onCopyResult, nextScreen, prevScreen, ...props }) => (
+const Tagger = ({ onCopyResult, ...props }) => (
   <Flex>
     <H1>{'Products Tagger'}</H1>
-    <ProductsAndTags nextScreen={nextScreen} prevScreen={prevScreen} tagsMatrix={tagsMatrix} {...props} />
-    <HelpText>{'Hotkeys: 1-9: tagging; W|S: navigate products; A|D: navigate tabs'}</HelpText>
-    <Buttons nextScreen={nextScreen} onCopyResult={onCopyResult} prevScreen={prevScreen} />
+    <ProductsAndTags data={data} {...props} />
+    <HelpText>{'Hotkeys: 1-9: tagging; W|S: navigate products'}</HelpText>
+    <Buttons onCopyResult={onCopyResult} />
   </Flex>
 )
 
 export default compose(
   withState('products', 'setProducts', () => parseProducts()),
   withState('productIndex', 'setProductIndex', 0),
-  withState('tagGroupIndex', 'setTagGroupIndex', 0),
   withState('isLoading', 'setIsLoading', true),
   withState('client', 'setClient', false),
   withState('changedProducts', 'setChangedProducts', []),
@@ -66,61 +58,25 @@ export default compose(
     currentProduct: products && products[productIndex],
   })),
   withHandlers({
-    nextScreen: ({ tagGroupIndex, setTagGroupIndex }) => () => {
-      if (tagGroupIndex < tagsMatrix.length - 1) return setTagGroupIndex(tagGroupIndex + 1)
-    },
-    prevScreen: ({ tagGroupIndex, setTagGroupIndex }) => () => {
-      if (tagGroupIndex > 0) return setTagGroupIndex(tagGroupIndex - 1)
-    },
-    changeProduct: ({ productIndex, products }) => ({ tag, product, index }) => {
-      const tagIsSimple = typeof tag === 'string'
-      const cancelOut = index === undefined ? true : index <= productIndex
-      const currentProduct = products[productIndex]
-      let currentValue = false
-      if (tagIsSimple) {
-        currentValue = currentProduct.tags ? !!currentProduct.tags[tag] : false
-      } else {
-        currentValue = currentProduct ? !!currentProduct[tag.key] : false
-      }
-      const value = !cancelOut ? currentValue : !currentValue
-      if (tagIsSimple) {
-        if (!product.tags) {
-          product.tags = { [tag]: value }
+    changeProducts: ({ products }) => ({ tag, productIds }) => {
+      return products.map((product, index) => {
+        if (productIds.indexOf(index) === -1) return product
+        if (tag.key) {
+          product[tag.key] = !product[tag.key]
         } else {
-          product.tags[tag] = value
+          product.tag = product.tag === tag ? '' : tag || product.tag
         }
-      } else {
-        product[tag.key] = value
-      }
-      return product
+        return product
+      })
     },
   }),
   withHandlers({
-    toggleTag: ({ changedProducts, setChangedProducts, products, setProducts, changeProduct, productIndex }) => ({
-      tag,
-      index,
-    }) => {
-      const currentIndex = index !== undefined ? index : productIndex
-      const productToTag = products[currentIndex]
-      products[currentIndex] = changeProduct({ product: productToTag, tag, index })
-      changedProducts[currentIndex]
-        ? (changedProducts[currentIndex][tag] = !changedProducts[currentIndex][tag])
-        : (changedProducts[currentIndex] = { [tag]: !!tag })
-      setChangedProducts(changedProducts)
-      setProducts(products)
+    toggleTag: ({ setProducts, changeProducts, multipleSelect, productIndex }) => tag => {
+      const productIds = multipleSelect.length > 0 ? multipleSelect : [productIndex]
+      setProducts(changeProducts({ productIds, tag }))
     },
   }),
   withHandlers({
-    onPressTagKey: ({ toggleTag, tagGroupIndex, multipleSelect }) => tagKey => {
-      const tag = tagsMatrix[tagGroupIndex][Number(tagKey) - 1]
-      if (multipleSelect.length > 0) {
-        multipleSelect.map(index => {
-          toggleTag({ tag, index })
-        })
-        return
-      }
-      toggleTag({ tag })
-    },
     onCopyResult: ({ setChangedProducts, client, products, setClient, setIsLoading }) => async () => {
       copyToClipboard(JSON.stringify({ products }))
       setIsLoading(true)
@@ -167,9 +123,6 @@ export default compose(
     },
   }),
   withHotkeys({
-    [keyboard.d]: ({ nextScreen }) => nextScreen,
-    [keyboard.a]: ({ prevScreen }) => prevScreen,
-
     [keyboard.w]: ({ goToProduct, selectMultiple, multipleSelect, productIndex }) => event => {
       if (event.shiftKey) {
         const index = multipleSelect.length === 0 ? productIndex - 1 : multipleSelect[0] - 1
@@ -186,16 +139,6 @@ export default compose(
       }
       goToProduct('next', true)
     },
-
-    49: ({ onPressTagKey }) => () => onPressTagKey(1),
-    50: ({ onPressTagKey }) => () => onPressTagKey(2),
-    51: ({ onPressTagKey }) => () => onPressTagKey(3),
-    52: ({ onPressTagKey }) => () => onPressTagKey(4),
-    //  numpad keys
-    97: ({ onPressTagKey }) => () => onPressTagKey(1),
-    98: ({ onPressTagKey }) => () => onPressTagKey(2),
-    99: ({ onPressTagKey }) => () => onPressTagKey(3),
-    100: ({ onPressTagKey }) => () => onPressTagKey(4),
   }),
   lifecycle({
     async componentDidMount() {
@@ -208,7 +151,7 @@ export default compose(
           const clientProduct = client[0].products.find(o => o.url === product.url)
           const clientProductIndex = client[0].products.findIndex(o => o.url === product.url)
           if (clientProduct) {
-            product.tags = clientProduct.tags
+            product.tag = clientProduct.tag
             product.highlight = clientProduct.highlight
             client[0].products.splice(clientProductIndex, 1)
           }
