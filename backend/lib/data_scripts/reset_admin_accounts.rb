2fe2b4265e3169732419ed064177eefa76148ca2
Duarte Henriques <duarte.henriques@gmail.com>
2019-03-26 14:59:05 +0000

    minor update

--- backend/lib/data_scripts/reset_admin_accounts.rb
+++ backend/lib/data_scripts/reset_admin_accounts.rb
@@ -18,11 +18,7 @@ class ResetAdminAccounts
     admin_emails = %w[dh@trendiamo.com tds@trendiamo.com alena@frekkls.com dw@trendiamo.com]
     admin_emails.each do |admin_email|
       admin_user = User.find_by(email: admin_email)
-      if admin_user
-        admin_user.update!(admin: true, account: nil)
-      else
-        User.create!(email: admin_email, admin: true, password: "password", confirmed_at: Time.now)
-      end
+      admin_user.update!(admin: true, account: nil)
     end
   end
 end
