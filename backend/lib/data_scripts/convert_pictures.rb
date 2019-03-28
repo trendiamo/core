791f901f9280a2940011393a5515be34ed2a19db
Duarte Henriques <duarte.henriques@gmail.com>
2019-03-28 15:01:32 +0000

    minor changes to the data script

--- backend/lib/data_scripts/convert_pictures.rb
+++ backend/lib/data_scripts/convert_pictures.rb
@@ -20,7 +20,9 @@ class ConvertPicUrlsToPics
 
   def convert_and_assign_pics(elements)
     elements.each do |element|
-      element.pic_url && element.pic = Picture.find_or_create_by!(url: element.pic_url)
+      next if element.pic_url.blank?
+      picture = Picture.find_or_create_by!(url: element.pic_url)
+      element.pic_id = picture.id
       element.save!
     end
   end
@@ -28,7 +30,9 @@ class ConvertPicUrlsToPics
   def convert_and_assign_profile_pics(elements)
     elements.each do |element|
       %i[profile_pic profile_pic_animation].each do |pic|
-        element["#{pic}_url"] && element["#{pic}_id"] = Picture.find_or_create_by!(url: element["#{pic}_url"]).id
+        next if element["#{pic}_url"].blank?
+        picture = Picture.find_or_create_by!(url: element["#{pic}_url"])
+        element["#{pic}_id"] = picture.id
         element.save!
       end
     end
