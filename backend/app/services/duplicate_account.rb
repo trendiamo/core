010c62d0b8aef6e2fa547e03f49fce1e5fc3d468
Duarte Henriques <duarte.henriques@gmail.com>
2019-04-03 15:57:18 +0100

    Fix logic for duplicating accounts

--- backend/app/services/duplicate_account.rb
+++ backend/app/services/duplicate_account.rb
@@ -1,22 +1,43 @@
+def generate_new_url(picture_url)
+  uri = URI(picture_url)
+  directories_array = path_without_slash(uri).split("/", 4)
+  directories_array[2] = SecureRandom.hex(4)
+  "#{uri.scheme}://#{uri.host}/#{directories_array.join('/')}"
+end
+
+def duplicate_pic_url(picture_url)
+  new_url = generate_new_url(picture_url)
+  source_path = path_without_slash(picture_url)
+  destination_path = path_without_slash(new_url)
+  bucket = Aws::S3::Bucket.new(ENV["DO_BUCKET"])
+  bucket.object(destination_path).copy_from(bucket: ENV["DO_BUCKET"], key: source_path, acl: "public-read")
+  new_url
+end
+
+def path_without_slash(url)
+  URI(url).path[1..-1]
+end
+
 class DuplicateAccount
   def initialize(account, name, hostnames)
+    @account = account
     @name = name
     @hostnames = hostnames
-    @account = account
-    @cloned_account = @account.deep_clone { |_void, copy| copy.name = @name }
-    @duplicate_pictures_service = Aws::S3::DuplicatePictures.new(@account, @cloned_account)
   end
 
   def perform
-    @duplicate_pictures_service.perform if duplicate
-  rescue StandardError, Aws::S3::Errors::ServiceError
+    duplicate
+  rescue StandardError, Aws::S3::Errors::ServiceError => e
     @cloned_account.destroy if @cloned_account.persisted?
+    raise e
   end
 
   private
 
   def duplicate
+    @cloned_account = @account.deep_clone { |_void, copy| copy.name = @name }
     @cloned_account.save!
+    @persona_id_mapping = {} # hash of original ids to duplicate ids
     duplicate_personas
     duplicate_websites
     duplicate_flows
@@ -24,85 +45,30 @@ class DuplicateAccount
   end
 
   def duplicate_personas
-    cloned_personas = clone_personas
-    @mapped_ids = Hash[@account.personas.pluck(:id).map { |id| [id, nil] }]
-    @personas = @account.personas
-    ActsAsTenant.with_tenant(@cloned_account) do
-      cloned_personas.each do |persona|
-        cloned_persona = persona.deep_clone include: %i[profile_pic]
-        cloned_persona.profile_pic.url = @duplicate_pictures_service.duplicate_pic_url(persona.profile_pic)
+    @account.personas.map do |persona|
+      cloned_persona = persona.deep_clone(include: %i[profile_pic])
+      cloned_persona.profile_pic.url = duplicate_pic_url(persona.profile_pic.url)
+      ActsAsTenant.with_tenant(@cloned_account) do
         cloned_persona.save!
-        @mapped_ids[persona.id] = cloned_persona.id
       end
-    end
-  end
-
-  def clone_personas
-    @account.personas.each do |persona|
-      persona.deep_clone include: %i[profile_pic]
+      @persona_id_mapping[persona.id] = cloned_persona.id
+      cloned_persona
     end
   end
 
   def duplicate_websites
-    @account.websites.each do |website|
-      website_cloned = website.deep_clone
-      website_cloned.name = @name
-      website_cloned.hostnames = @hostnames
+    @account.websites.map do |website|
+      cloned_website = website.deep_clone
+      cloned_website.name = @name
+      cloned_website.hostnames = @hostnames
       ActsAsTenant.with_tenant(@cloned_account) do
-        website_cloned.save!
+        cloned_website.save!
       end
     end
   end
 
   def duplicate_flows
-    flows = find_flows
-    ActsAsTenant.with_tenant(@cloned_account) do
-      flows.each(&:save!)
-    end
-  end
-
-  def find_flows
-    flows = { navigations: navigations,
-              showcases: showcases,
-              simple_chats: simple_chats,
-              outros: Outro.where(account: @account).map(&:deep_clone), }.collect { |_void, value| value }
-    assign_personas(flows.flatten)
-  end
-
-  def navigations
-    Navigation.where(account: @account).map do |navigation|
-      cloned_navigation = navigation.deep_clone include: { navigation_items: :pic }
-      cloned_navigation.navigation_items.map do |navigation_item|
-        navigation_item.pic.url = @duplicate_pictures_service.duplicate_pic_url(navigation_item.pic)
-        ActsAsTenant.with_tenant(@cloned_account) do
-          navigation_item.pic.save
-        end
-      end
-      cloned_navigation
-    end
-  end
-
-  def simple_chats
-    SimpleChat.where(account: @account).map do |simple_chat|
-      simple_chat.deep_clone include: { simple_chat_steps: :simple_chat_messages }
-    end
-  end
-
-  def showcases
-    showcases = Showcase.where(account: @account).map do |showcase|
-      cloned_showcase = showcase.deep_clone include: { spotlights: { product_picks: :pic } }
-      cloned_showcase.spotlights.map do |spotlight|
-        spotlight.product_picks.map do |product_pick|
-          product_pick.pic.url = @duplicate_pictures_service.duplicate_pic_url(product_pick.pic)
-          ActsAsTenant.with_tenant(@cloned_account) do
-            product_pick.pic.save
-          end
-        end
-        spotlight
-      end
-      cloned_showcase
-    end
-    showcases.map { |showcase| showcase if assign_personas(showcase.spotlights) }
+    DuplicateFlows.new(@account, @cloned_account, @persona_id_mapping).perform
   end
 
   def duplicate_triggers
@@ -115,11 +81,76 @@ class DuplicateAccount
       end
     end
   end
+end
+
+class DuplicateFlows
+  def initialize(account, cloned_account, persona_id_mapping)
+    @account = account
+    @cloned_account = cloned_account
+    @persona_id_mapping = persona_id_mapping
+  end
+
+  def perform
+    flows = {
+      navigations: navigations,
+      showcases: showcases,
+      simple_chats: simple_chats,
+      outros: Outro.where(account: @account).map(&:deep_clone),
+    }.collect { |_void, value| value }.flatten
+    assign_personas(flows)
+    ActsAsTenant.with_tenant(@cloned_account) do
+      flows.map(&:save!)
+    end
+  end
+
+  private
+
+  def navigations
+    Navigation.where(account: @account).map do |navigation|
+      cloned_navigation = navigation.deep_clone(include: { navigation_items: :pic })
+      process_cloned_navigation(cloned_navigation)
+      cloned_navigation
+    end
+  end
+
+  def showcases
+    Showcase.where(account: @account).map do |showcase|
+      cloned_showcase = showcase.deep_clone(include: { spotlights: { product_picks: :pic } })
+      process_cloned_showcase(cloned_showcase)
+      cloned_showcase
+    end
+  end
+
+  def simple_chats
+    SimpleChat.where(account: @account).map do |simple_chat|
+      simple_chat.deep_clone(include: { simple_chat_steps: :simple_chat_messages })
+    end
+  end
+
+  def process_cloned_navigation(cloned_navigation)
+    cloned_navigation.navigation_items.map do |navigation_item|
+      navigation_item.pic.url = duplicate_pic_url(navigation_item.pic.url)
+      ActsAsTenant.with_tenant(@cloned_account) do
+        navigation_item.pic.save!
+      end
+    end
+  end
+
+  def process_cloned_showcase(cloned_showcase)
+    assign_personas(cloned_showcase.spotlights)
+    cloned_showcase.spotlights.map do |spotlight|
+      spotlight.product_picks.map do |product_pick|
+        product_pick.pic.url = duplicate_pic_url(product_pick.pic.url)
+        ActsAsTenant.with_tenant(@cloned_account) do
+          product_pick.pic.save!
+        end
+      end
+    end
+  end
 
   def assign_personas(collection)
     collection.map do |object|
-      persona = Persona.find(@mapped_ids[object.persona.id])
-      object.persona = persona
+      object.persona_id = @persona_id_mapping[object.persona.id]
       object
     end
   end
