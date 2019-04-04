480f19abb006ca0e02beb044a179e84640c11cdf
Daniel Bugge <danielbugge96@gmail.com>
2019-04-04 16:10:10 +0100

    Remove embedded app configuration

--- integrations/shopify/config/initializers/shopify_app.rb
+++ integrations/shopify/config/initializers/shopify_app.rb
@@ -7,8 +7,6 @@ ShopifyApp.configure do |config|
   # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.scope = "read_products,write_script_tags"
 
-  config.embedded_app = true
-
   config.after_authenticate_job = false
 
   config.session_repository = ShopifyApp::InMemorySessionStore
