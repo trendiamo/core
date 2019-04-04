61891b5a2f60fa08be7aec4941a2c131d9b5a2ba
Daniel Bugge <danielbugge96@gmail.com>
2019-04-04 15:39:27 +0100

    remove unecessary X-Frame-Options deafult value

--- integrations/shopify/config/environments/development.rb
+++ integrations/shopify/config/environments/development.rb
@@ -40,10 +40,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-  }
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
