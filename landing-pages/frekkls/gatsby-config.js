5000d6c07e0579477cc8c40d4edff80a9ac40b82
Duarte Henriques <duarte.henriques@gmail.com>
2019-04-09 16:02:13 +0100

    Remove trailing slashes from generated urls

--- landing-pages/frekkls/gatsby-config.js
+++ landing-pages/frekkls/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     author: '@trendiamo',
   },
   plugins: [
+    'gatsby-plugin-remove-trailing-slashes',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
