f0ed27e2f6def58d5d08b3a1e406afe8335a0aef
Gabriele Canepa <gabriele@canepa.io>
2019-04-11 17:07:17 +0100

    Implement pictures modal

--- console-frontend/src/shared/picture-uploader.js
+++ console-frontend/src/shared/picture-uploader.js
@@ -1,8 +1,8 @@
 import CloudUpload from '@material-ui/icons/CloudUpload'
 import Delete from '@material-ui/icons/Delete'
 import omit from 'lodash.omit'
+import PicturesModal from './pictures-modal'
 import React from 'react'
-import ReactCrop, { getPixelCrop } from 'react-image-crop'
 import ReactDropzone from 'react-dropzone'
 import S3Upload from 'ext/react-s3-uploader'
 import styled from 'styled-components'
@@ -10,6 +10,7 @@ import theme from 'app/theme'
 import { apiGetSignedUrlFactory } from 'utils'
 import { Button, FormControl, InputLabel, LinearProgress } from '@material-ui/core'
 import { compose, lifecycle, shallowEqual, shouldUpdate, withHandlers, withProps, withState } from 'recompose'
+import { getPixelCrop } from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 
 const Container = styled.div`
@@ -39,7 +40,7 @@ const StyledDelete = styled(Delete)`
 `
 
 const FilteredReactDropzone = props => (
-  <ReactDropzone {...omit(props, ['isDragging', 'previewImage', 'setIsDragging'])} />
+  <ReactDropzone {...omit(props, ['isDragging', 'previewPicture', 'setIsDragging'])} />
 )
 
 const StyledDropzone = styled(FilteredReactDropzone)`
@@ -50,17 +51,17 @@ const StyledDropzone = styled(FilteredReactDropzone)`
   width: 150px;
   margin-top: 25px;
   margin-bottom: 0.4rem;
-  color: ${({ isDragging, previewImage }) => (previewImage ? '#fff' : isDragging ? '#ff6641' : '#7f8086')};
+  color: ${({ isDragging, previewPicture }) => (previewPicture ? '#fff' : isDragging ? '#ff6641' : '#7f8086')};
 
   ${InnerLabel} {
-    visibility: ${({ isDragging, previewImage }) => (isDragging || !previewImage ? 'visible' : 'hidden')};
-    background-color: ${({ isDragging, previewImage }) =>
-      isDragging && previewImage ? 'rgba(0, 0, 0, 0.6)' : 'transparent'};
+    visibility: ${({ isDragging, previewPicture }) => (isDragging || !previewPicture ? 'visible' : 'hidden')};
+    background-color: ${({ isDragging, previewPicture }) =>
+      isDragging && previewPicture ? 'rgba(0, 0, 0, 0.6)' : 'transparent'};
   }
 
   &:hover ${InnerLabel} {
     visibility: visible;
-    background-color: ${({ previewImage }) => (previewImage ? 'rgba(0, 0, 0, 0.6)' : 'transparent')};
+    background-color: ${({ previewPicture }) => (previewPicture ? 'rgba(0, 0, 0, 0.6)' : 'transparent')};
   }
 `
 
@@ -78,17 +79,17 @@ const Dropzone = compose(
       onDrop(acceptedFiles, rejectedFiles)
     },
   })
-)(({ disabled, square, isDragging, onDragEnter, onDragLeave, previewImage, ...props }) => (
+)(({ disabled, square, isDragging, onDragEnter, onDragLeave, previewPicture, ...props }) => (
   <StyledDropzone
-    disableClick={disabled}
+    disableClick
     disabled={disabled}
     isDragging={isDragging}
     onDragEnter={onDragEnter}
     onDragLeave={onDragLeave}
-    previewImage={previewImage}
+    previewPicture={previewPicture}
     {...props}
   >
-    <Img alt="" square={square} src={previewImage} />
+    <Img alt="" square={square} src={previewPicture} />
     <InnerLabel square={square}>
       <CloudUpload />
       <div>{'Drop a file or click to select one'}</div>
@@ -106,15 +107,6 @@ const Img = styled.img`
   width: 100%;
 `
 
-const HiddenImg = styled.img`
-  display: none;
-`
-
-const StyledReactCrop = styled(ReactCrop)`
-  margin-top: 1rem;
-  border: 2px dashed #fff;
-`
-
 const StyledLinearProgress = styled(LinearProgress)`
   margin-bottom: 1rem;
 `
@@ -123,7 +115,7 @@ const StatusMessage = styled.div`
   font-size: 14px;
   letter-spacing: 0.3px;
   color: rgba(0, 0, 0, 0.87);
-  line-height: 1.4;
+  line-height: 2.4;
 `
 
 const RemoveButtonContainer = styled.div`
@@ -144,87 +136,91 @@ const ProgressBar = compose(
     return !shallowEqual(props.progress, nextProps.progress)
   })
 )(({ progress }) => (
-  <React.Fragment>
+  <div style={{ marginTop: '2rem' }}>
     <StatusMessage>{`${progress.message}...`}</StatusMessage>
     <StyledLinearProgress value={progress.progress} variant="determinate" />
-  </React.Fragment>
+  </div>
 ))
 
 const BarebonesPictureUploader = ({
   crop,
-  doneCropping,
   disabled,
-  image,
-  imagePreviewRef,
-  previewImage,
+  doneCropping,
+  label,
+  modalOpen,
   onCancelClick,
+  onClick,
   onCropChange,
   onCropComplete,
   onDoneClick,
   onDrop,
-  onImageLoaded,
-  onRemove,
-  square,
-  label,
+  onModalClose,
+  onPictureLoaded,
+  onRemovePicture,
+  picture,
+  picturePreviewRef,
+  previewPicture,
   required = false,
+  setModalOpen,
+  square,
 }) => (
-  <Container>
-    <FormControl disabled={disabled} fullWidth margin="normal">
-      <InputLabel shrink>{`${label}${required ? ' *' : ''}`}</InputLabel>
-      <Dropzone
-        accept="image/*"
-        disabled={disabled}
-        multiple={false}
-        onDrop={onDrop}
-        previewImage={previewImage}
-        square={square}
-      />
-    </FormControl>
-    {(image || previewImage) && (image ? doneCropping : true) && (
-      <RemoveButtonContainer>
-        <Button disabled={disabled} mini onClick={onRemove} style={{ color: theme.palette.error.main }} type="button">
-          <StyledDelete />
-          {'clear'}
-        </Button>
-      </RemoveButtonContainer>
-    )}
-    {image && !doneCropping && (
-      <React.Fragment>
-        <HiddenImg alt="" ref={imagePreviewRef} src={image.preview} />
-        <StyledReactCrop
-          crop={crop}
-          imageStyle={{ maxHeight: 'initial' }}
-          keepSelection
-          minHeight={20}
-          minWidth={20}
-          onChange={onCropChange}
-          onComplete={onCropComplete}
-          onImageLoaded={onImageLoaded}
-          src={image.preview}
+  <React.Fragment>
+    <PicturesModal
+      crop={crop}
+      croppingState={!doneCropping}
+      onCancelClick={onCancelClick}
+      onCropChange={onCropChange}
+      onCropComplete={onCropComplete}
+      onDoneClick={onDoneClick}
+      onDrop={onDrop}
+      onModalClose={onModalClose}
+      onPictureLoaded={onPictureLoaded}
+      open={modalOpen}
+      picturePreview={picture && picture.preview}
+      picturePreviewRef={picturePreviewRef}
+      previewPicture={previewPicture}
+      setOpen={setModalOpen}
+    />
+    <Container>
+      <FormControl disabled={disabled} fullWidth margin="normal">
+        <InputLabel shrink>{`${label}${required ? ' *' : ''}`}</InputLabel>
+        <Dropzone
+          accept="image/*"
+          disabled={disabled}
+          multiple={false}
+          onClick={onClick}
+          onDrop={onDrop}
+          previewPicture={previewPicture}
+          square={square}
         />
-        <div>
-          <p>{'Please crop the image (or leave as default).'}</p>
-          <Button color="primary" onClick={onDoneClick} type="button" variant="contained">
-            {'Done'}
+      </FormControl>
+      {(picture || previewPicture) && (picture ? doneCropping : true) && (
+        <RemoveButtonContainer>
+          <Button
+            disabled={disabled}
+            mini
+            onClick={onRemovePicture}
+            style={{ color: theme.palette.error.main }}
+            type="button"
+          >
+            <StyledDelete />
+            {'clear'}
           </Button>
-          <Button onClick={onCancelClick} style={{ marginLeft: '1rem' }} type="button">
-            {'Cancel'}
-          </Button>
-        </div>
-      </React.Fragment>
-    )}
-  </Container>
+        </RemoveButtonContainer>
+      )}
+    </Container>
+  </React.Fragment>
 )
 
-const defaultCrop = image => {
-  const imageAspect = image.width / image.height
+const defaultCrop = picture => {
+  const pictureAspect = picture.width / picture.height
   let height, width
-  if (imageAspect < 1) {
+  if (pictureAspect < 1) {
     width = 100
-    height = width * imageAspect
+    height = width * pictureAspect
   } else {
     height = 100
-    width = height / imageAspect
+    width = height / pictureAspect
   }
   return {
     aspect: 1,
@@ -235,13 +231,13 @@ const defaultCrop = image => {
   }
 }
 
-const previewCrop = (image, imageElement, pixelCrop) => {
+const previewCrop = async (picture, pictureElement, pixelCrop) => {
   const canvas = document.createElement('canvas')
   canvas.width = pixelCrop.width
   canvas.height = pixelCrop.height
   const ctx = canvas.getContext('2d')
   ctx.drawImage(
-    imageElement,
+    pictureElement,
     pixelCrop.x,
     pixelCrop.y,
     pixelCrop.width,
@@ -251,16 +247,16 @@ const previewCrop = (image, imageElement, pixelCrop) => {
     pixelCrop.width,
     pixelCrop.height
   )
-  return canvas.toDataURL(image.type)
+  return canvas.toDataURL(picture.type)
 }
 
-const resultingCrop = (image, imageElement, pixelCrop) => {
+const resultingCrop = (picture, pictureElement, pixelCrop) => {
   const canvas = document.createElement('canvas')
   canvas.width = pixelCrop.width
   canvas.height = pixelCrop.height
   const ctx = canvas.getContext('2d')
   ctx.drawImage(
-    imageElement,
+    pictureElement,
     pixelCrop.x,
     pixelCrop.y,
     pixelCrop.width,
@@ -270,10 +266,10 @@ const resultingCrop = (image, imageElement, pixelCrop) => {
     pixelCrop.width,
     pixelCrop.height
   )
-  return new Promise(resolve => canvas.toBlob(resolve, image.type))
+  return new Promise(resolve => canvas.toBlob(resolve, picture.type))
 }
 
-const uploadImage = async ({ blob, setProgress, type }) => {
+const uploadPicture = async ({ blob, setProgress, type }) => {
   try {
     const { fileUrl } = await S3Upload({
       contentDisposition: 'auto',
@@ -297,67 +293,95 @@ const PictureUploader = compose(
     const ignoreProps = ['setDisabled', 'setPic', 'onChange']
     return !shallowEqual(omit(props, ignoreProps), omit(nextProps, ignoreProps))
   }),
-  withProps({ imagePreviewRef: React.createRef() }),
-  withState('image', 'setImage', null),
+  withProps({ picturePreviewRef: React.createRef() }),
   withState('crop', 'setCrop', {}),
-  withState('croppedImage', 'setCroppedImage', null),
+  withState('croppedPicture', 'setCroppedPicture', null),
   withState('doneCropping', 'setDoneCropping', true),
+  withState('picture', 'setPicture', null),
   withState('previousValue', 'setPreviousValue', null),
-  withProps(({ croppedImage, image, value }) => ({
-    previewImage: value ? value : croppedImage ? croppedImage : image ? image.preview : '',
+  withState('modalOpen', 'setModalOpen', false),
+  withProps(({ croppedPicture, picture, value }) => ({
+    previewPicture: value ? value : croppedPicture ? croppedPicture : picture ? picture.preview : '',
   })),
   withHandlers({
-    onCancelClick: ({ image, previousValue, onChange, setCroppedImage, setDoneCropping, setImage }) => () => {
-      onChange(previousValue)
-      setImage(null)
-      setCroppedImage(null)
+    onCancelClick: ({
+      picture,
+      previousValue,
+      onChange,
+      setCroppedPicture,
+      setDoneCropping,
+      setModalOpen,
+      setPicture,
+    }) => () => {
       setDoneCropping(true)
-      URL.revokeObjectURL(image.preview)
+      onChange(previousValue)
+      setPicture(null)
+      setCroppedPicture(null)
+      setModalOpen(false)
+      URL.revokeObjectURL(picture.preview)
     },
-    onCropChange: ({ setCrop }) => crop => setCrop(crop),
-    onCropComplete: ({ image, imagePreviewRef, onChange, setCrop, setCroppedImage }) => (_crop, pixelCrop) => {
+    onClick: ({ setModalOpen }) => () => {
+      setModalOpen(true)
+    },
+    onCropChange: ({ setCrop }) => crop => {
+      setCrop(crop)
+    },
+    onCropComplete: ({ picture, picturePreviewRef, onChange, setCrop, setCroppedPicture }) => async (
+      _crop,
+      pixelCrop
+    ) => {
       if (pixelCrop.height === 0 || pixelCrop.width === 0) {
-        const crop = defaultCrop(imagePreviewRef.current)
+        const crop = defaultCrop(picturePreviewRef.current)
         setCrop(crop)
-        pixelCrop = getPixelCrop(imagePreviewRef.current, crop)
+        pixelCrop = getPixelCrop(picturePreviewRef.current, crop)
       }
-      const previewUrl = previewCrop(image, imagePreviewRef.current, pixelCrop)
-      setCroppedImage(previewUrl)
+      const previewUrl = await previewCrop(picture, picturePreviewRef.current, pixelCrop)
+      setCroppedPicture(previewUrl)
       onChange(previewUrl)
     },
-    onDoneClick: ({ crop, image, imagePreviewRef, setDoneCropping, setPic }) => async () => {
+    onDoneClick: ({ crop, picture, picturePreviewRef, setDoneCropping, setModalOpen, setPic }) => async () => {
       setDoneCropping(true)
-      const blob = await resultingCrop(image, imagePreviewRef.current, getPixelCrop(imagePreviewRef.current, crop))
-      blob.name = image.name
+      setModalOpen(false)
+      const blob = await resultingCrop(
+        picture,
+        picturePreviewRef.current,
+        getPixelCrop(picturePreviewRef.current, crop)
+      )
+      blob.name = picture.name
       setPic(blob)
-      URL.revokeObjectURL(image.preview)
+      URL.revokeObjectURL(picture.preview)
     },
-    onDrop: ({ onChange, setDoneCropping, setImage, setPreviousValue, value }) => files => {
-      setPreviousValue(value)
-      onChange('')
+    onDrop: ({ onChange, setDoneCropping, setModalOpen, setPicture, setPreviousValue, value }) => files => {
       setDoneCropping(false)
+      onChange('')
+      setPreviousValue(value)
       if (files.length === 0) return
       const file = files[0]
-      setImage({
+      setPicture({
         name: file.name,
         preview: URL.createObjectURL(file),
         size: file.size,
         type: file.type,
       })
+      setModalOpen(true)
     },
-    onImageLoaded: ({ image, imagePreviewRef, onChange, setCrop, setCroppedImage }) => imageElement => {
-      const crop = defaultCrop(imageElement)
+    onModalClose: ({ onChange, setModalOpen }) => pictureUrl => {
+      setModalOpen(false)
+      onChange(pictureUrl)
+    },
+    onPictureLoaded: ({ picture, picturePreviewRef, onChange, setCrop, setCroppedPicture }) => async pictureElement => {
+      const crop = defaultCrop(pictureElement)
       setCrop(crop)
-      const pixelCrop = getPixelCrop(imageElement, crop)
-      const previewUrl = previewCrop(image, imagePreviewRef.current, pixelCrop)
-      setCroppedImage(previewUrl)
+      const pixelCrop = getPixelCrop(pictureElement, crop)
+      const previewUrl = await previewCrop(picture, picturePreviewRef.current, pixelCrop)
+      setCroppedPicture(previewUrl)
       onChange(previewUrl)
     },
-    onRemove: ({ onChange, setImage, setCroppedImage, setPic }) => () => {
-      setImage(null)
+    onRemovePicture: ({ onChange, setPicture, setCroppedPicture, setPic }) => () => {
       onChange('')
+      setPicture(null)
       setPic(null)
-      setCroppedImage(null)
+      setCroppedPicture(null)
     },
   }),
   lifecycle({
@@ -367,11 +391,11 @@ const PictureUploader = compose(
         setDisabled(!doneCropping)
     },
     componentWillUnmount() {
-      const { image } = this.props
-      image && URL.revokeObjectURL(image.preview)
+      const { picture } = this.props
+      picture && URL.revokeObjectURL(picture.preview)
     },
   })
 )(BarebonesPictureUploader)
 
-export { uploadImage, ProgressBar }
+export { uploadPicture, ProgressBar }
 export default PictureUploader
