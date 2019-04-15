f3b477152f94bf35df98b684af7c6ef0d5cb6d8b
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-04-15 13:32:12 +0100

    Fix re-order to 1st position bug

--- backend/test/models/simple_chat_step_test.rb
+++ backend/test/models/simple_chat_step_test.rb
@@ -1,18 +1,33 @@
 require "test_helper"
 
 class SimpleChatStepTest < ActiveSupport::TestCase
-  test "duplicated simple chat preserves order of simple chat steps" do
+  test "simple chat steps sorted after create, without order field" do
     ActsAsTenant.default_tenant = Account.create!
 
     simple_chat = create(:simple_chat)
 
     create(:simple_chat_step, simple_chat: simple_chat)
+    create(:simple_chat_step, simple_chat: simple_chat, key: Faker::Lorem.sentence)
+    create(:simple_chat_step, simple_chat: simple_chat, key: Faker::Lorem.sentence)
+    create(:simple_chat_step, simple_chat: simple_chat, key: Faker::Lorem.sentence)
+
+    result = simple_chat.simple_chat_steps.each(&:attributes).pluck(:order)
+
+    assert_equal [1, 2, 3, 4], result
+  end
+
+  test "simple chat messages steps after create, with order field" do
+    ActsAsTenant.default_tenant = Account.create!
+
+    simple_chat = create(:simple_chat)
+
     create(:simple_chat_step, simple_chat: simple_chat, order: 2, key: Faker::Lorem.sentence)
     create(:simple_chat_step, simple_chat: simple_chat, order: 8, key: Faker::Lorem.sentence)
+    create(:simple_chat_step, simple_chat: simple_chat, order: 1)
     create(:simple_chat_step, simple_chat: simple_chat, order: 3, key: Faker::Lorem.sentence)
 
     result = simple_chat.simple_chat_steps.each(&:attributes).pluck(:order)
 
-    assert_equal [1, 2, 8, 3], result
+    assert_equal [2, 8, 1, 3], result
   end
 end
