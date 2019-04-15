7d33570e588c44df9215e58898c4526e7cb57243
Duarte Henriques <duarte.henriques@gmail.com>
2019-04-15 18:23:23 +0100

    Try upgraded ruby version

--- integrations/shopify/app/jobs/app_uninstalled_job.rb
+++ integrations/shopify/app/jobs/app_uninstalled_job.rb
@@ -1,5 +1,5 @@
 class AppUninstalledJob < ActiveJob::Base
-  def perform(shop_domain:, webhook:)
+  def perform(shop_domain:)
     # webhook for when app is uninstalled, could be used to delete all products
 
     shop = Shop.find_by(shopify_domain: shop_domain)
