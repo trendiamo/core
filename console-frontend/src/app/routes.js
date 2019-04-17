4470605b2696093d191f6b269025d8684a22255f
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-04-17 18:25:34 +0100

    Add "Save & New" button to triggers form

--- console-frontend/src/app/routes.js
+++ console-frontend/src/app/routes.js
@@ -77,6 +77,9 @@ const routes = {
   admin() {
     return '/admin'
   },
+  nullRoute() {
+    return '/empty'
+  },
 }
 
 export default routes
--- console-frontend/src/shared/form-elements/actions.js
+++ console-frontend/src/shared/form-elements/actions.js
@@ -1,17 +1,76 @@
+import ActionsMenu from './simple-menu'
 import React from 'react'
 import SaveButton from './save-button'
+import styled from 'styled-components'
+import withWidth from '@material-ui/core/withWidth'
 
-const Actions = ({ tooltipEnabled, isFormSubmitting, onFormSubmit, saveDisabled, isFormPristine, tooltipText }) => (
+const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-end;
+  @media (min-width: 800px) {
+    flex-flow: row;
+  }
+`
+
+const FlexDiv = styled.div`
+  flex: 1;
+  margin: 2px 5px;
+  white-space: nowrap;
+`
+
+const actions = [{ label: 'Save', color: 'primaryGradient' }, { label: 'Save & New', color: 'actions' }]
+
+const Actions = ({
+  tooltipEnabled,
+  isFormSubmitting,
+  onFormSubmit,
+  saveDisabled,
+  isFormPristine,
+  tooltipText,
+  saveAndCreateNewEnabled,
+  width,
+}) => (
   <React.Fragment>
-    <SaveButton
-      disabled={saveDisabled}
-      isFormPristine={isFormPristine}
-      isFormSubmitting={isFormSubmitting}
-      onClick={onFormSubmit}
-      tooltipEnabled={tooltipEnabled}
-      tooltipText={tooltipText}
-    />
+    {saveAndCreateNewEnabled ? (
+      width === 'sm' || width === 'xs' ? (
+        <ActionsMenu
+          actions={actions}
+          disabled={saveDisabled}
+          isFormPristine={isFormPristine}
+          isFormSubmitting={isFormSubmitting}
+          onFormSubmit={onFormSubmit}
+        />
+      ) : (
+        <Container>
+          {actions.map((action, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <FlexDiv key={index}>
+              <SaveButton
+                color={action.color}
+                disabled={saveDisabled}
+                isFormPristine={isFormPristine}
+                isFormSubmitting={isFormSubmitting}
+                message={action.label}
+                onClick={event => onFormSubmit(event, action.label)}
+                tooltipEnabled={tooltipEnabled}
+                tooltipText={tooltipText}
+              />
+            </FlexDiv>
+          ))}
+        </Container>
+      )
+    ) : (
+      <SaveButton
+        disabled={saveDisabled}
+        isFormPristine={isFormPristine}
+        isFormSubmitting={isFormSubmitting}
+        onClick={onFormSubmit}
+        tooltipEnabled={tooltipEnabled}
+        tooltipText={tooltipText}
+      />
+    )}
   </React.Fragment>
 )
 
-export default Actions
+export default withWidth()(Actions)
