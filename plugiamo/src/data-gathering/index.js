4ba9c5874d5149f35fa37d9b7a1d10c2a75763a2
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-04-29 12:20:19 +0100

    Track impressorajato 'add to cart' and 'go to checkout' events

--- plugiamo/src/data-gathering/index.js
+++ plugiamo/src/data-gathering/index.js
@@ -1,4 +1,5 @@
 import buttwrapDataGathering from './buttwrap'
+import impressorajatoDataGathering from './impressorajato'
 import pierrecardinDataGathering from './pierre-cardin'
 import shopinfoDataGathering from './shopinfo'
 
@@ -6,6 +7,7 @@ const dataGatheringFactory = {
   'www.buttwrap.com': buttwrapDataGathering,
   'www.pierre-cardin.de': pierrecardinDataGathering,
   'www.shopinfo.com.br': shopinfoDataGathering,
+  'www.impressorajato.com.br': impressorajatoDataGathering,
 }
 
 const dataGathering = dataGatheringFactory[window.location.hostname]
