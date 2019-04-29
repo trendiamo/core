423320448c72bd7a36b6f32562e8c2c2a53ded3e
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-04-29 12:20:19 +0100

    Track impressorajato 'add to cart' and 'go to checkout' events

---
+++ plugiamo/src/data-gathering/impressorajato.js
@@ -0,0 +1,104 @@
+import mixpanel from 'ext/mixpanel'
+/* eslint-disable no-undef */
+
+const convertToCents = selector => {
+  return Number(selector.replace(/\D/g, ''))
+}
+
+export default {
+  addToCartObject() {
+    const formFields = jQuery
+      .noConflict()('#product_addtocart_form')
+      .serializeArray()
+
+    return {
+      name: 'Add To Cart',
+      data: {
+        hostname: location.hostname,
+        withPlugin: !!jQuery.noConflict()('.trendiamo-container')[0],
+        productId: formFields.find(element => element.name === 'product').value,
+        productName: jQuery
+          .noConflict()(".product-name > [itemprop='name']")
+          .text(),
+        productBrand: jQuery
+          .noConflict()('.fabricante-legenda > h2')
+          .text(),
+        currency: 'BRL',
+        subTotalInCents: {
+          noBoleto: convertToCents(jQuery.noConflict()('.price-boleto')[0].innerText),
+          inTenInstalments: convertToCents(
+            jQuery
+              .noConflict()("[data-id='price_parcelado']")
+              .text()
+          ),
+          aVistaNoCartao: convertToCents(
+            jQuery
+              .noConflict()("[data-id='product-price'] > .price")
+              .text()
+          ),
+        },
+      },
+    }
+  },
+  getProductsFromCart() {
+    return jQuery
+      .noConflict()('#shopping-cart-table > tbody')
+      .find('tr')
+      .map((index, element) => {
+        const productName = jQuery
+          .noConflict()(element)
+          .find('h2 > a')
+          .attr('title')
+        const productUrl = jQuery
+          .noConflict()(element)
+          .find('h2 > a')
+          .attr('href')
+        const productQuantity = Number(
+          jQuery
+            .noConflict()(element)
+            .find("[selected='selected']")
+            .attr('value')
+        )
+        const productPrice = convertToCents(element.children[2].innerText)
+        return { productName, productUrl, productPrice, productQuantity }
+      })
+      .toArray()
+  },
+  checkoutObject() {
+    return {
+      name: 'Proceed To Checkout',
+      data: {
+        hostname: location.hostname,
+        withPlugin: !!jQuery.noConflict()('.trendiamo-container')[0],
+        products: this.getProductsFromCart(),
+        currency: 'BRL',
+        subTotalInCents: convertToCents(
+          jQuery
+            .noConflict()('#shopping-cart-totals-table')
+            .find('.price')[0].innerText
+        ),
+      },
+    }
+  },
+  setupDataGathering() {
+    const _this = this
+    if (jQuery.noConflict()('.product-view').length === 1) {
+      // pdp
+      jQuery
+        .noConflict()('.product-shop')
+        .find('.button')
+        .on('click', () => {
+          const json = _this.addToCartObject()
+          mixpanel.track(json.name, json.data)
+        })
+    } else if (location.pathname.match(/^\/checkout\/cart\/?$/)) {
+      // cart
+      jQuery
+        .noConflict()('.btn-concluir-compra')
+        .on('click', () => {
+          const json = _this.checkoutObject()
+          mixpanel.track(json.name, json.data)
+        })
+    }
+  },
+}
