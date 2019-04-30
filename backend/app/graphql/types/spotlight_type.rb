516dc06263b2c934be3bf9b0c73965163cb2027b
Daniel Bugge <danielbugge96@gmail.com>
2019-04-30 14:10:25 +0100

    Implement order field tracking in Persona Clicked event

--- backend/app/graphql/types/spotlight_type.rb
+++ backend/app/graphql/types/spotlight_type.rb
@@ -2,6 +2,7 @@ Types::SpotlightType = GraphQL::ObjectType.define do
   name "Spotlight"
 
   field :id, !types.ID
+  field :order, types.Int
   field :persona, !Types::PersonaType do
     resolve ->(obj, _args, _ctx) {
       obj.persona
