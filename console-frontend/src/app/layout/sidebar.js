d26e73b782c3922117024192bff779ae47cac975
Duarte Henriques <duarte.henriques@gmail.com>
2019-05-06 18:59:10 +0100

    Use less recompose

--- console-frontend/src/app/layout/sidebar.js
+++ console-frontend/src/app/layout/sidebar.js
@@ -1,50 +1,51 @@
 import classNames from 'classnames'
-import React from 'react'
-import { compose, lifecycle, withState } from 'recompose'
+import React, { useEffect, useState } from 'react'
 import { Drawer, Hidden, withWidth } from '@material-ui/core'
 
-const Sidebar = ({ children, classes, sidebarOpen, toggleOpen, menuLoaded }) => (
-  <>
-    <Hidden implementation="js" mdUp>
-      <div className={classNames(classes.drawerGhost)} />
-      <Drawer
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        ModalProps={{
-          keepMounted: true, // Better open performance on mobile.
-        }}
-        onClose={toggleOpen}
-        open={sidebarOpen}
-        variant="temporary"
-      >
-        {React.cloneElement(children, { menuLoaded })}
-      </Drawer>
-    </Hidden>
-    <Hidden implementation="js" smDown>
-      <div className={classNames(classes.drawerGhost, !sidebarOpen && classes.drawerGhostClosed)} />
-      <Drawer
-        classes={{
-          paper: sidebarOpen ? classes.drawerPaper : classes.drawerPaperClose,
-        }}
-        open={sidebarOpen}
-        variant="permanent"
-      >
-        {React.cloneElement(children, { menuLoaded })}
-      </Drawer>
-    </Hidden>
-  </>
-)
+const Sidebar = ({ children, classes, sidebarOpen, toggleOpen }) => {
+  const [menuLoaded, setMenuLoaded] = useState(false)
 
-export default compose(
-  withState('menuLoaded', 'setMenuLoaded', false),
-  lifecycle({
-    componentDidMount() {
-      setTimeout(() => {
-        const { setMenuLoaded } = this.props
-        setMenuLoaded(true)
-      }, 2000)
-    },
-  }),
-  withWidth({ resizeInterval: Infinity, noSSR: true }) // used to initialize the visibility on first render
-)(Sidebar)
+  useEffect(() => {
+    let didCancel = false
+    setTimeout(() => {
+      didCancel || setMenuLoaded(true)
+    }, 2000)
+    return () => (didCancel = true)
+  }, [])
+
+  return (
+    <>
+      <Hidden implementation="js" mdUp>
+        <div className={classNames(classes.drawerGhost)} />
+        <Drawer
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          onClose={toggleOpen}
+          open={sidebarOpen}
+          variant="temporary"
+        >
+          {React.cloneElement(children, { menuLoaded })}
+        </Drawer>
+      </Hidden>
+      <Hidden implementation="js" smDown>
+        <div className={classNames(classes.drawerGhost, !sidebarOpen && classes.drawerGhostClosed)} />
+        <Drawer
+          classes={{
+            paper: sidebarOpen ? classes.drawerPaper : classes.drawerPaperClose,
+          }}
+          open={sidebarOpen}
+          variant="permanent"
+        >
+          {React.cloneElement(children, { menuLoaded })}
+        </Drawer>
+      </Hidden>
+    </>
+  )
+}
+
+// withWidth used to initialize the visibility on first render
+export default withWidth({ resizeInterval: Infinity, noSSR: true })(Sidebar)
