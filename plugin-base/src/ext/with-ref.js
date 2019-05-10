9121fc25b97791ee6531c632e4f079b31a3e6a12
Duarte Henriques <duarte.henriques@gmail.com>
2019-05-10 15:10:08 +0100

    Fix withRef for admin previews

--- plugin-base/src/ext/with-ref.js
+++ plugin-base/src/ext/with-ref.js
@@ -1,6 +1,11 @@
+import isEqual from 'lodash.isequal'
 import { compose, withHandlers } from 'recompose'
 
-const getRef = element => (element && element.base ? getRef(element.base) : element)
+const getRef = element => {
+  // this weirdness required to support showcase preview
+  if (!element || isEqual(element, { base: null })) return null
+  return element.base ? getRef(element.base) : element
+}
 
 const withRef = (refName, setRefName) => BaseComponent =>
   compose(
