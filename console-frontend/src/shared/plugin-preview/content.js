1cb31b214254a3369fd3c502cf42a6c7e675e308
Duarte Henriques <duarte.henriques@gmail.com>
2019-05-10 15:10:18 +0100

    Use less recompose

--- console-frontend/src/shared/plugin-preview/content.js
+++ console-frontend/src/shared/plugin-preview/content.js
@@ -1,10 +1,9 @@
 import omit from 'lodash.omit'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
-import { branch, compose, lifecycle, renderNothing, withProps, withState } from 'recompose'
 import { ContentWrapper, FrameBase } from 'plugin-base'
 
-const ContentContainerTemplate = styled(props => <FrameBase {...omit(props, ['entry', 'setEntry', 'isUnmounting'])} />)`
+const StyledFrameBase = styled(props => <FrameBase {...omit(props, ['isEntry', 'isUnmounting'])} />)`
   border: 0;
   overflow: hidden;
   border-radius: 8px;
@@ -13,31 +12,36 @@ const ContentContainerTemplate = styled(props => <FrameBase {...omit(props, ['en
   box-shadow: 0 5px 40px rgba(0, 0, 0, 0.16);
   position: relative;
   margin-bottom: 30px;
-  opacity: ${({ entry }) => (entry ? 0 : 1)};
-  transform: ${({ entry, isUnmounting }) => (entry || isUnmounting ? 'translateY(20px)' : 'none')};
+  opacity: ${({ isEntry }) => (isEntry ? 0 : 1)};
+  transform: ${({ isEntry, isUnmounting }) => (isEntry || isUnmounting ? 'translateY(20px)' : 'none')};
   transition: opacity 0.25s ease, transform 0.4s ease;
   z-index: 10;
 `
 
-const ContentContainer = compose(
-  withState('entry', 'setEntry', true),
-  lifecycle({
-    componentDidMount() {
-      const { setEntry } = this.props
-      setTimeout(() => {
-        setEntry(false)
-      }, 10)
-    },
-  })
-)(ContentContainerTemplate)
+const ContentContainer = ({ iframeRef, children }) => {
+  const [isEntry, setIsEntry] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsEntry(false)
+    }, 10)
+  }, [])
 
-const Content = ({ contentRef, Base }) => (
-  <ContentContainer iframeRef={contentRef}>
-    <ContentWrapper>{Base}</ContentWrapper>
-  </ContentContainer>
-)
+  return (
+    <StyledFrameBase iframeRef={iframeRef} isEntry={isEntry}>
+      {children}
+    </StyledFrameBase>
+  )
+}
 
-export default compose(
-  branch(({ showingContent }) => !showingContent, renderNothing),
-  withProps({ contentRef: React.createRef() })
-)(Content)
+const Content = ({ Base, showingContent }) => {
+  const contentRef = useRef
+  if (!showingContent) return null
+
+  return (
+    <ContentContainer iframeRef={contentRef}>
+      <ContentWrapper>{Base}</ContentWrapper>
+    </ContentContainer>
+  )
+}
+
+export default Content
