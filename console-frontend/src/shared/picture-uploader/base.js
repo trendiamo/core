dcd1005f4780a25c2e549eaad6040b5621b781f8
Duarte Henriques <duarte.henriques@gmail.com>
2019-05-13 14:01:33 +0100

    Refactor PictureUploader

---
+++ console-frontend/src/shared/picture-uploader/base.js
@@ -0,0 +1,189 @@
+import CloudUpload from '@material-ui/icons/CloudUpload'
+import Delete from '@material-ui/icons/Delete'
+import omit from 'lodash.omit'
+import PicturesModal from 'shared/pictures-modal'
+import React from 'react'
+import ReactDropzone from 'react-dropzone'
+import styled from 'styled-components'
+import theme from 'app/theme'
+import { Button, FormControl, InputLabel } from '@material-ui/core'
+import { compose, withHandlers, withState } from 'recompose'
+
+const Container = styled.div`
+  margin-bottom: 1rem;
+  max-width: 400px;
+`
+
+const InnerLabel = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  letter-spacing: 0.3px;
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${({ square }) => (square ? 'none' : '50%')};
+`
+
+const StyledDelete = styled(Delete)`
+  margin-bottom: 2px;
+`
+
+const FilteredReactDropzone = props => (
+  <ReactDropzone {...omit(props, ['isDragging', 'onFileUpload', 'previewPicture', 'setIsDragging'])} />
+)
+
+const StyledDropzone = styled(FilteredReactDropzone)`
+  border-width: 0;
+  cursor: pointer;
+  position: relative;
+  height: 150px;
+  width: 150px;
+  margin-top: 25px;
+  margin-bottom: 0.4rem;
+  color: ${({ isDragging, previewPicture }) => (previewPicture ? '#fff' : isDragging ? '#ff6641' : '#7f8086')};
+
+  ${InnerLabel} {
+    visibility: ${({ isDragging, previewPicture }) => (isDragging || !previewPicture ? 'visible' : 'hidden')};
+    background-color: ${({ isDragging, previewPicture }) =>
+      isDragging && previewPicture ? 'rgba(0, 0, 0, 0.6)' : 'transparent'};
+  }
+
+  &:hover ${InnerLabel} {
+    visibility: visible;
+    background-color: ${({ previewPicture }) => (previewPicture ? 'rgba(0, 0, 0, 0.6)' : 'transparent')};
+  }
+`
+
+const Img = styled.img`
+  background-color: ${({ src }) => (src ? 'transparent' : '#f5f5f5')};
+  border: ${({ src }) => (src ? 'none' : 'dashed 2px')};
+  border-radius: ${({ square }) => (square ? 'none' : '50%')};
+  display: block;
+  height: 100%;
+  position: relative;
+  width: 100%;
+`
+
+const Dropzone = compose(
+  withState('isDragging', 'setIsDragging', false),
+  withHandlers({
+    onDragEnter: ({ setIsDragging }) => () => {
+      setIsDragging(true)
+    },
+    onDragLeave: ({ setIsDragging }) => () => {
+      setIsDragging(false)
+    },
+    onDrop: ({ onFileUpload, setIsDragging }) => (acceptedFiles, rejectedFiles) => {
+      setIsDragging(false)
+      onFileUpload(acceptedFiles, rejectedFiles)
+    },
+  })
+)(({ disabled, square, isDragging, onDragEnter, onDragLeave, previewPicture, ...props }) => (
+  <StyledDropzone
+    disableClick
+    disabled={disabled}
+    isDragging={isDragging}
+    onDragEnter={onDragEnter}
+    onDragLeave={onDragLeave}
+    previewPicture={previewPicture}
+    {...props}
+  >
+    <Img alt="" square={square} src={previewPicture} />
+    <InnerLabel square={square}>
+      <CloudUpload />
+      <div>{'Drop a file or click to select one'}</div>
+    </InnerLabel>
+  </StyledDropzone>
+))
+
+const RemoveButtonContainer = styled.div`
+  text-align: center;
+  width: 150px;
+
+  button {
+    font-size: 11px;
+  }
+  svg {
+    font-size: 17px;
+    margin-right: 4px;
+  }
+`
+
+const BasePictureUploader = ({
+  crop,
+  disabled,
+  doneCropping,
+  label,
+  modalOpen,
+  onCancelClick,
+  onClick,
+  onCropChange,
+  onCropComplete,
+  onDoneClick,
+  onFileUpload,
+  onModalClose,
+  onPictureLoaded,
+  onRemovePicture,
+  picture,
+  picturePreviewRef,
+  previewPicture,
+  required = false,
+  setModalOpen,
+  square,
+}) => (
+  <>
+    <PicturesModal
+      crop={crop}
+      croppingState={!doneCropping}
+      onCancelClick={onCancelClick}
+      onCropChange={onCropChange}
+      onCropComplete={onCropComplete}
+      onDoneClick={onDoneClick}
+      onFileUpload={onFileUpload}
+      onModalClose={onModalClose}
+      onPictureLoaded={onPictureLoaded}
+      open={modalOpen}
+      picturePreview={picture && picture.preview}
+      picturePreviewRef={picturePreviewRef}
+      previewPicture={previewPicture}
+      setOpen={setModalOpen}
+    />
+    <Container>
+      <FormControl disabled={disabled} fullWidth margin="normal">
+        <InputLabel shrink>{`${label}${required ? ' *' : ''}`}</InputLabel>
+        <Dropzone
+          accept="image/*"
+          disabled={disabled}
+          multiple={false}
+          onClick={onClick}
+          onFileUpload={onFileUpload}
+          previewPicture={previewPicture}
+          square={square}
+        />
+      </FormControl>
+      {(picture || previewPicture) && (picture ? doneCropping : true) && (
+        <RemoveButtonContainer>
+          <Button
+            disabled={disabled}
+            mini
+            onClick={onRemovePicture}
+            style={{ color: theme.palette.error.main }}
+            type="button"
+          >
+            <StyledDelete />
+            {'clear'}
+          </Button>
+        </RemoveButtonContainer>
+      )}
+    </Container>
+  </>
+)
+
+export default BasePictureUploader
--- console-frontend/src/shared/picture-uploader/index.js
+++ console-frontend/src/shared/picture-uploader/index.js
@@ -1,111 +1,13 @@
-import CloudUpload from '@material-ui/icons/CloudUpload'
-import Delete from '@material-ui/icons/Delete'
+import BasePictureUploader from './base'
 import omit from 'lodash.omit'
-import PicturesModal from './pictures-modal'
 import React from 'react'
-import ReactDropzone from 'react-dropzone'
 import S3Upload from 'ext/react-s3-uploader'
 import styled from 'styled-components'
-import theme from 'app/theme'
 import { apiGetSignedUrlFactory } from 'utils'
-import { Button, FormControl, InputLabel, LinearProgress } from '@material-ui/core'
 import { compose, lifecycle, shallowEqual, shouldUpdate, withHandlers, withProps, withState } from 'recompose'
 import { getPixelCrop } from 'react-image-crop'
+import { LinearProgress } from '@material-ui/core'
 import 'react-image-crop/dist/ReactCrop.css'
-
-const Container = styled.div`
-  margin-bottom: 1rem;
-  max-width: 400px;
-`
-
-const InnerLabel = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-
-  letter-spacing: 0.3px;
-  text-align: center;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border-radius: ${({ square }) => (square ? 'none' : '50%')};
-`
-
-const StyledDelete = styled(Delete)`
-  margin-bottom: 2px;
-`
-
-const FilteredReactDropzone = props => (
-  <ReactDropzone {...omit(props, ['isDragging', 'onFileUpload', 'previewPicture', 'setIsDragging'])} />
-)
-
-const StyledDropzone = styled(FilteredReactDropzone)`
-  border-width: 0;
-  cursor: pointer;
-  position: relative;
-  height: 150px;
-  width: 150px;
-  margin-top: 25px;
-  margin-bottom: 0.4rem;
-  color: ${({ isDragging, previewPicture }) => (previewPicture ? '#fff' : isDragging ? '#ff6641' : '#7f8086')};
-
-  ${InnerLabel} {
-    visibility: ${({ isDragging, previewPicture }) => (isDragging || !previewPicture ? 'visible' : 'hidden')};
-    background-color: ${({ isDragging, previewPicture }) =>
-      isDragging && previewPicture ? 'rgba(0, 0, 0, 0.6)' : 'transparent'};
-  }
-
-  &:hover ${InnerLabel} {
-    visibility: visible;
-    background-color: ${({ previewPicture }) => (previewPicture ? 'rgba(0, 0, 0, 0.6)' : 'transparent')};
-  }
-`
-
-const Dropzone = compose(
-  withState('isDragging', 'setIsDragging', false),
-  withHandlers({
-    onDragEnter: ({ setIsDragging }) => () => {
-      setIsDragging(true)
-    },
-    onDragLeave: ({ setIsDragging }) => () => {
-      setIsDragging(false)
-    },
-    onDrop: ({ onFileUpload, setIsDragging }) => (acceptedFiles, rejectedFiles) => {
-      setIsDragging(false)
-      onFileUpload(acceptedFiles, rejectedFiles)
-    },
-  })
-)(({ disabled, square, isDragging, onDragEnter, onDragLeave, previewPicture, ...props }) => (
-  <StyledDropzone
-    disableClick
-    disabled={disabled}
-    isDragging={isDragging}
-    onDragEnter={onDragEnter}
-    onDragLeave={onDragLeave}
-    previewPicture={previewPicture}
-    {...props}
-  >
-    <Img alt="" square={square} src={previewPicture} />
-    <InnerLabel square={square}>
-      <CloudUpload />
-      <div>{'Drop a file or click to select one'}</div>
-    </InnerLabel>
-  </StyledDropzone>
-))
-
-const Img = styled.img`
-  background-color: ${({ src }) => (src ? 'transparent' : '#f5f5f5')};
-  border: ${({ src }) => (src ? 'none' : 'dashed 2px')};
-  border-radius: ${({ square }) => (square ? 'none' : '50%')};
-  display: block;
-  height: 100%;
-  position: relative;
-  width: 100%;
-`
 
 const StyledLinearProgress = styled(LinearProgress)`
   margin-bottom: 1rem;
@@ -118,19 +20,6 @@ const StatusMessage = styled.div`
   line-height: 2.4;
 `
 
-const RemoveButtonContainer = styled.div`
-  text-align: center;
-  width: 150px;
-
-  button {
-    font-size: 11px;
-  }
-  svg {
-    font-size: 17px;
-    margin-right: 4px;
-  }
-`
-
 const ProgressBar = compose(
   shouldUpdate((props, nextProps) => {
     return !shallowEqual(props.progress, nextProps.progress)
@@ -141,76 +30,6 @@ const ProgressBar = compose(
     <StyledLinearProgress value={progress.progress} variant="determinate" />
   </div>
 ))
-
-const BarebonesPictureUploader = ({
-  crop,
-  disabled,
-  doneCropping,
-  label,
-  modalOpen,
-  onCancelClick,
-  onClick,
-  onCropChange,
-  onCropComplete,
-  onDoneClick,
-  onFileUpload,
-  onModalClose,
-  onPictureLoaded,
-  onRemovePicture,
-  picture,
-  picturePreviewRef,
-  previewPicture,
-  required = false,
-  setModalOpen,
-  square,
-}) => (
-  <>
-    <PicturesModal
-      crop={crop}
-      croppingState={!doneCropping}
-      onCancelClick={onCancelClick}
-      onCropChange={onCropChange}
-      onCropComplete={onCropComplete}
-      onDoneClick={onDoneClick}
-      onFileUpload={onFileUpload}
-      onModalClose={onModalClose}
-      onPictureLoaded={onPictureLoaded}
-      open={modalOpen}
-      picturePreview={picture && picture.preview}
-      picturePreviewRef={picturePreviewRef}
-      previewPicture={previewPicture}
-      setOpen={setModalOpen}
-    />
-    <Container>
-      <FormControl disabled={disabled} fullWidth margin="normal">
-        <InputLabel shrink>{`${label}${required ? ' *' : ''}`}</InputLabel>
-        <Dropzone
-          accept="image/*"
-          disabled={disabled}
-          multiple={false}
-          onClick={onClick}
-          onFileUpload={onFileUpload}
-          previewPicture={previewPicture}
-          square={square}
-        />
-      </FormControl>
-      {(picture || previewPicture) && (picture ? doneCropping : true) && (
-        <RemoveButtonContainer>
-          <Button
-            disabled={disabled}
-            mini
-            onClick={onRemovePicture}
-            style={{ color: theme.palette.error.main }}
-            type="button"
-          >
-            <StyledDelete />
-            {'clear'}
-          </Button>
-        </RemoveButtonContainer>
-      )}
-    </Container>
-  </>
-)
 
 const defaultCrop = picture => {
   const pictureAspect = picture.width / picture.height
@@ -395,7 +214,7 @@ const PictureUploader = compose(
       picture && URL.revokeObjectURL(picture.preview)
     },
   })
-)(BarebonesPictureUploader)
+)(BasePictureUploader)
 
 export { uploadPicture, ProgressBar }
 export default PictureUploader
