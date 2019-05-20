c6ab767b401ea075129ab2715ffa89825865e9ed
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-05-20 16:08:20 +0100

    Track baldessarini page events

--- plugiamo/src/data-gathering/index.js
+++ plugiamo/src/data-gathering/index.js
@@ -1,3 +1,4 @@
+import baldessariniDataGathering from './baldessarini'
 import buttwrapDataGathering from './buttwrap'
 import impressorajatoDataGathering from './impressorajato'
 import pierrecardinDataGathering from './pierre-cardin'
@@ -8,6 +9,7 @@ const dataGatheringFactory = {
   'www.pierre-cardin.de': pierrecardinDataGathering,
   'www.shopinfo.com.br': shopinfoDataGathering,
   'www.impressorajato.com.br': impressorajatoDataGathering,
+  'www.baldessarini.com': baldessariniDataGathering,
 }
 
 const dataGathering = dataGatheringFactory[window.location.hostname]
