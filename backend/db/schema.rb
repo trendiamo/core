526c392e48d6b24747e0dc49a8ba60463d84ef77
Daniel Bugge <danielbugge96@gmail.com>
2019-05-20 16:42:53 +0100

    Remove default simple chat message text type

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190514154758) do
+ActiveRecord::Schema.define(version: 20190520153252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,7 +151,7 @@ ActiveRecord::Schema.define(version: 20190514154758) do
     t.bigint "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type", default: "SimpleChatTextMessage"
+    t.string "type"
     t.string "title"
     t.string "url"
     t.string "display_price"
