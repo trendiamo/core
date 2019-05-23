8946d1edfe75caec94068233f571d0464bd0bf77
Duarte Henriques <duarte.henriques@gmail.com>
2019-05-23 13:39:58 +0100

    Ignore plugin base build

--- slack-bot/lib/services/deploy.rb
+++ slack-bot/lib/services/deploy.rb
@@ -37,7 +37,7 @@ SH
 PLUGIN_CMD = <<~SH.freeze
   cd #{ENV['BUILD_FOLDER']}/core/plugiamo && \
   yarn install --silent --no-progress && \
-  cp plugin-base/plugin-base.js node_modules/plugin-base/ && \
+  cd ../plugin-base && yarn build && cd ../plugiamo && yarn add ../plugin-base && rm -rf node_modules/plugin-base/node_modules && \
   cp #{ENV['PLUGIN_ENV_FILE']} . && \
   mkdir -p ~/.aws && \
   ([ -f ~/.aws/credentials ] || cp #{ENV['AWS_CREDENTIALS_FILE']} ~/.aws/credentials) && \
@@ -50,7 +50,7 @@ SH
 CONSOLE_FRONTEND_CMD = <<~SH.freeze
   cd #{ENV['BUILD_FOLDER']}/core/console-frontend && \
   yarn install --silent --no-progress && \
-  cp plugin-base/plugin-base.js node_modules/plugin-base/ && \
+  cd ../plugin-base && yarn build && cd ../console-frontend && yarn add ../plugin-base && rm -rf node_modules/plugin-base/node_modules && \
   bin/deploy 'ssh -o "StrictHostKeyChecking=no" -i #{ENV['STATIC_KEY_FILE']}'
 SH
 
