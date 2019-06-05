6ec71b3973f5e6e0596cbf86a060ffce08f85b45
Gabriele Canepa <gabriele@canepa.io>
2019-06-05 16:54:57 +0100

    Send confirmation email when adding a new user

--- backend/app/mailers/devise_sparkpost_mailer.rb
+++ backend/app/mailers/devise_sparkpost_mailer.rb
@@ -1,12 +1,24 @@
 class DeviseSparkpostMailer < Devise::Mailer
   default from: "Trendiamo Team <no-reply@trendiamo.com>"
 
+  def confirmation_instructions(record, token, _opts = {})
+    url = api_v1_users_confirmation_url(record, confirmation_token: token)
+    sparkpost_data = {
+      substitution_data: {
+        user_identifier: record.first_name,
+        # https://developers.sparkpost.com/api/template-language/#header-links
+        dynamic_html: { confirm_email_link: %(<a href="#{url}">Confirm your email</a>) },
+      },
+      template_id: "confirmation-instructions",
+    }
+    mail(to: record.email, sparkpost_data: sparkpost_data)
+  end
+
   def reset_password_instructions(record, token, _opts = {})
     url = api_v1_users_password_edit_url(record, reset_password_token: token)
     sparkpost_data = {
       substitution_data: {
-        user_identifier: record.email,
-        # https://developers.sparkpost.com/api/template-language/#header-links
+        user_identifier: record.first_name,
         dynamic_html: { change_password_link: %(<a href="#{url}">Change my password</a>) },
       },
       template_id: "reset-password-instructions",
