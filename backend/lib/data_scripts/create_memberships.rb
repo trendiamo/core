bb2b09b0ca225abd1a15a125eed45e5c3875d328
Daniel Bugge <danielbugge96@gmail.com>
2019-06-06 17:59:01 +0100

    Fix query for memberships create

--- backend/lib/data_scripts/create_memberships.rb
+++ backend/lib/data_scripts/create_memberships.rb
@@ -1,4 +1,4 @@
-User.all.map do |user|
+User.where.not(account_id: nil).count.map do |user|
   Membership.destroy_all
   Membership.create!(account_id: user.account_id, user_id: user.id, role: user.role)
 end
