728ebd4803106fd8962111a6b23e062a725efa1a
MaxChu23 <mymaksym@gmail.com>
2019-06-12 10:07:32 +0100

    Size Guide V2

---
+++ plugiamo/src/special/assessment/size-guide/data.js
@@ -0,0 +1,142 @@
+import { header } from 'special/assessment/data'
+
+export const tagSizeGuides = {
+  'Casual/Jeans und Hosen/': 'jeansUndHosen',
+  'Casual/Jacken/Blousons': 'shirts',
+  'Casual/Jacken/Mäntel': 'shirts',
+  'Casual/Jacken/Jacken': 'shirts',
+  'Casual/Jacken/Westen': 'shirts',
+  'Casual/Oberteile/T-Shirts': 'shirts',
+  'Casual/Oberteile/Polos': 'shirts',
+  'Casual/Oberteile/Hemden': 'shirts',
+  'Casual/Oberteile/Pullover': 'shirts',
+  'Casual/Sakkos/': 'suit',
+
+  'Business/Komplettanzüge': 'suit',
+  'Business/Sakkos/': 'suit',
+  'Business/Westen': 'suit',
+  'Business/Anzughosen/': 'suit',
+  'Business/Hemden/': 'suit',
+  'Business/Accessoires/Gürtel': 'suit',
+  'Business/Accessoires/Krawatten & Einstecktücher': 'suit',
+
+  'Basics/Jeans und Hosen': 'jeansUndHosen',
+}
+
+const simpleChatSteps = {
+  shirts: [
+    {
+      key: 'default',
+      simpleChatMessages: [
+        {
+          type: 'SimpleChatTextMessage',
+          text:
+            'Falls du dir unsicher bist, welche Größe du wählen solltest, kannst du mehr in unserem Größenberater erfahren. Hier sind alle unsere Maße erklärt! 🙂',
+        },
+        {
+          type: 'imageCarousel',
+          imageCarousel: [
+            {
+              picUrl: 'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/size-guide/shirts_0.jpg',
+            },
+            {
+              picUrl:
+                'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/size-guide/Hemden_Kasual.png',
+            },
+          ],
+        },
+        {
+          type: 'SimpleChatTextMessage',
+          text:
+            'Wir zeigen Ihnen, wie Sie in fünf Schritten Maß nehmen, um Ihre passende <a href="https://www.pierre-cardin.de/size-advisor#hemdgroesse">Hemdgröße</a> zu ermitteln.',
+        },
+      ],
+    },
+  ],
+  jeansUndHosen: [
+    {
+      key: 'default',
+      simpleChatMessages: [
+        {
+          type: 'SimpleChatTextMessage',
+          text:
+            'Falls du dir unsicher bist, welche Größe du wählen solltest, kannst du mehr in unserem Größenberater erfahren. Hier sind alle unsere Maße erklärt! 🙂',
+        },
+        {
+          type: 'imageCarousel',
+          imageCarousel: [
+            {
+              picUrl: 'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/size-guide/pants_0.jpg',
+            },
+            {
+              picUrl:
+                'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/size-guide/JeansUndHosen.png',
+            },
+          ],
+        },
+        {
+          type: 'SimpleChatTextMessage',
+          text:
+            'Erfahren Sie hier, wie Sie Ihre <a href="https://www.pierre-cardin.de/size-advisor#hosengroesse">Hosengröße</a> in nur drei Schritten richtig messen.',
+        },
+      ],
+    },
+  ],
+  suit: [
+    {
+      key: 'default',
+      simpleChatMessages: [
+        {
+          type: 'SimpleChatTextMessage',
+          text:
+            'Falls du dir unsicher bist, welche Größe du wählen solltest, kannst du mehr in unserem Größenberater erfahren. Hier sind alle unsere Maße erklärt! 🙂',
+        },
+        {
+          type: 'imageCarousel',
+          imageCarousel: [
+            {
+              picUrl: 'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/size-guide/anzug_0.jpg',
+            },
+            {
+              picUrl:
+                'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/size-guide/Konfection.png',
+            },
+          ],
+        },
+        {
+          type: 'SimpleChatTextMessage',
+          text:
+            'Wir erklären Ihnen, wie Sie Ihre <a href="https://www.pierre-cardin.de/size-advisor#konfektionsgroesse">Konfektionsgröße</a> richtig messen.',
+        },
+      ],
+    },
+  ],
+}
+
+const data = type => ({
+  flowType: 'asmt-size-guide',
+  header,
+  launcher: {
+    chatBubbleText: 'Brauchst du Hilfe bei der Größenwahl?',
+    chatBubbleExtraText: '',
+    persona: {
+      name: 'Nico de Roy',
+      profilePic: {
+        url: 'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/Nico-de-Roy.jpg',
+      },
+    },
+  },
+  closedLauncher: {
+    chatBubbleText: 'Wir sehen uns an der Kasse! 🙂',
+    chatBubbleExtraText: '',
+    persona: {
+      name: 'Nico de Roy',
+      profilePic: {
+        url: 'https://console-assets.ams3.digitaloceanspaces.com/manual/pierre-cardin/Nico-de-Roy.jpg',
+      },
+    },
+  },
+  simpleChat: { simpleChatSteps: simpleChatSteps[type] },
+})
+
+export default data
