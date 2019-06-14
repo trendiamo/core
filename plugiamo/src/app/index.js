4af2a38c913c0137149dc66989c46e7e163bf0e6
Duarte Henriques <duarte.henriques@gmail.com>
2019-06-14 10:27:22 +0100

    Fix bug where plugin would sometimes not show
    When clicked just at the wrong time

--- plugiamo/src/app/index.js
+++ plugiamo/src/app/index.js
@@ -134,10 +134,10 @@ export default compose(
         setSkipContentEntry(true)
       }
       if (showingContent === prevProps.showingContent) return
-      setSkipContentEntry(showingContent)
       if (showingContent) {
         document.documentElement.classList.add('trnd-open')
       } else {
+        setSkipContentEntry(false)
         document.documentElement.classList.remove('trnd-open')
       }
     },
