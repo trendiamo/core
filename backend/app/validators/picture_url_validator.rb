4ba35990da5abe718dc8b03e14f99771d1653a69
Daniel Bugge <danielbugge96@gmail.com>
2019-06-18 09:28:19 +0100

    Fix picture url validator breaking things

--- backend/app/validators/picture_url_validator.rb
+++
@@ -1,7 +0,0 @@
-class PictureUrlValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value.starts_with?('http')
-      record.errors.add(:url, attribute, message: "Invalid URL")
-    end
-  end
-end
