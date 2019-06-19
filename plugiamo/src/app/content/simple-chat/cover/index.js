91bfa7b5eaf953971798259c37566f848616bcfc
Duarte Henriques <duarte.henriques@gmail.com>
2019-06-20 00:06:06 +0100

    minor fix

--- plugiamo/src/app/content/simple-chat/cover/index.js
+++ plugiamo/src/app/content/simple-chat/cover/index.js
@@ -2,9 +2,7 @@ import CoverAssessment from 'special/assessment/cover'
 import { h } from 'preact'
 import { SimpleChatCover as SimpleChatCoverBase } from 'plugin-base'
 
-const SimpleChatCover = props => {
-  const { assessment } = props
-
+const SimpleChatCover = ({ assessment, ...props }) => {
   if (assessment) return <CoverAssessment {...props} />
 
   return <SimpleChatCoverBase {...props} />
