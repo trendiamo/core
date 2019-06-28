d236306f5767fd3ec73a9b44fe75611c79054ad9
Duarte Henriques <duarte.henriques@gmail.com>
2019-06-28 14:18:57 +0100

    Don't print results at the end
    Makes it go faster when executed in the console

--- backend/lib/data_scripts/convert_markdown_messages_to_html.rb
+++ backend/lib/data_scripts/convert_markdown_messages_to_html.rb
@@ -2,4 +2,4 @@ require "redcarpet"
 
 SimpleChatTextMessage.where(html: nil).each do |message|
   message.update!(html: Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(message.text).strip)
-end
+end; nil
