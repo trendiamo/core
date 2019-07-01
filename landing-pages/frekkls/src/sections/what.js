d3879aa6684e77bfa8c1b402670a626141f4d5cc
Duarte Henriques <duarte.henriques@gmail.com>
2019-07-02 00:46:39 +0100

    What section mobilized

--- landing-pages/frekkls/src/sections/what.js
+++ landing-pages/frekkls/src/sections/what.js
@@ -11,45 +11,49 @@ const What = styled(({ className }) => (
   <Section className={className} id="what-you-get">
     <h3>{'What you get'}</h3>
     <div className="what-content">
-      <div className="what-img-container">
-        <WhatLeftImg />
-        <img alt="" className="what-arrow" src={ArrowOrange} />
+      <div clasName="what-social">
+        <div className="what-img-container">
+          <WhatLeftImg />
+          <img alt="" className="what-arrow" src={ArrowOrange} />
+        </div>
+        <div className="what-text">
+          <b className="social-headline">{'Social Salesforce'}</b>
+          <p>
+            {
+              'Let over 100.000 influencers become your personal salesforce matched perfectly with your products and values on page. Take advantage of their reach and expertise and humanize your online sales in seconds with real people.'
+            }
+          </p>
+        </div>
       </div>
-      <div className="what-right-container">
-        <WhatRightImg />
-        <FakeChatWbImg className="fake-chat-wb-img" />
-      </div>
-    </div>
-    <div className="what-content bg2">
-      <div className="what-text">
-        <b className="social-headline">{'Social Salesforce'}</b>
-        <p>
-          {
-            'Let over 100.000 influencers become your personal salesforce matched perfectly with your products and values on page. Take advantage of their reach and expertise and humanize your online sales in seconds with real people.'
-          }
-        </p>
-      </div>
-      <div className="what-text">
-        <b className="tool-headline">{'Sales Technology'}</b>
-        <p>
-          {
-            'Integrate those people and their content with our smart tool directly into your shop and bridge seamlessly their social power with your shop checkouts to increase your conversions and sales for any kind of products and businesses.'
-          }
-        </p>
+      <div className="what-sales">
+        <div className="what-right-container">
+          <WhatRightImg />
+          <FakeChatWbImg className="fake-chat-wb-img" />
+        </div>
+        <div className="what-text">
+          <b className="tool-headline">{'Sales Technology'}</b>
+          <p>
+            {
+              'Integrate those people and their content with our smart tool directly into your shop and bridge seamlessly their social power with your shop checkouts to increase your conversions and sales for any kind of products and businesses.'
+            }
+          </p>
+        </div>
       </div>
     </div>
   </Section>
 ))`
   .what-content {
     flex: 1;
+    flex-direction: column;
     width: 100vw;
     display: flex;
-    align-items: center;
-    background: linear-gradient(to right, #f07643 0%, #f07643 50%, #fff 50%, #fff 100%);
+    align-items: flex-start;
   }
 
-  .bg2 {
-    background: #fafafa;
+  @media (min-width: 900px) {
+    .what-content {
+      flex-direction: row;
+    }
   }
 
   .social-headline {
@@ -62,6 +66,8 @@ const What = styled(({ className }) => (
 
   .what-text {
     padding: 1.8vw;
+    background: #fafafa;
+    flex: 1;
   }
 
   .what-text p {
@@ -75,18 +81,15 @@ const What = styled(({ className }) => (
     font-size: 1.8vw;
   }
 
-  .what-content > div {
-    flex: 1;
-  }
-
   .what-img-container {
     align-self: flex-end;
+    position: relative;
   }
 
   .what-arrow {
     position: absolute;
     top: 40%;
-    left: calc(50% - 1px);
+    right: -5%;
     width: 5%;
     max-width: 80px;
     z-index: 999;
@@ -100,7 +103,13 @@ const What = styled(({ className }) => (
     position: absolute !important;
     bottom: 0;
     right: 0;
-    width: 21vw;
+    width: 34vw;
+  }
+
+  @media (min-width: 900px) {
+    .fake-chat-wb-img.gatsby-image-wrapper {
+      width: 21vw;
+    }
   }
 `
 
