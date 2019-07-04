6ffc95b5d72d5303f38b9f0b73e242633688802d
Duarte Henriques <duarte.henriques@gmail.com>
2019-07-04 15:22:22 +0100

    Upload source maps to rollbar
    So that tracking errors is easier

--- plugiamo/webpack.prod.js
+++ plugiamo/webpack.prod.js
@@ -1,9 +1,21 @@
 /* eslint-disable */
-const webpack = require('webpack')
-const merge = require('webpack-merge')
 const config = require('./webpack.config.js')
+const cp = require('child_process')
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' }).parsed
+const merge = require('webpack-merge')
+const RollbarSourcemapPlugin = require('rollbar-sourcemap-webpack-plugin')
+const webpack = require('webpack')
+
+let version
+try {
+  version = cp.execSync('git rev-parse -q --verify HEAD', { cwd: __dirname, encoding: 'utf8' })
+} catch (err) {
+  console.error('Error getting revision', err)
+  process.exit(1)
+}
 
 module.exports = merge(config, {
+  devtool: 'hidden-source-map',
   mode: 'production',
   optimization: {
     minimize: true,
@@ -11,5 +23,13 @@ module.exports = merge(config, {
   output: {
     publicPath: 'https://js.frekkls.com/',
   },
+  plugins: [
+    dotenv.ROLLBAR_DEPLOY_TOKEN &&
+      new RollbarSourcemapPlugin({
+        accessToken: dotenv.ROLLBAR_DEPLOY_TOKEN,
+        publicPath: 'https://js.frekkls.com',
+        version,
+      }),
+  ].filter(p => p),
   watch: false,
 })
