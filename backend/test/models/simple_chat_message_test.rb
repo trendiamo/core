f1b6a7b4ef3e61d1173668d9acc195dc54bc2ea3
Duarte Henriques <duarte.henriques@gmail.com>
2019-07-05 11:25:42 +0100

    Remove unneeded and flaky test

--- backend/test/models/simple_chat_message_test.rb
+++ backend/test/models/simple_chat_message_test.rb
@@ -10,21 +10,4 @@ class SimpleChatMessageTest < ActiveSupport::TestCase
 
     assert_equal [1, 2, 3, 4], result
   end
-
-  test "simple chat messages sorted after create, with order field" do
-    ActsAsTenant.default_tenant = Account.create!
-
-    simple_chat = create(:simple_chat_with_simple_chat_steps).reload
-    simple_chat_step = simple_chat.simple_chat_steps.first
-    simple_chat_step.simple_chat_messages.first.destroy!
-
-    create(:simple_chat_message, simple_chat_step: simple_chat_step, order: 2)
-    create(:simple_chat_message, simple_chat_step: simple_chat_step, order: 8)
-    create(:simple_chat_message, simple_chat_step: simple_chat_step, order: 1)
-    create(:simple_chat_message, simple_chat_step: simple_chat_step, order: 3)
-
-    result = simple_chat.simple_chat_steps.first.simple_chat_messages.pluck(:order)
-
-    assert_equal [2, 8, 1, 3], result
-  end
 end
