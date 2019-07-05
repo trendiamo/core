c6a41280e1a83577d7fdcbf37ac553909960ce0e
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-07-05 13:55:26 +0100

    Track more clients
    Co-Authored-By: MaxChu23 <mymaksym@gmail.com>

--- plugiamo/src/data-gathering/pionier-workwear.js
+++ plugiamo/src/data-gathering/pionier-workwear.js
@@ -1,9 +1,119 @@
 import mixpanel from 'ext/mixpanel'
+/* eslint-disable no-undef */
 
 export default {
+  addToCartObject() {
+    return {
+      name: 'Add To Cart',
+      data: {
+        hostname: location.hostname,
+        withPlugin: !!$('.frekkls-container')[0],
+        productId: $('input.inputtext.number.itemControl').attr('value'),
+        productName: $('.single-item-title > h2').text(),
+        currency: 'EUR',
+        subTotalInCents: Number(
+          $('.price')
+            .text()
+            .replace(/\D/g, '')
+        ),
+        productOptions: $('.single-item-info > .info-holder')
+          .map((i, element) => {
+            let optionSelectedTitle, optionSelectedValue
+            if ($(element).find('li.active, li.selected')[0]) {
+              optionSelectedTitle = $(element)
+                .find('.parametr-title')
+                .text()
+              optionSelectedValue = $(element)
+                .find('li.active, li.selected')
+                .find('span')
+                .attr('data-value')
+            } else if ($(element).find('fieldset > input')[0]) {
+              optionSelectedTitle = $(element)
+                .find('.parametr-title')
+                .text()
+              optionSelectedValue = $(element).find('fieldset > input')[0].value
+            }
+            if (!optionSelectedValue) return
+            return {
+              attributeLabel: optionSelectedTitle,
+              attributeValue: optionSelectedValue,
+            }
+          })
+          .toArray(),
+      },
+    }
+  },
+  getProductsFromCart() {
+    return $('.item-holder')
+      .map((i, item) => {
+        const itemName = $(item)
+          .find('.item-name')
+          .text()
+          .trim()
+        if (!itemName) return
+        const itemUrl =
+          location.hostname +
+          $(item)
+            .find('a')
+            .attr('href')
+        const itemPrice = $(item)
+          .find('.item-info > li > .value')
+          .last()
+          .text()
+          .trim()
+        const itemPriceInCents = Number(
+          $(item)
+            .find('.item-info > li > .value')
+            .last()
+            .text()
+            .replace(/\D/g, '')
+        )
+        const itemQuantity = $(item)
+          .find('.item-info > li > .value')
+          .first()
+          .text()
+        return {
+          name: itemName,
+          url: itemUrl,
+          price: itemPrice,
+          priceInCents: itemPriceInCents,
+          quantity: itemQuantity,
+          currency: 'EUR',
+        }
+      })
+      .toArray()
+  },
+  checkoutObject() {
+    return {
+      name: 'Proceed To Checkout',
+      data: {
+        hostname: location.hostname,
+        withPlugin: !!$('.frekkls-container')[0],
+        products: this.getProductsFromCart(),
+        subTotalInCents: Number(
+          $('#total_summary_list_info')
+            .text()
+            .replace(/\D/g, '')
+        ),
+        currency: 'EUR',
+      },
+    }
+  },
   setupDataGathering() {
+    const _this = this
+
     if (location.pathname.match(/formonline.shop.shoppingcart.checkout.overview/)) {
       mixpanel.track('Purchase Success', { hostname: location.hostname })
+    } else if ($('.product-form')[0]) {
+      $('.alvineJSPluginOrderformSubmit').on('click', () => {
+        const json = _this.addToCartObject()
+        mixpanel.track(json.name, json.data)
+      })
+    } else if ($('#shCartForm')[0]) {
+      $('.paypalexpress_checkoutbutton > a, .checkoutbutton > a').on('click', () => {
+        const json = _this.checkoutObject()
+        mixpanel.track(json.name, json.data)
+      })
     }
   },
 }
--- plugiamo/src/data-gathering/time-block.js
+++ plugiamo/src/data-gathering/time-block.js
@@ -1,9 +1,201 @@
 import mixpanel from 'ext/mixpanel'
+/* eslint-disable no-undef */
 
 export default {
+  addToCartObject(isfromPLP = false, target) {
+    if (isfromPLP) {
+      const productDiv = $(target).closest('.tmb')
+      return {
+        name: 'Add To Cart',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          productId: $(target).attr('data-product_id'),
+          productName: productDiv.find('.t-entry-title').text(),
+          currency: 'EUR',
+          subTotalInCents: Number(
+            productDiv
+              .find('.woocommerce-Price-amount.amount')
+              .last()
+              .text()
+              .trim()
+              .replace(/\D/g, '')
+          ),
+          isSubscription: !!productDiv.find('.subscription-details').text(),
+        },
+      }
+    } else {
+      return {
+        name: 'Add To Cart',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          productId: $('button.add_to_cart_button')[0].value,
+          productName: $('.product_title').text(),
+          currency: 'EUR',
+          subTotalInCents: Number(
+            $('.price-container')
+              .text()
+              .trim()
+              .replace(/\D/g, '')
+          ),
+          isSubscription: !!$('.price-container .subscription-details').text(),
+        },
+      }
+    }
+  },
+  getProductsFromCart() {
+    return $('.woocommerce-cart-form__cart-item.cart_item')
+      .map((i, item) => {
+        const itemName = $(item)
+          .find('.product-name')
+          .text()
+          .trim()
+        if (!itemName) return
+        const itemUrl = $(item)
+          .find('.product-name > a')
+          .first()
+          .attr('href')
+        const itemPrice = $(item)
+          .find('.woocommerce-Price-amount.amount')
+          .last()
+          .text()
+        const itemPriceInCents = Number(
+          $(item)
+            .find('.woocommerce-Price-amount.amount')
+            .last()
+            .text()
+            .replace(/\D/g, '')
+        )
+        const itemQuantity = $(item)
+          .find('.input-text.qty.text')
+          .attr('value')
+        const isSubscription = !!$(item)
+          .find('.subscription-details')
+          .text()
+        return {
+          name: itemName,
+          url: itemUrl,
+          price: itemPrice,
+          priceInCents: itemPriceInCents,
+          quantity: itemQuantity,
+          currency: 'EUR',
+          isSubscription,
+        }
+      })
+      .toArray()
+  },
+  getProductsFromAjaxCart() {
+    return $('.product_list_widget')
+      .find('.cart-item-list')
+      .map((i, item) => {
+        const itemName = $(item)
+          .find('.cart-item')
+          .text()
+          .trim()
+        if (!itemName) return
+        const itemUrl = $(item)
+          .find('.cart-thumb')
+          .first()
+          .attr('href')
+        const itemPrice = $(item)
+          .find('.woocommerce-Price-amount.amount')
+          .last()
+          .text()
+        const itemPriceInCents = Number(
+          $(item)
+            .find('.woocommerce-Price-amount.amount')
+            .last()
+            .text()
+            .replace(/\D/g, '')
+        )
+        const itemQuantity = $(item)
+          .find('.quantity-container')
+          .text()
+          .replace(/\D/g, '')
+
+        const isSubscription = !!$(item)
+          .find('.subscription-details')
+          .text()
+
+        return {
+          name: itemName,
+          url: itemUrl,
+          price: itemPrice,
+          priceInCents: itemPriceInCents,
+          quantity: itemQuantity,
+          currency: 'EUR',
+          isSubscription,
+        }
+      })
+      .toArray()
+  },
+  checkoutObject(isfromAjaxCart = false) {
+    if (isfromAjaxCart) {
+      return {
+        name: 'Proceed To Checkout',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          products: this.getProductsFromAjaxCart(),
+          subTotalInCents: Number(
+            $('.product_list_widget')
+              .find('.woocommerce-Price-amount.amount')
+              .last()
+              .text()
+              .replace(/\D/g, '')
+          ),
+          currency: 'EUR',
+        },
+      }
+    } else {
+      return {
+        name: 'Proceed To Checkout',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          products: this.getProductsFromCart(),
+          subTotalInCents: Number(
+            $('.order-total')
+              .first()
+              .text()
+              .replace(/\D/g, '')
+          ),
+          currency: 'EUR',
+        },
+      }
+    }
+  },
   setupDataGathering() {
+    const _this = this
+    $('.checkout.wc-forward.btn.btn-link').on('click', () => {
+      // ajax cart
+      const isfromAjaxCart = true
+      const json = _this.checkoutObject(isfromAjaxCart)
+      mixpanel.track(json.name, json.data)
+    })
     if (location.pathname.match(/order-received/)) {
+      // after purchase page
       mixpanel.track('Purchase Success', { hostname: location.hostname })
+    } else if (location.pathname.match(/^\/de\/produkt\//)) {
+      // pdp
+      $('.add_to_cart_button.ajax_add_to_cart').on('click', () => {
+        const json = _this.addToCartObject()
+        mixpanel.track(json.name, json.data)
+      })
+    } else if (location.pathname.match(/^\/de\/produkte\/$/)) {
+      // plp
+      $('.add_to_cart_button.ajax_add_to_cart').click(event => {
+        const isfromPLP = true
+        const json = _this.addToCartObject(isfromPLP, event.target)
+        mixpanel.track(json.name, json.data)
+      })
+    } else if (location.pathname.match(/^\/de\/warenkorb/)) {
+      // regular cart
+      $('.checkout-button.btn.btn-default.alt.wc-forward ').on('click', () => {
+        const json = _this.checkoutObject()
+        mixpanel.track(json.name, json.data)
+      })
     }
   },
 }
