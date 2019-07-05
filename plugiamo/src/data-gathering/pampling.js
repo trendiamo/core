d768395b08ad362ba699ab1cbc70a71f16091b46
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-07-05 13:55:26 +0100

    Track more clients
    Co-Authored-By: MaxChu23 <mymaksym@gmail.com>

--- plugiamo/src/data-gathering/pampling.js
+++ plugiamo/src/data-gathering/pampling.js
@@ -1,9 +1,160 @@
 import mixpanel from 'ext/mixpanel'
+/* eslint-disable no-undef */
 
 export default {
+  addToCartObject(isProductFromModal = false, target) {
+    if (isProductFromModal) {
+      $('.capa-categoria.active')
+        .find('.precio-categoria')
+        .text()
+      return {
+        name: 'Add To Cart',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          productName: $(target)
+            .parent()
+            .attr('data-autor'),
+          currency: 'EUR',
+          subTotalInCents: Number(
+            $('.capa-categoria.active')
+              .find('.precio-categoria')
+              .text()
+              .replace(/\D/g, '') * 10
+          ),
+          productOptions: [
+            {
+              attributeCode: Number(
+                $(target)
+                  .parent()
+                  .attr('data-id-instancia')
+              ),
+              optionValue: $(target)
+                .parent()
+                .attr('data-talla'),
+              attributeLabel: $('.capa-categoria.active')
+                .find('.titulo-categoria')
+                .text(),
+            },
+          ],
+        },
+      }
+    } else {
+      return {
+        name: 'Add To Cart',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          productName: $('#datos_autor')
+            .text()
+            .trim()
+            .replace(/\s+/g, ' '),
+          currency: 'EUR',
+          subTotalInCents: Number(
+            $('#precio_total')
+              .text()
+              .replace(/\D/g, '')
+          ),
+          productOptions: [
+            {
+              attributeCode: Number(
+                $('.opciones_producto')
+                  .find('.marcada')
+                  .find('label')
+                  .attr('for')
+                  .replace(/\D/g, '')
+              ),
+              optionValue: $('.opciones_producto')
+                .find('.marcada')
+                .find('label')
+                .text(),
+              attributeLabel: $('.pestanya.actual')
+                .find('.contenedor_titulo_pestanha')
+                .text()
+                .trim(),
+            },
+          ],
+        },
+      }
+    }
+  },
+  getProductsFromCart() {
+    return $('.pedido')
+      .map((i, item) => {
+        const itemName = $(item)
+          .find('.articulo > a')
+          .text()
+        const itemSize = $(item)
+          .find('.talla > b')
+          .text()
+        const itemUrl = $(item)
+          .find('.articulo > a')
+          .attr('href')
+        const itemPrice = $(item)
+          .find('.total')
+          .text()
+        const itemPriceInCents = Number(
+          $(item)
+            .find('.total')
+            .text()
+            .replace(/\D/g, '')
+        )
+        const itemQuantity = $(item)
+          .find('.cantidad')
+          .text()
+
+        return {
+          name: itemName,
+          size: itemSize,
+          url: itemUrl,
+          price: itemPrice,
+          priceInCents: itemPriceInCents,
+          quantity: itemQuantity,
+          currency: 'EUR',
+        }
+      })
+      .toArray()
+  },
+  checkoutObject() {
+    return {
+      name: 'Proceed To Checkout',
+      data: {
+        hostname: location.hostname,
+        withPlugin: !!$('.frekkls-container')[0],
+        products: this.getProductsFromCart(),
+        subTotalInCents: Number(
+          $('#cantidad_total_productos')
+            .text()
+            .replace(/\D/g, '')
+        ),
+        currency: 'EUR',
+      },
+    }
+  },
   setupDataGathering() {
+    const _this = this
     if (location.pathname.match(/pedido-finalizado/)) {
       mixpanel.track('Purchase Success', { hostname: location.hostname })
+    } else if (location.pathname.match(/tienda\/cesta/)) {
+      if (!$('.pedido')[0]) return
+      $('#btn_comprar').on('click', () => {
+        const json = _this.checkoutObject()
+        mixpanel.track(json.name, json.data)
+      })
+    } else if (location.pathname.match(/productos\//)) {
+      $('input#btn_anadir_articulo').on('click', () => {
+        if ($('form#form_anadir_cesta').serializeArray().length === 0) return
+        const json = _this.addToCartObject()
+        mixpanel.track(json.name, json.data)
+      })
+    } else if (location.pathname.match(/^\/|\/index$/)) {
+      $('.btn.btn-comprar').click(() => {
+        $('.capa-tallas').on('click', '.talla', event => {
+          const isProductFromModal = true
+          const json = _this.addToCartObject(isProductFromModal, event.target)
+          mixpanel.track(json.name, json.data)
+        })
+      })
     }
   },
 }
--- plugiamo/src/data-gathering/tontonetfils.js
+++ plugiamo/src/data-gathering/tontonetfils.js
@@ -1,9 +1,164 @@
 import mixpanel from 'ext/mixpanel'
+/* eslint-disable no-undef */
 
 export default {
+  addToCartObject() {
+    const formFields = $('#AddToCartForm--product-template').serializeArray()
+    return {
+      name: 'Add To Cart',
+      data: {
+        hostname: location.hostname,
+        withPlugin: !!$('.frekkls-container')[0],
+        productId: formFields.find(element => element.name === 'id').value,
+        productName: $('.product-single__title').text(),
+        currency: 'EUR',
+        subTotalInCents: Number(
+          $('#ProductPrice')
+            .text()
+            .replace(/\D/g, '')
+        ),
+        productOptions: $('.single-option-radio')
+          .map((i, element) => {
+            const optionSelected = $(element).find('[checked]')
+            const optionSelectedValue = optionSelected.attr('value')
+            if (!optionSelectedValue) return
+            return {
+              attributeLabel: optionSelected.attr('name'),
+              attributeValue: optionSelectedValue,
+              attributeId: optionSelected.attr('id'),
+            }
+          })
+          .toArray(),
+      },
+    }
+  },
+  getProductsFromCart() {
+    return $('.cart__row')
+      .map((i, item) => {
+        const itemName = $(item)
+          .find('.cart__product-name')
+          .text()
+          .trim()
+        if (!itemName) return
+        const itemUrl =
+          location.hostname +
+          $(item)
+            .find('.cart__product-name')
+            .attr('href')
+        const itemPrice = $(item)
+          .find('.cart__price')
+          .text()
+        const itemPriceInCents = Number(
+          $(item)
+            .find('.cart__price')
+            .text()
+            .replace(/\D/g, '')
+        )
+        const itemQuantity = $(item)
+          .find('.js-qty__num')
+          .attr('value')
+        return {
+          name: itemName,
+          url: itemUrl,
+          price: itemPrice,
+          priceInCents: itemPriceInCents,
+          quantity: itemQuantity,
+          currency: 'EUR',
+        }
+      })
+      .toArray()
+  },
+  getProductsFromAjaxCart() {
+    return $('.ajaxcart__product')
+      .map((i, item) => {
+        const itemName = $(item)
+          .find('.ajaxcart__product-name')
+          .text()
+          .trim()
+        if (!itemName) return
+        const itemUrl =
+          location.hostname +
+          $(item)
+            .find('.ajaxcart__product-name')
+            .attr('href')
+        const itemPrice = $(item)
+          .find('.ajaxcart__price')
+          .text()
+          .trim()
+        const itemPriceInCents = Number(
+          $(item)
+            .find('.ajaxcart__price')
+            .text()
+            .replace(/\D/g, '')
+        )
+        const itemQuantity = $(item)
+          .find('.ajaxcart__qty-num')
+          .attr('value')
+        return {
+          name: itemName,
+          url: itemUrl,
+          price: itemPrice,
+          priceInCents: itemPriceInCents,
+          quantity: itemQuantity,
+          currency: 'EUR',
+        }
+      })
+      .toArray()
+  },
+  checkoutObject() {
+    if ($('#CartDrawer.js-drawer-open')[0]) {
+      return {
+        name: 'Proceed To Checkout',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          products: this.getProductsFromAjaxCart(),
+          subTotalInCents: Number(
+            $('.ajaxcart__subtotal')
+              .last()
+              .text()
+              .replace(/\D/g, '')
+          ),
+          currency: 'EUR',
+        },
+      }
+    } else {
+      return {
+        name: 'Proceed To Checkout',
+        data: {
+          hostname: location.hostname,
+          withPlugin: !!$('.frekkls-container')[0],
+          products: this.getProductsFromCart(),
+          subTotalInCents: Number(
+            $('.cart__subtotal')
+              .text()
+              .replace(/\D/g, '')
+          ),
+          currency: 'EUR',
+        },
+      }
+    }
+  },
   setupDataGathering() {
+    const _this = this
+    $(document).on('submit', 'form.cart.ajaxcart', () => {
+      const json = _this.checkoutObject()
+      mixpanel.track(json.name, json.data)
+    })
     if (location.pathname.match(/9662595172/)) {
       mixpanel.track('Purchase Success', { hostname: location.hostname })
+    } else if ($('#AddToCart--product-template')[0]) {
+      $(document).ajaxComplete((event, xhr, settings) => {
+        if (settings.url === '/cart/add.js' && xhr.status === 200) {
+          const json = _this.addToCartObject()
+          mixpanel.track(json.name, json.data)
+        }
+      })
+    } else if (location.pathname.match(/^\/cart/)) {
+      $('form.cart').on('click', 'button.cart__checkout', () => {
+        const json = _this.checkoutObject()
+        mixpanel.track(json.name, json.data)
+      })
     }
   },
 }
