247e94e2fe322ad5b84cb0e45598d82e17f8de0b
Duarte Henriques <duarte.henriques@gmail.com>
2019-07-08 17:51:09 +0100

    Fix Purchase success event

--- plugiamo/src/data-gathering/pampling.js
+++ plugiamo/src/data-gathering/pampling.js
@@ -2,7 +2,7 @@ import mixpanel from 'ext/mixpanel'
 
 export default {
   setupDataGathering() {
-    if (location.pathname.match(/contacto/)) {
+    if (location.pathname.match(/pedido-finalizado/)) {
       mixpanel.track('Purchase Success', { hostname: location.hostname })
     }
   },
