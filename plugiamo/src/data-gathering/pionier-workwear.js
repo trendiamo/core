a57dedf5fcb1d028a8269f573eadcfbeff1650b3
Duarte Henriques <duarte.henriques@gmail.com>
2019-07-15 10:54:09 +0100

    Fix purschase success event for p-w

--- plugiamo/src/data-gathering/pionier-workwear.js
+++ plugiamo/src/data-gathering/pionier-workwear.js
@@ -102,7 +102,7 @@ export default {
   setupDataGathering() {
     const _this = this
 
-    if (location.pathname.match(/formonline.shop.shoppingcart.checkout.overview/)) {
+    if (location.pathname.match(/formonline\.shop\.shoppingcart\.checkout\.overview/i)) {
       mixpanel.track('Purchase Success', { hostname: location.hostname })
     } else if ($('.product-form')[0]) {
       $('.alvineJSPluginOrderformSubmit').on('click', () => {
