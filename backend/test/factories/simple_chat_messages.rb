0153cbde4d5d847a21af2503a427b7d33c0e58ec
Duarte Henriques <duarte.henriques@gmail.com>
2019-07-16 14:15:12 +0100

    Fix tests re: html chat message changes

--- backend/test/factories/simple_chat_messages.rb
+++ backend/test/factories/simple_chat_messages.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :simple_chat_message do
-    sequence(:text) { Faker::Lorem.sentence }
+    sequence(:html) { Faker::Lorem.sentence }
     simple_chat_step
 
     after(:build) { |record| record.account = ActsAsTenant.default_tenant }
