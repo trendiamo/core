6a5b00e424c022aedfcb94b6cd0db10b8b522277
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-07-26 11:17:44 +0100

    Sort pictures with new fields

--- console-frontend/src/app/resources/pictures/list.js
+++ console-frontend/src/app/resources/pictures/list.js
@@ -1,14 +1,14 @@
 import BlankStateTemplate from 'shared/blank-state'
-import React, { useMemo } from 'react'
+import React from 'react'
 import { ActiveColumn, EnhancedList, Picture, TableCell, Text } from 'shared/table-elements'
 import { apiPictureDestroy, apiPictureList } from 'utils'
 
 const columns = [
   { name: 'id', label: 'id', sortable: true },
   { name: 'picture' },
-  { name: 'type', label: 'type', sortable: true },
+  { name: 'file_format', label: 'type', sortable: true },
   { name: 'url', label: 'url', sortable: true },
-  { name: 'status', label: 'status', sortable: true },
+  { name: 'active', label: 'status', sortable: true },
 ]
 
 const BlankState = () => <BlankStateTemplate imageSource="/img/background/img-welcome.png" title="No pictures yet" />
@@ -25,45 +25,34 @@ const tooltipTextActive = ({ personas, productPicks, simpleChatMessages }) => {
   )}`
 }
 
-const PicturesRow = ({ record, highlightInactive }) => {
-  const recordType = useMemo(
-    () =>
-      record.url
-        .split('.')
-        .pop()
-        .toUpperCase(),
-    [record.url]
-  )
-
-  return (
-    <>
-      <TableCell width="5%">
-        <Picture disabled={highlightInactive} src={record.url} />
-      </TableCell>
-      <TableCell width="10%">
-        <Text disabled={highlightInactive}>{recordType}</Text>
-      </TableCell>
-      <TableCell width="85%">
-        <Text disabled={highlightInactive} style={{ wordBreak: 'break-all' }}>
-          {record.url}
-        </Text>
-      </TableCell>
-      <ActiveColumn
-        highlightInactive={highlightInactive}
-        symbolTextActive="In use"
-        symbolTextInactive="Not in use"
-        tooltipTextActive={tooltipTextActive(record)}
-        tooltipTextInactive="Not used yet"
-      />
-    </>
-  )
-}
+const PicturesRow = ({ record, highlightInactive }) => (
+  <>
+    <TableCell width="5%">
+      <Picture disabled={highlightInactive} src={record.url} />
+    </TableCell>
+    <TableCell width="10%">
+      <Text disabled={highlightInactive}>{record.fileFormat.toUpperCase()}</Text>
+    </TableCell>
+    <TableCell width="85%">
+      <Text disabled={highlightInactive} style={{ wordBreak: 'break-all' }}>
+        {record.url}
+      </Text>
+    </TableCell>
+    <ActiveColumn
+      highlightInactive={highlightInactive}
+      symbolTextActive="In use"
+      symbolTextInactive="Not in use"
+      tooltipTextActive={tooltipTextActive(record)}
+      tooltipTextInactive="Not used yet"
+    />
+  </>
+)
 
 const api = { fetch: apiPictureList, destroy: apiPictureDestroy }
 const picturesRoutes = {}
 const canEditResource = ({ personas, productPicks, simpleChatMessages }) =>
   (personas + productPicks + simpleChatMessages).length < 1
-const defaultSorting = { column: 'status', direction: 'asc' }
+const defaultSorting = { column: 'active', direction: 'asc' }
 const highlightInactive = ['personas', 'productPicks', 'simpleChatMessages']
 
 const PicturesList = () => (
