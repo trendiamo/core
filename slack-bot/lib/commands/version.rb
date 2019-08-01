ab138a437d07ff0839a15c9f606f282ecad61087
Duarte Henriques <duarte.henriques@gmail.com>
2019-08-01 11:08:05 +0100

    Fix git rev in prod

--- slack-bot/lib/commands/version.rb
+++ slack-bot/lib/commands/version.rb
@@ -1,7 +1,7 @@
 module Commands
   class Version < Commands::Base
     def perform
-      git_version = `git rev-parse -q --verify HEAD`.strip
+      git_version = ENV["GIT_REV"] || `git rev-parse -q --verify HEAD`.strip
       hostname = `hostname`.strip
       say_in_context("I'm running on `#{hostname}`, at rev: `#{git_version}`")
     end
