366b4b52a2c32bd428a249ae1e524799b90f8a96
MaxChu23 <mymaksym@gmail.com>
2019-08-14 12:18:36 +0100

    Fix mymuesli data-gathering script

--- plugiamo/src/data-gathering/mymuesli.js
+++ plugiamo/src/data-gathering/mymuesli.js
@@ -1,66 +1,28 @@
 import mixpanel from 'ext/mixpanel'
 /* eslint-disable no-undef */
 
+const convertToCents = selector => {
+  return Number(selector.replace(/\D/g, ''))
+}
+
 export default {
-  addToCartObject(form) {
-    const formFields = $(form).serializeArray()
-    let productId, productName, subTotalInCents
-    if (
-      location.pathname.includes('/muesli/') &&
-      !$(form)
-        .find('button')
-        .attr('data-tm-offer-id')
-    ) {
-      productId = $(form)
-        .find('button')
-        .attr('value')
-      productName = 'Lieblingsmüsli (575g)'
-      subTotalInCents = Number(
-        $(form)
-          .closest('.offerbutton-new-container')
-          .find('.offer-price')
-          .text()
-          .trim()
-          .split('für ')[1]
-          .replace(/\D/g, '')
-      )
-    } else if (location.pathname.includes('/mixer/')) {
-      productId = formFields[3].value
-      productName = 'Lieblingsmüsli (575g)'
-      subTotalInCents = Number(
-        $('.js-total-price')
-          .text()
-          .replace(/\D/g, '')
-      )
-    } else {
-      productId = $(form)
-        .find('button')
-        .attr('data-tm-offer-id')
-      productName = formFields[1].value
-      subTotalInCents = Number(
-        $(form)
-          .find('button')
-          .attr('data-tm-offer-price')
-          .replace(/\D/g, '')
-      )
-    }
+  addToCartObject({ form }) {
     return {
       name: 'Add To Cart',
       data: {
         hostname: location.hostname,
         withPlugin: !!$('.frekkls-container')[0],
-        productId,
-        productName,
         currency: 'EUR',
-        subTotalInCents,
+        ...form,
       },
     }
   },
   getProductsFromCart() {
-    return $('.row[data-offer-id]:not(.toggleOffer)').map((i, item) => {
-      const itemName = $(item)
-        .find('.article-description a')
-        .first()
+    const finalObject = []
+    $('.row[data-offer-id]:not(.toggleOffer)').map((i, item) => {
+      const itemDiv = $(item)
+      const link = itemDiv.find('.article-description a').first()
+      const itemName = link
         .text()
         .trim()
         .replace(/\n|\r/g, '')
@@ -68,14 +30,13 @@ export default {
       let itemUrl = $(item)
         .find('.article-description a')
         .attr('href')
-      if (!itemUrl.includes('www.mymuesli.com')) {
+      if (itemUrl && !itemUrl.includes('www.mymuesli.com')) {
         itemUrl = `https://www.mymuesli.com${itemUrl}`
       }
-      const price = Number(
+      const price = convertToCents(
         $(item)
           .find('.js-total')
           .text()
-          .replace(/\D/g, '')
       )
       const itemQuantity = Number(
         $(item)
@@ -84,19 +45,20 @@ export default {
           .trim()
       )
       let id = $(item).attr('data-offer-id')
-      if ($(item).attr('data-offer-id') === '148') {
-        const splittedUrl = itemUrl.split('/')
-        id = `custom-${splittedUrl[splittedUrl.length - 1]}`
+      if (id === '148') {
+        const splittedUrl = itemUrl && itemUrl.split('/')
+        id = splittedUrl.length !== 0 && `custom-${splittedUrl[splittedUrl.length - 1]}`
       }
-      return {
+      finalObject.push({
         id,
         name: itemName,
         url: itemUrl,
         price,
         quantity: itemQuantity,
         currency: 'EUR',
-      }
+      })
     })
+    return finalObject
   },
   checkoutObject() {
     return {
@@ -105,29 +67,77 @@ export default {
         hostname: location.hostname,
         withPlugin: !!$('.frekkls-container')[0],
         products: this.getProductsFromCart(),
-        subTotalInCents: Number(
-          $('.js-cartamount')
-            .text()
-            .replace(/\D/g, '')
-        ),
+        subTotalInCents: convertToCents($('.js-cartamount').text()),
         currency: 'EUR',
       },
     }
   },
-  setupDataGathering() {
-    if (location.pathname.match(/^\/shop\/complete/)) {
-      mixpanel.track('Purchase Success', { hostname: location.hostname })
-    } else if (location.pathname.match(/^\/shop\/cart/)) {
-      $('.cart-controls').on('click', () => {
-        const json = this.checkoutObject()
-        mixpanel.track(json.name, json.data)
-      })
+  trackAddToCart({ dataElement, isForm }) {
+    const form = this.getAddToCartObject({ dataElement, isForm })
+    const json = this.addToCartObject({ form })
+    mixpanel.track(json.name, json.data)
+  },
+  getAddToCartObject({ dataElement, isForm }) {
+    const element = $(dataElement)
+    if (isForm) {
+      const formFields = element.serializeArray()
+      let productId, productName, subTotalInCents
+      if (location.pathname.includes('/muesli/') && !element.find('button').attr('data-tm-offer-id')) {
+        productId = element.find('button').attr('value')
+        productName = $('.container.basic-section.custommix')
+          .find('.basic-text h1')
+          .first()
+          .text()
+        const priceBase = element
+          .closest('.offerbutton-new-container')
+          .find('.offer-price')
+          .text()
+          .trim()
+          .split('für ')
+        subTotalInCents = convertToCents((priceBase.length > 1 && priceBase[1]) || '0')
+      } else if (location.pathname.includes('/mixer/')) {
+        productId = formFields && formFields[3] && formFields[3].value
+        productName = formFields && formFields[0] && formFields[0].value
+        subTotalInCents = convertToCents($('.js-total-price').text())
+      } else {
+        productId = element.find('button').attr('data-tm-offer-id')
+        productName = formFields && formFields[1] && formFields[1].value
+        subTotalInCents = convertToCents(element.find('button').attr('data-tm-offer-price'))
+      }
+      return {
+        productId,
+        productName,
+        subTotalInCents,
+      }
     }
-    if ($('form[action="https://www.mymuesli.com/shop/cart/put"]')) {
-      $('form[action="https://www.mymuesli.com/shop/cart/put"]').on('submit', event => {
-        const json = this.addToCartObject(event.target)
-        mixpanel.track(json.name, json.data)
+    const productName = element.find('abbr').text()
+    const productId = element.find('.offerbutton-new-button button').attr('data-offer-id')
+    const subTotalInCents = convertToCents(element.find('.nobr span').text())
+    return {
+      productId,
+      productName,
+      subTotalInCents,
+    }
+  },
+  setupDataGathering() {
+    try {
+      if (location.pathname.match(/^\/shop\/complete/)) {
+        mixpanel.track('Purchase Success', { hostname: location.hostname })
+      } else if (location.pathname.match(/^\/shop\/cart/)) {
+        $(document).on('click', 'a[href="https://www.mymuesli.com/shop/kasse"]', () => {
+          const json = this.checkoutObject()
+          mixpanel.track(json.name, json.data)
+        })
+      }
+      $(document).on('click', '.offer-popup-container', event => {
+        const dataElement = $(event.target).closest('.offer-item.offerbutton-new-container')
+        this.trackAddToCart({ dataElement, isForm: false })
       })
+      $('form[action="https://www.mymuesli.com/shop/cart/put"]').on('submit', event =>
+        this.trackAddToCart({ dataElement: event.target, isForm: true })
+      )
+    } catch (error) {
+      // No action
     }
   },
 }
