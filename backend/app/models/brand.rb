513a31d069d0f3d487ee0cbbf84f5dac01a88a47
MaxChu23 <mymaksym@gmail.com>
2019-08-28 15:27:39 +0100

    U2U: fix brand website link button

--- backend/app/models/brand.rb
+++ backend/app/models/brand.rb
@@ -9,5 +9,6 @@ class Brand < ApplicationRecord
       .slice("id", "name", "description", "full_description", "logo_url", "header_image_url", "commission_value",
              "commission_description", "terms_and_conditions", "instagram_url", "facebook_url", "twitter_url",
              "created_at", "updated_at", "lock_version")
+      .merge(website_hostname: account.websites.first.hostnames.first)
   end
 end
