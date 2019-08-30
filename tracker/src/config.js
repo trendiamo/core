6b8ea861d2f1a0bba6d1e5650bfa1548fd6dafa9
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-08-30 11:03:29 +0100

    Track tracker's data-gathering errors in Rollbar

--- tracker/src/config.js
+++ tracker/src/config.js
@@ -1,8 +1,9 @@
 const mixpanelToken = process.env.MIXPANEL_TOKEN
 const production = process.env.NODE_ENV === 'production'
+const rollbarToken = process.env.ROLLBAR_TOKEN
 
 const location = {
   ...window.location,
 }
 
-export { location, mixpanelToken, production }
+export { location, mixpanelToken, production, rollbarToken }
