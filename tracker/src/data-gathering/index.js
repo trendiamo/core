d640b4d2abc19f19238872543983ba830eefebf2
Duarte Henriques <duarte.henriques@gmail.com>
2019-09-05 14:55:09 +0100

    Track a few new websites

--- tracker/src/data-gathering/index.js
+++ tracker/src/data-gathering/index.js
@@ -1,11 +1,18 @@
+import almagreendesign from './almagreendesign'
+import elementum from './elementum'
 import theoceanbottleDataGathering from './theoceanbottle'
 import trendiamoMVP from './trendiamo-mvp'
+import vintageforacause from './vintageforacause'
 import waterhaulDataGathering from './waterhaul'
 
 const dataGatheringFactory = {
   'theoceanbottle.com': theoceanbottleDataGathering,
   'waterhaul.co': waterhaulDataGathering,
   'trendiamo-mvp.myshopify.com': trendiamoMVP,
+  'almagreendesign.com': almagreendesign,
+  'elementum.store': elementum,
+  'www.almagreendesign.com': almagreendesign,
+  'vintageforacause.pt': vintageforacause,
 }
 
 const dataGathering = dataGatheringFactory[window.location.hostname]
---
+++ tracker/src/data-gathering/vintageforacause.js
@@ -0,0 +1,77 @@
+import mixpanel from 'ext/mixpanel'
+import { getAffiliateToken } from 'utils'
+import { RollbarWrapper } from 'ext/rollbar'
+
+const $$ = (selector, callback) => Array.prototype.map.call(document.querySelectorAll(selector), callback)
+const convertToCents = text => (text ? Number(text.replace(/\D/g, '')) : 0)
+
+export default {
+  triggerPurchaseSuccess() {
+    mixpanel.track('Purchase Success', { hostname: location.hostname, affiliateToken: getAffiliateToken() })
+  },
+  triggerCartProceedToCheckout() {
+    const products = $$('#CartItems .line-item', element => {
+      const id = (element.dataset || { variantId: '' }).variantId
+      const name = (element.querySelector('.cart-product-desc a') || { textContent: '' }).textContent.trim()
+      const url = (element.querySelector('.cart-product-desc a') || { href: '' }).href
+      const price = convertToCents((element.querySelector('.product-price') || { textContent: '' }).textContent)
+      const quantity = (element.querySelector('.inputCounter') || { value: 0 }).value
+      return { id, name, url, price, quantity, currency: 'EUR' }
+    })
+    mixpanel.track('Proceed To Checkout', {
+      hostname: location.hostname,
+      products,
+      currency: 'EUR',
+      subTotalInCents: convertToCents((document.querySelector('.cart-total-price') || { textContent: '' }).textContent),
+      affiliateToken: getAffiliateToken(),
+    })
+  },
+  triggerDirectProceedToCheckout() {
+    const id = (((window.ShopifyAnalytics || {}).meta || {}).product || {}).id
+    const name = (document.querySelector('[itemprop="name"]') || { textContent: '' }).textContent.trim()
+    const url = (document.querySelector('meta[itemprop="url"]') || { content: '' }).content
+    const price = convertToCents((document.querySelector('[itemprop="price"]') || { textContent: '' }).textContent)
+    const quantity = (document.querySelector('#quantity') || { value: 0 }).value
+    const products = [
+      {
+        id,
+        name,
+        url,
+        price,
+        quantity,
+        currency: 'EUR',
+      },
+    ]
+    mixpanel.track('Proceed To Checkout', {
+      hostname: location.hostname,
+      products,
+      currency: 'EUR',
+      subTotalInCents: convertToCents(
+        (document.querySelector('.product-normal-price') || { textContent: '' }).textContent
+      ),
+      affiliateToken: getAffiliateToken(),
+    })
+  },
+  _setupDataGathering() {
+    if (location.pathname.match(/\/thank_you$/)) {
+      this.triggerPurchaseSuccess()
+    } else {
+      document.addEventListener('click', event =>
+        RollbarWrapper(() => {
+          if (event.target.classList.contains('cart-checkout') || event.target.classList.contains('paypal-button')) {
+            this.triggerCartProceedToCheckout()
+          }
+        })
+      )
+      $$('[data-testid="PayPalInContext-button"], .shopify-payment-button__more-options', button => {
+        button.addEventListener('click', () => {
+          RollbarWrapper(this.triggerDirectProceedToCheckout.bind(this))
+          return true
+        })
+      })
+    }
+  },
+  setupDataGathering() {
+    RollbarWrapper(this._setupDataGathering.bind(this))
+  },
+}
