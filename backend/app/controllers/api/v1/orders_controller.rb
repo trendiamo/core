c3b70365485d4db41e7a31a8cd83fba669db3957
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-09-09 18:20:39 +0100

    Add revenues endpoint

--- backend/app/controllers/api/v1/orders_controller.rb
+++ backend/app/controllers/api/v1/orders_controller.rb
@@ -8,6 +8,36 @@ module Api
         authorize @orders
         render json: @orders
       end
+
+      def revenues
+        @orders = current_user.orders.where(captured_at: params[:from_date]..params[:to_date])
+        authorize @orders
+        render json: calculate_revenues
+      end
+
+      private
+
+      def calculate_revenues
+        @orders.group_by(&:account_id).map do |account_id, orders|
+          totals = []
+          orders.group_by(&:currency).map do |currency, orders_by_currency|
+            totals << {
+              currency: currency,
+              value: orders_by_currency.sum { |order| order.amount_in_cents * order.commission_rate.to_f },
+            }
+          end
+          { brand_attributes: brand_attributes(account_id), totals: totals }
+        end
+      end
+
+      def brand_attributes(account_id)
+        brand = Account.find(account_id).brand
+        {
+          name: brand.name,
+          description: brand.description,
+          logo_url: brand.logo_url,
+        }
+      end
     end
   end
 end
--- backend/app/policies/order_policy.rb
+++ backend/app/policies/order_policy.rb
@@ -2,4 +2,8 @@ class OrderPolicy < ApplicationPolicy
   def index?
     user
   end
+
+  def revenues?
+    user
+  end
 end
