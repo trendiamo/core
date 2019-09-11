a1a946c29468c0ecc8d6d953d9c47b2dca23b557
Duarte Henriques <duarte.henriques@gmail.com>
2019-09-11 16:21:00 +0100

    minor fix

--- backend/test/factories/users.rb
+++ backend/test/factories/users.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     sequence(:last_name) { Faker::Name.last_name }
     sequence(:email) { Faker::Internet.email(:last_name) }
     sequence(:password) { Faker::Internet.password(8) }
-    currency "EUR"
+    currency "eur"
   end
 end
