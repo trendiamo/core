9bd8e941e2b2332c41e36ed82400d9cc824f57a2
Duarte Henriques <duarte.henriques@gmail.com>
2019-09-19 11:47:54 +0100

    Add GTM in prod

--- console-frontend/src/index.js
+++ console-frontend/src/index.js
@@ -2,6 +2,12 @@ import App from 'app'
 import createHistory from 'history/createBrowserHistory'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TagManager from 'react-gtm-module'
+
+const gtmId = window.location.hostname.match(/uptous\.co$/)
+  ? process.env.REACT_APP_UPTOUS_GTM
+  : process.env.REACT_APP_FREKKLS_GTM
+if (gtmId) TagManager.initialize({ gtmId })
 
 const history = createHistory()
 
