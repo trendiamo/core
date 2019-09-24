c9031c0f23bd44f0de92090a077e8671c703192f
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-09-24 15:05:23 +0100

    Fix u2u privacy-policy and cookie-policy pages

--- console-frontend/src/app/screens/cookie-policy/index.js
+++ console-frontend/src/app/screens/cookie-policy/index.js
@@ -1,18 +1,33 @@
-import content from './content'
 import NotFound from 'app/screens/not-found'
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from 'react'
 import { Container } from 'shared/blank-state/components'
 import { showUpToUsBranding } from 'utils'
 
-const CookiePolicyContent = styled.div``
+const loadContent = (w, d) => {
+  var s = d.createElement('script'),
+    tag = d.getElementsByTagName('script')[0]
+  s.src = 'https://cdn.iubenda.com/iubenda.js'
+  tag.parentNode.insertBefore(s, tag)
+}
 
 const CookiePolicyPage = () => {
+  useEffect(() => {
+    if (showUpToUsBranding()) {
+      loadContent(window, document)
+    }
+  }, [])
+
   if (showUpToUsBranding()) {
     return (
       <Container>
         <div style={{ maxWidth: '700px' }}>
-          <CookiePolicyContent dangerouslySetInnerHTML={{ __html: content }} />
+          <a
+            className="iubenda-white no-brand iubenda-embed iub-body-embed"
+            href="https://www.iubenda.com/privacy-policy/89514409/cookie-policy"
+            title="Cookie Policy"
+          >
+            {'Cookie Policy'}
+          </a>
         </div>
       </Container>
     )
