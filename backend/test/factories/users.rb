a6ce6950a107256f97ff37bcc86bb0db3ab4abfd
Duarte Henriques <duarte.henriques@gmail.com>
2019-09-25 15:21:47 +0100

    Remove unnedeed param in factory

--- backend/test/factories/users.rb
+++ backend/test/factories/users.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
     sequence(:last_name) { Faker::Name.last_name }
     sequence(:email) { Faker::Internet.email(:last_name) }
     sequence(:password) { Faker::Internet.password(8) }
-    currency "eur"
   end
 end
