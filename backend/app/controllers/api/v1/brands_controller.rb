f38d0b0e5d4f913f5764727029dfe563dd4f60f1
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-10-01 17:51:53 +0100

    Minor fix

--- backend/app/controllers/api/v1/brands_controller.rb
+++ backend/app/controllers/api/v1/brands_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class BrandsController < RestAdminController
+      before_action :authenticate_user!
+
       def index
         @brands = policy_scope(Brand).where.not(account_id: current_user.affiliations&.pluck(:account_id)).order(:order)
         authorize @brands
