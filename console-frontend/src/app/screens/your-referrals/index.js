a9d64dd1555e68a1a8a22be34a47a3d84c6b1115
MaxChu23 <mymaksym@gmail.com>
2019-10-03 16:13:06 +0100

    Make U2U admin responsive: referrals + content creating pages

--- console-frontend/src/app/screens/your-referrals/index.js
+++ console-frontend/src/app/screens/your-referrals/index.js
@@ -1,26 +1,37 @@
 import auth from 'auth'
 import ClipboardInput from 'shared/clipboard-input'
 import mixpanel from 'ext/mixpanel'
+import Paper from 'shared/paper'
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import useAppBarContent from 'ext/hooks/use-app-bar-content'
 import UserAvatar from 'shared/user-avatar'
 import { apiMeReferrals, apiRequest } from 'utils'
 import { Callout, CalloutTitle, CardContent, MainCard } from 'shared/uptous'
-import { Paper, Typography } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import { ReactComponent as StartUpIcon } from 'assets/icons/start-up.svg'
 import { useSnackbar } from 'notistack'
 
-const FlexContainer = styled.div`
-  display: flex;
+const ReferralsList = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  @media (min-width: 960px) {
+    min-width: 300px;
+    margin-top: 0;
+  }
+`
+
+const FlexGrid = styled(Grid)`
   justify-content: center;
 `
 
-const ReferralsList = styled.div`
-  min-width: 300px;
-`
-
 const StyledClipboardInput = styled(ClipboardInput)`
+  width: 100%;
+  margin-top: 12px;
+  @media (min-width: 960px) {
+    width: auto;
+    margin-top: 0;
+  }
   input {
     letter-spacing: 4px;
     font-weight: 700;
@@ -31,9 +42,9 @@ const Referral = styled(Paper)`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 12px;
-  margin-bottom: 10px;
-  color: #8799a4;
+  flex-direction: row;
+  padding: 12px;
+  margin-top: 12px;
 `
 
 const ReferralStatus = styled(({ className, state }) => <div className={className}>{state}</div>)`
@@ -45,8 +56,12 @@ const ReferralStatus = styled(({ className, state }) => <div className={classNam
 
 const StyledStartUpIcon = styled(StartUpIcon)`
   align-self: center;
-  width: 62px;
-  margin-bottom: 40px;
+  width: 45px;
+  margin-bottom: 20px;
+  @media (min-width: 960px) {
+    margin-bottom: 40px;
+    width: 62px;
+  }
 `
 
 const ReferralUser = styled.div`
@@ -95,33 +110,37 @@ const YourReferrals = () => {
   }, [])
 
   return (
-    <FlexContainer>
-      <MainCard>
-        <CardContent>
-          <StyledStartUpIcon />
-          <Typography variant="h5">{'Refer a friend and earn twice!'}</Typography>
-          <Typography variant="body2">
-            {
-              'Changing the world for the better is hard to do alone. The more people spreading the word about impactful brands, the better! You can contribute by inviting your friends to also sign up to UPTOUS! Whenever someone that signed up with your referral code creates revenue for the first time, you will receive an additional 10€ in your next payout. Let’s make the world a better place together!'
-            }
-          </Typography>
-        </CardContent>
-        <Callout>
-          <CalloutTitle align="center" variant="h5">
-            {'Your referral code'}
-          </CalloutTitle>
-          <StyledClipboardInput onCopy={onCopyReferralCode} text={referralCode} type="golden" />
-        </Callout>
-      </MainCard>
-      {referrals.length > 0 && (
-        <ReferralsList>
-          <Typography variant="h5">{'Your Referrals'}</Typography>
-          {referrals.map(referral => (
-            <ReferralItem key={referral.user.id} referral={referral} />
-          ))}
-        </ReferralsList>
-      )}
-    </FlexContainer>
+    <FlexGrid container>
+      <Grid item lg={5} md={7} xl={3} xs={12}>
+        <MainCard>
+          <CardContent>
+            <StyledStartUpIcon />
+            <Typography variant="h5">{'Refer a friend and earn twice!'}</Typography>
+            <Typography variant="body2">
+              {
+                'Changing the world for the better is hard to do alone. The more people spreading the word about impactful brands, the better! You can contribute by inviting your friends to also sign up to UPTOUS! Whenever someone that signed up with your referral code creates revenue for the first time, you will receive an additional 10€ in your next payout. Let’s make the world a better place together!'
+              }
+            </Typography>
+          </CardContent>
+          <Callout>
+            <CalloutTitle align="center" variant="h5">
+              {'Your referral code'}
+            </CalloutTitle>
+            <StyledClipboardInput onCopy={onCopyReferralCode} text={referralCode} type="golden" />
+          </Callout>
+        </MainCard>
+      </Grid>
+      <Grid item lg={2} md={4} xl={2} xs={12}>
+        {referrals.length > 0 && (
+          <ReferralsList>
+            <Typography variant="h5">{'Your Referrals'}</Typography>
+            {referrals.map(referral => (
+              <ReferralItem key={referral.user.id} referral={referral} />
+            ))}
+          </ReferralsList>
+        )}
+      </Grid>
+    </FlexGrid>
   )
 }
 
--- console-frontend/src/shared/uptous/index.js
+++ console-frontend/src/shared/uptous/index.js
@@ -3,29 +3,36 @@ import styled from 'styled-components'
 import { Typography } from '@material-ui/core'
 
 export const MainCard = styled(Paper)`
-  margin-left: 1rem;
-  margin-right: 1rem;
   align-self: center;
-  max-width: 600px;
-  min-width: 500px;
   overflow: hidden;
   align-items: stretch;
+  @media (min-width: 960px) {
+    margin-right: 14px;
+  }
 `
 
 export const CardContent = styled.div`
-  padding: 60px 50px 40px;
+  padding: 40px 12px 40px;
   display: flex;
   flex-direction: column;
+  @media (min-width: 960px) {
+    padding: 60px 50px 40px;
+  }
 `
 
 export const Callout = styled.div`
   background-color: #e7ecef;
-  padding: 2rem 3.75rem;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 24px 12px 34px;
+  @media (min-width: 960px) {
+    padding: 2rem 3.75rem;
+  }
 `
 
 export const CalloutTitle = styled(Typography)`
-  margin-bottom: 20px;
+  @media (min-width: 960px) {
+    margin-bottom: 20px;
+  }
 `
