6f2671ffdbe0f3c4393f3f6d71f156339a940467
MaxChu23 <mymaksym@gmail.com>
2019-10-03 16:13:06 +0100

    Make U2U admin responsive: referrals + content creating pages

--- console-frontend/src/app/screens/content-creation/index.js
+++ console-frontend/src/app/screens/content-creation/index.js
@@ -7,12 +7,11 @@ import styled from 'styled-components'
 import useAppBarContent from 'ext/hooks/use-app-bar-content'
 import { apiMeRequestUpgrade, apiRequest } from 'utils'
 import { Callout, CalloutTitle, CardContent, MainCard } from 'shared/uptous'
+import { Grid, Typography } from '@material-ui/core'
 import { ReactComponent as PlayButtonIcon } from 'assets/icons/play-button.svg'
-import { Typography } from '@material-ui/core'
 import { useSnackbar } from 'notistack'
 
-const FlexContainer = styled.div`
-  display: flex;
+const FlexGrid = styled(Grid)`
   justify-content: center;
 `
 
@@ -24,8 +23,12 @@ const RequestPending = styled(Typography)`
 
 const StyledPlayButtonIcon = styled(PlayButtonIcon)`
   align-self: center;
-  width: 62px;
-  margin-bottom: 40px;
+  width: 45px;
+  margin-bottom: 20px;
+  @media (min-width: 960px) {
+    margin-bottom: 40px;
+    width: 62px;
+  }
 `
 
 const ContentCreation = () => {
@@ -61,48 +64,50 @@ const ContentCreation = () => {
   )
 
   return (
-    <FlexContainer>
-      <MainCard>
-        <CardContent>
-          <StyledPlayButtonIcon />
-          <Typography variant="h5">{'Monetize with your content on brand websites!'}</Typography>
-          <Typography variant="body2">
-            {
-              'Gain extra exposure and earn higher commissions by creating and showing guiding and promotional content directly on the brands website! You’ll gain access to additional features that make it easy to upload and submit content to all the brand stores that work with us. However, becoming active in the stores is not automatic and your request to become a content editor will be reviewed by our team within the next 24 hours.'
-            }
-          </Typography>
-        </CardContent>
-        <Callout>
-          <CalloutTitle align="center" variant="h5">
-            {wasSubmitted ? 'YAY!' : 'Become a content editor now'}
-          </CalloutTitle>
-          {wasSubmitted ? (
-            <>
-              <Typography variant="body2">
-                {
-                  'Your request has been submitted and we’ll be in touch as soon as possible! Once you are admitted as a content editor new features will be available here. Check your mail inbox for updates!'
-                }
-              </Typography>
-              <RequestPending variant="button">{'Request pending'}</RequestPending>
-            </>
-          ) : (
-            <Button
-              color="primaryGradient"
-              disabled={isFormSubmitting}
-              isFormSubmitting={isFormSubmitting}
-              onClick={openBioAndPicModal}
-            >
-              {!isFormSubmitting && 'I want in!'}
-            </Button>
-          )}
-        </Callout>
-      </MainCard>
+    <FlexGrid container>
+      <Grid item lg={6} md={8} xl={4} xs={12}>
+        <MainCard>
+          <CardContent>
+            <StyledPlayButtonIcon />
+            <Typography variant="h5">{'Monetize with your content on brand websites!'}</Typography>
+            <Typography variant="body2">
+              {
+                'Gain extra exposure and earn higher commissions by creating and showing guiding and promotional content directly on the brands website! You’ll gain access to additional features that make it easy to upload and submit content to all the brand stores that work with us. However, becoming active in the stores is not automatic and your request to become a content editor will be reviewed by our team within the next 24 hours.'
+              }
+            </Typography>
+          </CardContent>
+          <Callout>
+            <CalloutTitle align="center" variant="h5">
+              {wasSubmitted ? 'YAY!' : 'Become a content editor now'}
+            </CalloutTitle>
+            {wasSubmitted ? (
+              <>
+                <Typography variant="body2">
+                  {
+                    'Your request has been submitted and we’ll be in touch as soon as possible! Once you are admitted as a content editor new features will be available here. Check your mail inbox for updates!'
+                  }
+                </Typography>
+                <RequestPending variant="button">{'Request pending'}</RequestPending>
+              </>
+            ) : (
+              <Button
+                color="primaryGradient"
+                disabled={isFormSubmitting}
+                isFormSubmitting={isFormSubmitting}
+                onClick={openBioAndPicModal}
+              >
+                {!isFormSubmitting && 'I want in!'}
+              </Button>
+            )}
+          </Callout>
+        </MainCard>
+      </Grid>
       <BioAndPicModal
         open={isBioAndPicModalOpen}
         sendUpgradeRequest={sendUpgradeRequest}
         setOpen={setIsBioAndPicModalOpen}
       />
-    </FlexContainer>
+    </FlexGrid>
   )
 }
 
