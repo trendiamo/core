490d16d43a8dff96204a0bc31f2b644f9d8ef6ce
Duarte Henriques <duarte.henriques@gmail.com>
2019-10-04 18:00:04 +0100

    Fix some links

--- console-frontend/src/app/screens/revenues/sections/stripe-button.js
+++ console-frontend/src/app/screens/revenues/sections/stripe-button.js
@@ -1,32 +1,36 @@
 import auth from 'auth'
 import Button from 'shared/button'
-import Link from 'shared/link'
 import queryString from 'query-string'
 import React from 'react'
 
-const user = auth.getUser()
+const goToStripe = () => {
+  const user = auth.getUser()
 
-const stripeUser = {
-  'stripe_user[first_name]': user.firstName,
-  'stripe_user[last_name]': user.lastName,
-  'stripe_user[email]': user.email,
-  'stripe_user[url]': user.socialMediaUrl,
-  'stripe_user[business_name]': user.socialMediaUrl,
-  'stripe_user[business_type]': 'individual',
+  const stripeUser = {
+    'stripe_user[first_name]': user.firstName,
+    'stripe_user[last_name]': user.lastName,
+    'stripe_user[email]': user.email,
+    'stripe_user[url]': user.socialMediaUrl,
+    'stripe_user[business_name]': user.socialMediaUrl,
+    'stripe_user[business_type]': 'individual',
+  }
+
+  window.location = `https://connect.stripe.com/express/oauth/authorize?client_id=${
+    process.env.REACT_APP_STRIPE_CLIENT_ID
+  }&${queryString.stringify(stripeUser)}&redirect_uri=https://338cda40.ngrok.io/revenues`
 }
 
-const stripeLink = `https://connect.stripe.com/express/oauth/authorize?client_id=${
-  process.env.REACT_APP_STRIPE_CLIENT_ID
-}&${queryString.stringify(stripeUser)}`
-
-const StripeButton = ({ isLoading }) => {
-  return (
-    <Link href={stripeLink}>
-      <Button color="primaryGradient" disabled={isLoading} inline isFormSubmitting={isLoading} size="large">
-        {'Connect with stripe'}
-      </Button>
-    </Link>
-  )
-}
+const StripeButton = ({ isLoading }) => (
+  <Button
+    color="primaryGradient"
+    disabled={isLoading}
+    inline
+    isFormSubmitting={isLoading}
+    onClick={goToStripe}
+    size="large"
+  >
+    {'Connect with stripe'}
+  </Button>
+)
 
 export default StripeButton
