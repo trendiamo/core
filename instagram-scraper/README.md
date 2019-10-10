2a2ddf36360f5bd30ce90abb206d3b8c565c1caa
Gabriele Canepa <gabriele.canepa93@gmail.com>
2019-10-10 17:44:31 +0100

    Add HTTP service to Instagram scraper and improve what is scraped

--- instagram-scraper/README.md
+++ instagram-scraper/README.md
@@ -1,28 +1,26 @@
 # README
 
-This is our Instagram scraper, a simple command-line Ruby application built using the `open-uri` module and a set of proxies, necessary to bypass Instagram's rate limit.
+This is our Instagram scraper, a simple Ruby application built using the `open-uri` module and a set of proxies, necessary to bypass Instagram's rate limit.
+
+The scraper can be used with a CLI and a web API, built with [Sinatra](http://sinatrarb.com/).
 
 ## Setup
 
-You'll need to create your `.env` file and specify a `PROXIES_URL` to scrape proxies from - ask a team member about this.
+You'll need to create your `.env` file and specify a `PROXIES_URL` to scrape proxies from and an `API_KEY` to authenticate - ask a team member about this.
 
 ```sh
 bundle
 ```
 
-## Run locally
+## CLI
+
+### Run locally
 
 ```sh
 bin/instagram-scraper [options]
 ```
 
-Or, if you have `./bin` in your `$PATH`, just
-
-```sh
-instagram-scraper [options]
-```
-
-## Usage
+### Usage
 
 Print the usage message in your terminal with
 
@@ -30,8 +28,41 @@ Print the usage message in your terminal with
 bin/instagram-scraper -h
 ```
 
-or
+## API
+
+### Run locally
+
+Start a server with
 
 ```sh
-bin/instagram-scraper --help
+rackup config.ru
 ```
+
+### Endpoints
+
+For a more detailed description check the [docs on Postman](https://documenter.getpostman.com/view/8797404/SVtVSnfz?version=latest).
+
+#### Scrape Brand Posts
+
+Returns a CSV file as attachment, containing the posts matching the specified parameters, or an eventual error in JSON format.
+
+* **URL**
+
+  `/`
+
+* **Method:**
+
+  `GET`
+
+* **Headers**
+
+  * `Authorization=[API_KEY]` **required**
+
+*  **URL Params**
+
+  * `brands=[list]` **required**
+  * `keywords=[list]`
+  * `min_likes=[number]`
+  * `start_date=[date]`
+  * `end_date=[date]`
+  * `output=[filename]`
---
+++ instagram-scraper/lib/scraper/api.rb
@@ -0,0 +1,85 @@
+require "dotenv/load"
+require "json"
+require "sinatra/base"
+require_relative "client"
+require_relative "csv_util"
+
+module InstagramScraper
+  class Api < Sinatra::Base
+    configure do
+      set :show_exceptions, false
+    end
+
+    get "/" do
+      @errors = []
+      @options = parse_params(params)
+      @client = Client.new(@options)
+      validate_options
+      return render_errors unless @errors.empty?
+
+      posts = scrape_posts
+      posts.empty? ? render_unsuccess_message : generate_csv_file(posts)
+    end
+
+    error do |e|
+      add_error(e.message)
+      render_errors
+    end
+
+    private
+
+    def scrape_posts
+      @options[:brands].reduce([]) do |posts, brand|
+        brand_info = @client.scrape_brand_info(brand)
+        next posts unless brand_info
+
+        posts + @client.scrape_brand_posts(brand_info[:id])
+      end
+    end
+
+    def generate_csv_file(posts)
+      content_type "application/csv"
+      status 200
+      attachment @options[:output]
+
+      CSVUtil.generate_csv_from_posts(posts)
+    end
+
+    def parse_params(params) # rubocop:disable Metrics/AbcSize
+      {
+        brands: params[:brands]&.split(","),
+        keywords: params[:keywords]&.split(","),
+        min_likes: params[:min_likes]&.to_i,
+        start_date: params[:start_date] && Date.new(*params[:start_date]&.split("-")&.map(&:to_i)),
+        end_date: params[:end_date] && Date.new(*params[:end_date]&.split("-")&.map(&:to_i)),
+        output: params[:output] || "IG Data (#{Time.now.strftime('%Y-%m-%d at %H.%M.%S')}).csv",
+      }
+    end
+
+    def validate_options
+      add_error("Invalid API key") unless request.env["HTTP_AUTHORIZATION"] == ENV["API_KEY"]
+      add_error("You must specify at least one brand") unless @options[:brands]&.any?
+      return unless @options[:start_date] && @options[:end_date] && @options[:start_date] > @options[:end_date]
+
+      add_error("Start date must come before end date")
+    end
+
+    def add_error(error)
+      @errors << error
+    end
+
+    def render_errors
+      content_type :json
+      status 400
+
+      { errors: @errors }.to_json
+    end
+
+    def render_unsuccess_message
+      content_type :json
+      status 200
+
+      { message: "No posts were found with the specified options" }.to_json
+    end
+  end
+end
