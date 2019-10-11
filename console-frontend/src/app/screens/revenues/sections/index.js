ba58542366f66fb15c92c8df70e18f20253c2c1e
Gabriele Canepa <gabriele.canepa93@gmail.com>
2019-10-11 16:31:59 +0100

    Add Stripe button to revenues blank state

--- console-frontend/src/app/screens/revenues/sections/index.js
+++ console-frontend/src/app/screens/revenues/sections/index.js
@@ -3,6 +3,5 @@ import Activity from './activity'
 import BrandsSplit from './brands-split'
 import MonthlyRevenue from './monthly-revenue'
 import Orders from './orders'
-import StripeButton from './stripe-button'
 
-export { Activity, BrandsSplit, MonthlyRevenue, Orders, StripeButton, Actions }
+export { Activity, BrandsSplit, MonthlyRevenue, Orders, Actions }
