354d74c58db43c06612fd07d88dcfb81e084e14d
Gabriele Canepa <gabriele.canepa93@gmail.com>
2019-11-04 14:45:34 +0000

    Implement new content creation page

--- console-frontend/src/app/screens/content-creation/index.js
+++ console-frontend/src/app/screens/content-creation/index.js
@@ -6,33 +6,50 @@ import React, { useCallback, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import useAppBarContent from 'ext/hooks/use-app-bar-content'
 import { apiMeRequestUpgrade, apiRequest } from 'utils'
-import { Callout, CalloutTitle, CardContent, MainCard } from 'shared/uptous'
+import { Callout, CardContent, MainCard } from 'shared/uptous'
+import { ReactComponent as ContentIcon } from 'assets/icons/content.svg'
 import { Grid, Typography } from '@material-ui/core'
-import { ReactComponent as PlayButtonIcon } from 'assets/icons/play-button.svg'
 import { useSnackbar } from 'notistack'
 
 const FlexGrid = styled(Grid)`
   justify-content: center;
 `
 
-const RequestPending = styled(Typography)`
-  color: #0f7173;
-  margin-top: 14px;
-  width: 100%;
+const StyledCard = styled(MainCard)`
+  overflow: visible;
+  position: relative;
 `
 
-const StyledPlayButtonIcon = styled(PlayButtonIcon)`
+const BetaBadge = styled(props => <div {...props}>{'BETA'}</div>)`
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  z-index: 1202;
+  padding: 5px 20px;
+  background-color: #f05d5e;
+  border-radius: 6px;
+  color: white;
+  font-weight: bold;
+`
+
+const StyledContentIcon = styled(ContentIcon)`
   align-self: center;
-  width: 45px;
+  width: 100px;
   margin-bottom: 20px;
+
   @media (min-width: 960px) {
     margin-bottom: 40px;
-    width: 62px;
   }
 `
 
+const RequestPending = styled(Typography)`
+  align-self: flex-start;
+  color: #0f7173;
+  margin-top: 14px;
+`
+
 const ContentCreation = () => {
-  const appBarContent = useMemo(() => ({ title: 'Become a content editor for brand websites' }), [])
+  const appBarContent = useMemo(() => ({ title: 'Become a product curator on brand websites!' }), [])
   useAppBarContent(appBarContent)
 
   const [isFormSubmitting, setIsFormSubmitting] = useState(false)
@@ -66,28 +83,29 @@ const ContentCreation = () => {
   return (
     <FlexGrid container>
       <Grid item lg={6} md={8} xl={4} xs={12}>
-        <MainCard>
+        <StyledCard>
+          <BetaBadge />
           <CardContent>
-            <StyledPlayButtonIcon />
-            <Typography variant="h5">{'Monetize with your content on brand websites!'}</Typography>
+            <StyledContentIcon />
+            <Typography variant="h5">{'Push and monetize your content on brand websites'}</Typography>
+            <Typography style={{ marginBottom: '20px' }} variant="body2">
+              {
+                'Apply to our Curation Programme to share your passion and content directly on brand websites and help them to convert their own traffic with your expertise!'
+              }
+            </Typography>
             <Typography variant="body2">
               {
-                'Gain extra exposure and earn higher commissions by creating and showing guiding and promotional content directly on the brands website! You’ll gain access to additional features that make it easy to upload and submit content to all the brand stores that work with us. However, becoming active in the stores is not automatic and your request to become a content editor will be reviewed by our team within the next 24 hours.'
+                'While the programme is in beta mode, only selected impacters may participate. We’ll notify you per email about the status of your application. Thank you!'
               }
             </Typography>
           </CardContent>
           <Callout>
-            <CalloutTitle align="center" variant="h5">
-              {wasSubmitted ? 'YAY!' : 'Become a content editor now'}
-            </CalloutTitle>
             {wasSubmitted ? (
               <>
                 <Typography variant="body2">
-                  {
-                    'Your request has been submitted and we’ll be in touch as soon as possible! Once you are admitted as a content editor new features will be available here. Check your mail inbox for updates!'
-                  }
+                  {'Thank you for your application! We’ll be in touch as soon as we have news!'}
                 </Typography>
-                <RequestPending variant="button">{'Request pending'}</RequestPending>
+                <RequestPending>{'Status: Application Pending'}</RequestPending>
               </>
             ) : (
               <Button
@@ -100,7 +118,7 @@ const ContentCreation = () => {
               </Button>
             )}
           </Callout>
-        </MainCard>
+        </StyledCard>
       </Grid>
       <BioAndPicModal
         open={isBioAndPicModalOpen}
