95fb278b4dd980b37f7665631901b1e5bba161cc
Francisco Bota <francisco.rodrigues.bota@gmail.com>
2019-11-06 14:36:18 +0000

    Remove collapsible sample product request from brand page

--- console-frontend/src/app/screens/affiliate-partners/details/description-extra.js
+++ console-frontend/src/app/screens/affiliate-partners/details/description-extra.js
@@ -13,27 +13,6 @@ const ExpansionPanelContent = styled.div`
   }
 `
 
-const onZendeskHelpClick = () => {
-  if (!window.zE) return
-  const plugin = document.querySelector('.frekkls-container')
-  if (plugin) plugin.remove()
-  window.zE('webWidget', 'show')
-  window.zE('webWidget', 'toggle')
-}
-
-const TextButton = styled.button`
-  font-family: Lato, 'Helvetica', 'Arial', sans-serif;
-  font-size: 16px;
-  background: none;
-  border: 0;
-  margin: 0;
-  padding: 0;
-  outline: none;
-  text-decoration: underline;
-  cursor: pointer;
-  color: #00e;
-`
-
 const StyledExpansionPanel = styled(ExpansionPanel)`
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.05);
   &,
@@ -42,40 +21,6 @@ const StyledExpansionPanel = styled(ExpansionPanel)`
     border-radius: 0;
   }
 `
-
-/* eslint-disable react/jsx-max-depth */
-const SampleProductInstructions = () => (
-  <div>
-    <StyledExpansionPanel>
-      <ExpansionPanelSummary aria-controls="panel-spi-content" expandIcon={<ExpandMoreIcon />} id="panel-spi-header">
-        <Typography variant="h6">{'Sample Product Requests'}</Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <ExpansionPanelContent>
-          <p>
-            {'If you are interested in receiving a free sample product you can apply by sending an email to '}
-            <a href="mailto:support@uptous.co">{'support@uptous.co'}</a>
-            {' or by filling out '}
-            <TextButton onClick={onZendeskHelpClick}>{'this form'}</TextButton>
-            {' with the following information:'}
-          </p>
-          <ul>
-            <li>{'Name of the Brand'}</li>
-            <li>{'Link to the Product'}</li>
-            <li>{'Text describing the content you will create with the product and how you will post it.'}</li>
-          </ul>
-          <p>
-            {
-              'Using email: Please, use the same email address you used for your Uptous account as the sender address and put "Sample Product Request" as the subject.'
-            }
-          </p>
-          <p>{'Using the form: Please select "Sample Product Request" as the subject.'}</p>
-          <p>{'Applications that miss any information mentioned above will be declined.'}</p>
-        </ExpansionPanelContent>
-      </ExpansionPanelDetails>
-    </StyledExpansionPanel>
-  </div>
-)
 
 /* eslint-enable react/jsx-max-depth */
 const TermsAndConditions = ({ brand }) => {
@@ -101,12 +46,7 @@ const TermsAndConditions = ({ brand }) => {
 const DescriptionExtra = ({ brand }) => {
   if (brand.isPreview) return null
 
-  return (
-    <>
-      <SampleProductInstructions />
-      <TermsAndConditions brand={brand} />
-    </>
-  )
+  return <TermsAndConditions brand={brand} />
 }
 
 export default DescriptionExtra
