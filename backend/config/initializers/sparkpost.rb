654f17100c578396de368dfc964336fd5415d737
Duarte Henriques <duarte.henriques@gmail.com>
2019-11-11 18:33:59 +0000

    minor fix

--- backend/config/initializers/sparkpost.rb
+++ backend/config/initializers/sparkpost.rb
@@ -1,6 +1,6 @@
 if ENV["SPARKPOST_API_KEY"]
   SparkPostRails.configure do |c|
-    c.api_endpoint = "https://api.eu.sparkpost.com/api/v1/transmissions"
+    c.api_endpoint = "https://api.eu.sparkpost.com/api/"
     c.api_key = ENV["SPARKPOST_API_KEY"]
   end
 end
