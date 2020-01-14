4abc820a32c9e4df599ca7ad6a2fb36987e8718f
Daniel Welzel <daniel@familiewelzel.de>
2020-01-14 17:45:46 +0000

    New 404 page design and img

--- landing-pages/uptous/src/pages/404.js
+++ landing-pages/uptous/src/pages/404.js
@@ -4,7 +4,7 @@ import { graphql, Link } from 'gatsby'
 
 import Container from '../components/container'
 import Layout from '../layout'
-import MagnifyingGlass from '../images/magnifying-glass.svg'
+import image404 from '../images/404.png'
 import Section from '../components/section'
 import Seo from '../layout/seo'
 
@@ -15,11 +15,11 @@ const NotFoundPage = styled(({ className, data }) => (
     <Seo title="Uptous - 404 Not Found" />
     <Section className={className}>
       <Container>
-        <MagnifyingGlass />
+        <Image src={image404} />
         <Content>
-          <h1>{'Oops!'}</h1>
-          <p>{"We couldn't find the page you were looking for!"}</p>
-          <Link to="/">{'‹ Back home'}</Link>
+          <h1>{'Nothing seems to be at this URL'}</h1>
+          <p>{"Sorry, the URL you entered doesn’t have any results on this website!"}</p>
+          <Link to="/">{'Go to Homepage'}</Link>
         </Content>
       </Container>
     </Section>
@@ -28,10 +28,9 @@ const NotFoundPage = styled(({ className, data }) => (
   flex: 1;
   ${Container} {
     min-height: 500px;
-    max-width: 600px;
+    max-width: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     position: relative;
   }
   svg {
@@ -43,16 +42,51 @@ const NotFoundPage = styled(({ className, data }) => (
   }
   ${Content} {
     z-index: 1;
+    margin-top: 60px;
   }
   h1 {
-    font-size: 2rem;
+    font-size: 20px;
     font-weight: bold;
     text-align: center;
-    color: #12e5c4;
   }
   a {
-    color: #12e5c4;
+    display: block;
+    color: white;
+    padding: 12px;
+    width: 220px;
+    text-align: center;
+    margin: 0 auto;
+    background: black;
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: 700;
+    border-radius: 3px;
   }
+  p {
+    text-align: center;
+    margin-top: 10px;
+    font-size: 16px;
+  }
+
+  @media (min-width: 1000px) {
+    ${Container} {
+      min-height: 700px;
+    }
+    h1 {
+      font-size: 36px;
+    }
+    ${Content} {
+      margin-top: 120px;
+    }
+  }
+`
+
+const Image = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  user-select: none;
 `
 
 export const query = graphql`
